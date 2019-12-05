c1c81c5b95d252e2273f14628bb8c36bdb28659d
jdjakem <29109026+jdjakem@users.noreply.github.com>
2019-12-05 15:21:00 -0700

    second order stochastic dominance is now working with disutility formulation

--- pyapprox/stochastic_dominance.py
+++ pyapprox/stochastic_dominance.py
@@ -38,7 +38,8 @@ def build_inequality_contraints(Y,Yvec,basis_matrix,p,
        The values y_i, i=1,...,M
 
     Yvec : np.ndarray (N,1)
-        The values v_i = E[(y_i-Y)^{+}], v_i=1,...,M
+        The values v_i = E[(y_i-Y)^{+}], v_i=1,...,M or
+        v_i = E[(y_i+Y)^{+}] if disutility_formulation=True
 
     p : np.ndarray (N,1)
        The probabilities p_k, k=1,...,M
@@ -94,10 +95,12 @@ def compute_conditional_expectations(eta,samples,disutility_formulation=True):
     assert eta.ndim==1
     if disutility_formulation:
         values = np.asarray(
-            [np.maximum(eta[ii]+samples,0).mean() for ii in range(eta.shape[0])])
+            [np.maximum(eta[ii]+samples,0).mean()
+             for ii in range(eta.shape[0])])
     else:
         values = np.asarray(
-            [np.maximum(eta[ii]-samples,0).mean() for ii in range(eta.shape[0])])
+            [np.maximum(eta[ii]-samples,0).mean()
+             for ii in range(eta.shape[0])])
     return values
 
 def gradient(coeff0,N,M):
