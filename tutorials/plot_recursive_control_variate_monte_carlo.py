00a224a22d9a88c5398857a676afbca76445929a
jdjakem <29109026+jdjakem@users.noreply.github.com>
2020-02-12 12:52:11 -0700

    figured out how to embed arbitrary images in sphixn gallery examples

--- tutorials/plot_recursive_control_variate_monte_carlo.py
+++ tutorials/plot_recursive_control_variate_monte_carlo.py
@@ -1,11 +1,69 @@
 r"""
 Recursive Approximate Control Variate Monte Carlo
-=======================================
+=================================================
 This tutorial builds upon :ref:`sphx_glr_auto_tutorials_plot_approximate_control_variate_monte_carlo.py` and demonstrates that multi-level Monte Carlo and multi-fidelity Monte Carlo are both approximate control variate techniques and how this understanding can be used to improve their efficiency.
 
 
 Multi-level Monte Carlo (MLMC)
 ------------------------------
+The multi-level (MLMC) estimator based on :math:`M+1` models :math:`f_0,\ldots,f_M` ordered by decreasing fidelity (note typically MLMC literature reverses this order) is given by
+
+.. math:: Q_0^\mathrm{ML}=\mean{f_M}+\sum_{\alpha=1}^M\mean{f_{\alpha-1}-f_\alpha}
+
+Similarly to ACV we approximate each expectation using Monte Carlo sampling such that
+
+.. math::Q_{0,N,\V{r}}^\mathrm{ML}=\sum_{n=1}^{\hat{r}_MN}f_M^{(n)}(\hat{\mathcal{Z}}_{M})+\sum_{\alpha=1}^M\left(\sum_{n=1}^{\hat{r}_{\alpha-1} N}\left(f_{\alpha-1}^{(n)}(\hat{\mathcal{Z}}_{\alpha-1})-f_\alpha^{(n)}(\hat{\mathcal{Z}}_{\alpha-1})\right)\right)
+
+where :math:`\hat{\mathcal{Z}}_{\alpha}\cap\hat{\mathcal{Z}}_{\kappa}=\emptyset`, if :math:`\alpha\neq\kappa` and  :math:`\hat{r}_0=1`.
+The 2 model MLMC estimator is
+
+.. math::
+   Q_{0,N,\V{r}}^\mathrm{ML}&=\sum_{n=1}^{\hat{r}_1N}f_1^{(n)}(\hat{\mathcal{Z}}_1)+\sum_{n=1}^{N}\left(f_{0}^{(n)}(\hat{\mathcal{Z}}_0)-f_1^{(n)}(\hat{\mathcal{Z}}_0)\right)\\
+   &=Q_{1,\mathcal{Z}_{1,2}}+\left(Q_{0,\mathcal{Z}_{0,2}}-Q_{1,\mathcal{Z}_{1,1}}\right)
+
+By rearranging terms it is clear that this is just a control variate estimator
+
+.. math::
+
+   Q_{0,N,\V{r}}^\mathrm{ML}&=Q_{0,\mathcal{Z}_{0,2}}-\left(Q_{1,\mathcal{Z}_{1,1}}-Q_{1,\mathcal{Z}_{1,2}}\right)\\
+  &=\sum_{n=1}^{N}f_0^{(n)}(\mathcal{Z}_{0,2}) -\left(\sum_{n=1}^{N}f_1^{(n)}(\mathcal{Z}_{1,1})-\sum_{n=1}^{\hat{r}_1N}f_{1}^{(n)}(\mathcal{Z}_{1,2})\right)\\
+  &=\sum_{n=1}^{N}f_0^{(n)}(\mathcal{Z}_{0,2}) -\left(\sum_{n=1}^{N}f_1^{(n)}(\mathcal{Z}_{1,1})-\mu_{1,N,r_1}\right)
+
+is just an approximate control variate estimator with the control variate weight :math:`\eta=-1` and :math:`\mathcal{Z}_{0,2}=\mathcal{Z}_{1,1}=\hat{\mathcal{Z}}_0`, :math:`\mathcal{Z}_{1,2}=\hat{\mathcal{Z}}_1`, and :math:`\mathcal{Z}_{0,1}=\emptyset`, . But unlike before the values of the low-fidelity model obtained at the samples used by the high-fidelity model as well are not used to approxixmate the low-fidelity mean :math:`\mu_{1,N,r_1}` so  in the ACV notation :math:`r_1=\hat{r}_1+N`
+
+When using more than two models, MLMC just introduces more expectations of differences. For three models we have
+
+.. math:: Q_{0,N,\V{r}}^\mathrm{ML}=Q_{2,\mathcal{Z}_{2,2}}+\left(Q_{1,\mathcal{Z}_{1,2}}-Q_{2,\mathcal{Z}_{2,1}}\right)+\left(Q_{0,\mathcal{Z}_{0,2}}-Q_{1,\mathcal{Z}_{1,1}}\right)
+
+Which we can then rearrange into a control variate estimator
+
+.. math:: Q_{0,N,\V{r}}^\mathrm{ML}=Q_{0,\mathcal{Z}_{0,2}} - \left(Q_{1,\mathcal{Z}_{1,1}}-Q_{1,\mathcal{Z}_{1,2}}\right)-\left(Q_{2,\mathcal{Z}_{2,1}}-Q_{2,\mathcal{Z}_{2,2}}\right)
+
+and by inductive reasoning we get the M model ACV version of the MLMC estimator
+
+.. math:: Q_{0,N,\V{r}^\mathrm{ML}}=Q_{0,\mathcal{Z}_{0,1}} +\sum_{\alpha=1}^M\eta_\alpha\left(Q_{\alpha,\mathcal{Z}_{\alpha-1,1}}-\mu_{\alpha,\mathcal{Z}_{\alpha,2}}\right)
+
+where :math:`\eta_\alpha=-1,\forall\alpha` and :math:`\mathcal{Z}_{\alpha,1}=\mathcal{Z}_{\alpha-1,2}`, and :math:`\mu_{\alpha,\mathcal{Z}_{\alpha,2}}=Q_{\alpha,\mathcal{Z}_{\alpha,2}}`
+ 
+.. list-table::
+
+   * - .. figure:: ../figures/mlmc.png
+          :width: 100%
+          :align: center
+
+          MLMC sampling strategy
+
+     - .. figure:: ../figures/acv_is.png
+          :width: 100%
+          :align: center
+
+          ACV sampling strategy
+
+
+.. math::  R^2 = - \eta_1^2 \tau_{1}^2 - 2 \eta_1 \rho_{1} \tau_{1} - \eta_M^2 \frac{\tau_{M}}{\hat{r}_{M}} - \sum_{1=2}^M \frac{1}{\hat{r}_{i-1}}
+                                                          \left( \eta_i^2 \tau_{i}^2 + \tau_{i-1}^2 \tau_{i-1}^2 - 2 \eta_i \eta_{i-1} \rho_{i,i-1} \tau_{i} \tau_{i-1} \right),
+
+where  :math:`\tau_\alpha=\left(\frac{\var{Q_\alpha}}{\var{Q_0}}\right)^{\frac{1}{2}}`. Recall that and :math:`\hat{r}_\alpha=\lvert\mathcal{Z}_{\alpha,2}\rvert/N` is the ratio of the cardinality of the sets :math:`\mathcal{Z}_{\alpha,2}` and :math:`\mathcal{Z}_{0,2}`. Thus we can see that the variance reduction is bounded by the CV estimator using the lowest fidelity model with the highest correlation with :math:`f_0`
 
 Total cost is
 
