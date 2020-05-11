0772eed478709462759384220213824a7ee2b939
jdjakem <29109026+jdjakem@users.noreply.github.com>
2020-05-11 15:02:07 -0600

    added ability to annotate heat map

--- pyapprox/visualization.py
+++ pyapprox/visualization.py
@@ -693,3 +693,66 @@ def pya_colorbar(mappable,ax_in=None):
     if ax_in is None:
         plt.sca(last_axes)
     return cbar
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A list or array of two color specifications.  The first is used for
+        values below a threshold, the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = mpl.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    ranges=im.get_extent()
+    xlocs=np.linspace(ranges[0],ranges[1],data.shape[1]+1)[:-1]
+    xlocs+=(xlocs[1]-xlocs[0])/2
+    ylocs=np.linspace(ranges[2],ranges[3],data.shape[0]+1)[:-1]
+    ylocs+=(ylocs[1]-ylocs[0])/2
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(
+                xlocs[j], ylocs[i], valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
