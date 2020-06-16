6031f4e449a231b5289382aae07efb92dc40a61c
jdjakem <29109026+jdjakem@users.noreply.github.com>
2020-06-16 17:25:35 -0600

    added wrapper to ipopt for optimization

--- pyapprox/optimization.py
+++ pyapprox/optimization.py
@@ -534,27 +534,24 @@ def nonlinear_basis_pursuit(func,func_jac,func_hess,init_guess,options):
 
 def kouri_smooth_absolute_value(t,r,x):
     vals = np.zeros(x.shape[0])
-    I = np.where(r*x+t<-1)[0]
-    vals[I] = -1/r*(r * x[I] + t[I] + 1/2 + 1/2 * t[I]**2)
-    J = np.where((-1<=r*x+t)&(r*x+t<=1))[0]
-    vals[J] = t[J] * x[J] + r/2 * x[J]**2
-    K = np.where(1<r*x+t)[0]
-    vals[K] = 1/r * (r * x[K] + t[K] - 1/2 - 1/2 * t[K]**2)
+    z = r*x+t
+    I = np.where(z<-1)[0]
+    vals[I] = -1/r*(z[I] + 0.5 + 0.5 * t[I]**2)
+    J = np.where((-1<=z)&(z<=1))[0]
+    vals[J] = t[J] * x[J] + 0.5*r * x[J]**2
+    K = np.where(1<z)[0]
+    vals[K] = 1/r * (z[K] - 0.5 - 0.5 * t[K]**2)
     return vals
 
 def kouri_smooth_absolute_value_gradient(t,r,x):
-    grad = np.zeros(x.shape[0])
-    I = np.where(r*x+t<-1)[0]
-    grad[I] = -1
-    J = np.where((-1<=r*x+t)&(r*x+t<=1))[0]
-    grad[J] = t[J] + r * x[J]
-    K = np.where(1<r*x+t)[0]
-    grad[K] = 1
+    z = r*x+t
+    grad = np.maximum(-1,np.minimum(1,z))
     return grad
 
 def kouri_smooth_absolute_value_hessian(t,r,x):
     hess = np.zeros(x.shape[0])
-    J = np.where((-1<=r*x+t)&(r*x+t<=1))[0]
+    z = r*x+t
+    J = np.where(np.absolute(z)<=1)[0]
     hess[J] = r
     return hess
 
@@ -576,14 +573,11 @@ def kouri_smooth_l1_norm_hessp(t,r,x,v):
     hess = kouri_smooth_absolute_value_hessian(t,r,x)
     return hess*v[0]
         
-def basis_pursuit_denoising(func,func_jac,func_hess,init_guess,eps,options,homotopy_options):
+def basis_pursuit_denoising(func,func_jac,func_hess,init_guess,eps,options):
 
     t = np.zeros_like(init_guess)
-    r = 1
 
-    method = 'trust-constr'
-    #method = 'slsqp'
-    #method = 'cobyla'
+    method = options.get('method','slsqp')
     nunknowns = init_guess.shape[0]
 
     def constraint_obj(x):
@@ -608,50 +602,134 @@ def basis_pursuit_denoising(func,func_jac,func_hess,init_guess,eps,options,homot
     constraint_hessian = BFGS()
 
     nonlinear_constraint = NonlinearConstraint(
-        constraint_obj,-eps,eps,jac=constraint_jac,hess=constraint_hessian,
+        constraint_obj,0,eps**2,jac=constraint_jac,hess=constraint_hessian,
         keep_feasible=False)
     constraints = [nonlinear_constraint]
 
+    # Maximum Number Outer Iterations
+    maxiter = options.get('maxiter',100)
+    # Maximum Number Outer Iterations
+    maxiter_inner = options.get('maxiter_inner',1000)
+    # Desired Dual Tolerance 
+    ttol = options.get('dualtol',1e-6)
+    # Verbosity Level
+    verbose = options.get('verbose',1)
+    # Initial Penalty Parameter
+    r = options.get('r0',1)
+    # Max Penalty Parameter
+    rmax = options.get('rmax',1e6)
+    # Optimization Tolerance Update Factor
+    tfac = options.get('tfac',1e-1)
+    # Penalty Parameter Update Factor
+    rfac = options.get('rfac',2)
+    # Desired Feasibility Tolerance
+    ctol = options.get('ctol',1e-8)
+    # Desired Optimality Tolerance
+    gtol = options.get('gtol',1e-8)
+    # Initial Dual Tolerance
+    ttol0  = options.get('ttol0',1)
+    # Initial Feasiblity Tolerance
+    ctol0  = options.get('ctol0',1e-2)
+    # Initial Optimality Tolerance
+    gtol0  = options.get('gtol0',1e-2)
+    # Tolerance for termination for change in objective
+    ftol = options.get('ftol',1e-8)
+
     niter=0
-    prev_obj = np.inf
     x0=init_guess
-    maxiter,xtol = homotopy_options.get('maxiter',1000),homotopy_options.get('xtol',1e-8)
-    verbose=homotopy_options.get('verbose',1)
+    f0 = np.inf
     nfev,njev,nhev=0,0,0
     constr_nfev,constr_njev,constr_nhev=0,0,0
     while True:
         obj  = partial(kouri_smooth_l1_norm,t,r)
-        jac  = partial(kouri_smooth_l1_norm_gradient,t,r)
+        jac  = partial(kouri_smooth_l1_norm_gradient,t,r);
         #hessp = partial(kouri_smooth_l1_norm_hessp,t,r)
         hessp=None
 
-        res = minimize(
-            obj, x0, method=method, jac=jac, hessp=hessp, options=options,
-            constraints=constraints)
-        assert res.status==1 or res.status==2
+        if method=='slsqp':
+            options0 = {'ftol':gtol0,'verbose':max(0,verbose-2),
+                        'maxiter':maxiter_inner,'disp':(verbose>2)}
+        elif method=='trust-constr':
+            options0 = {'gtol':gtol0,'tol':gtol0,'verbose':max(0,verbose-2),
+                        'barrier_tol':ctol,'maxiter':maxiter_inner,
+                        'disp':(verbose>2)}
+        elif method=='cobyla':
+            options0 = {'tol':gtol0,'verbose':max(0,verbose-2),
+                        'maxiter':maxiter_inner,'rhoend':gtol0,'rhobeg':1,
+                        'disp':(verbose>2),'catol':ctol0}
+        if method!='ipopt':
+            #init_guess=x0
+            res = minimize(
+                obj, init_guess,method=method,jac=jac,hessp=hessp,options=options0,
+                constraints=constraints)
+        else:
+            from ipopt import minimize_ipopt
+            options0 = {'tol':gtol0,'print_level':max(0,verbose-1),
+                        'maxiter':int(maxiter_inner),'acceptable_constr_viol_tol':ctol0,
+                        'derivative_test':'first-order'}
+            from scipy.optimize._constraints import new_constraint_to_old
+            con = new_constraint_to_old(constraints[0],init_guess)
+            res = minimize_ipopt(
+                obj, init_guess,method=method,jac=jac,hessp=hessp,options=options0,
+                constraints=con)
+            #assert res.success, res
+        
+        if method=='trust-constr':
+            assert res.status==1 or res.status==2
+        elif method=='slsqp':
+            assert res.status==0
 
-        if abs(prev_obj-res.fun)<xtol:
-            msg = f'homotopy xtol {xtol} reached after {niter} iters.'
-            status=2
-            break
-        if niter >= maxiter:
-            msg = f'maxiter {maxiter} reached. f(x_prev)-f(x)={prev_obj-res.fun}'
-            status=0
-            break
+        fdiff = np.linalg.norm(f0-res.fun)
+        xdiff = np.linalg.norm(x0-res.x)
+        t0=t.copy()
+        t = np.maximum(-1, np.minimum(1, t0 + r*res.x))
+
+        tdiff = np.linalg.norm(t0-t)
+        niter+=1
+        x0 = res.x.copy()
+        f0 = res.fun
+
+        nfev+=res.nfev;
+        if hasattr(res,'njev'):
+            njev+=res.njev;
+        if hasattr(res,'nhev'):
+            nhev+=res.nhev
         
         if verbose>1:
-            print (f'Current objective value {res.fun}')
-        niter+=1
-        x = res.x
-        prev_obj=res.fun
-        t = np.maximum(-1, np.minimum(1, t + r*x))
-        r *= 2
-        x0=x.copy()
-        nfev+=res.nfev;njev+=res.njev;nhev+=res.nhev
-        constr_nfev+=res.constr_nfev[0];constr_njev+=res.constr_njev[0];constr_nhev+=res.constr_nhev[0]
+            #print('  i = %d  tdiff = %11.10e  r = %11.10e  ttol = %3.2e  ctol = %3.2e  gtol = %3.2e  iter = %d'%(niter,tdiff,r,ttol0,ctol0,gtol0,0))
+            print('  i = %d  tdiff = %11.10e  fdiff = %11.10e  xdiff = %11.10e  r = %11.10e  ttol = %3.2e  gtol = %3.2e  nfev = %d'%(niter,tdiff,fdiff,xdiff,r,ttol0,gtol0,nfev))
+
+        if tdiff < ttol:
+            msg = f'ttol {ttol} reached'
+            status=0
+            #break
+
+        if fdiff < ftol:
+            msg = f'ftol {ftol} reached'
+            status=0
+            break
+
+        if niter >= maxiter:
+            msg = f'maxiter {maxiter} reached'
+            status=1
+            break
+
+            
+        if tdiff > ttol0:
+            r = min(r*2,rmax)
+        ttol0,gtol0=max(tfac*ttol0,ttol),max(tfac*gtol0,gtol)
+        ctol0=max(tfac*ctol0,ctol)
+            
+        #constr_nfev only for trust-constr
+        #constr_nfev+=res.constr_nfev[0];constr_njev+=res.constr_njev[0];constr_nhev+=res.constr_nhev[0]
 
     if verbose>0:
         print(msg)
 
-    res = OptimizeResult(fun=res.fun,x=res.x,nit=niter,msg=msg,nfev=nfev,njev=njev,nhev=nhev,constr_nfev=constr_nfev,constr_njev=constr_njev,constr_nhev=constr_nhev,status=status)
+        
+    res = OptimizeResult(fun=res.fun,x=res.x,nit=niter,msg=msg,nfev=nfev,njev=njev,status=status)#constr_nfev=constr_nfev,constr_njev=constr_njev)
     return res
+
+
+    
+    
