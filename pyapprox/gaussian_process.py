implemented correct formula for P needed to compute mean of the standard deviation of a Gaussian process

pyapprox/gaussian_process.py
@@ -108,6 +108,22 @@ def variance_of_variance_gaussian_MMC3(train_samples,delta,mu,sigma):
                 MMC3[mm,nn]*=np.exp(-(-(xm+zn)*(48*si**4*mi*si+20*si**2*di**2*mi+2*di**3*mi))/(denom1*denom2))*np.sqrt(di**2/denom1**2)
     return MMC3
 
+def mean_of_mean_gaussian_P(train_samples,delta,mu,sigma):
+    #haylock has typo. had to derive expression myself
+    nvars,ntrain_samples = train_samples.shape
+    P=np.ones((ntrain_samples,ntrain_samples))
+    for ii in range(nvars):
+        si,mi,di = sigma[ii,0],mu[ii,0],delta[ii,0]
+        denom1 = 4*(di+4*si**2)
+        term2 = np.sqrt(di/(di+4*si**2))
+        for mm in range(ntrain_samples):
+            xm = train_samples[ii,mm]
+            for nn in range(mm,ntrain_samples):
+                xn = train_samples[ii,nn]
+                P[mm,nn]*=np.exp(-1/(2*si**2*di)*(2*si**2*(xm**2+xn**2)+di*mi**2-(4*si**2*(xm+xn)+2*di*mi)**2/denom1))*term2
+                P[nn,mm]=P[mm,nn]
+    return P
+        
 
 def integrate_gaussian_process(gp,variable,return_full=False):
     kernel_types = [RBF,Matern]
@@ -297,16 +313,17 @@ def integrate_gaussian_process_squared_exponential_kernel(X_train,Y_train,K_inv,
     W = U-T.dot(A_inv).dot(T.T)
     variance_random_mean = kernel_var*(W)
 
-    expected_random_var = Y_train.T.dot(A_inv.dot(P).dot(A_inv)).dot(Y_train)+kernel_var*(1-np.trace(A_inv.dot(P)))-expected_random_mean**2-variance_random_mean
+    #[V]
+    V = kernel_var*(1-np.trace(A_inv.dot(P)))
+    #[M^2]
+    M_sq = Y_train.T.dot(A_inv.dot(P).dot(A_inv)).dot(Y_train)
+    
+    expected_random_var = M_sq+V-expected_random_mean**2-variance_random_mean
 
     #[C]
     C=U
     #[M]
     M=expected_random_mean
-    #[M^2]
-    M_sq = Y_train.T.dot(A_inv.dot(P).dot(A_inv)).dot(Y_train)
-    #[V]
-    V = (1-np.trace(A_inv.dot(P)))
     #[MC]
     A = np.linalg.inv(A_inv)
     MC = MC2.T.dot(A.dot(Y_train))#haylock
@@ -352,6 +369,6 @@ def integrate_gaussian_process_squared_exponential_kernel(X_train,Y_train,K_inv,
             variance_random_var
 
     intermeadiate_quantities={'C':C,'M':M,'M_sq':M_sq,'V':V,'MC':MC,'MMC':MMC,
-                              'CC':CC,'C_sq':C_sq,'MC2':MC2,'MMC3':MMC3}
+                              'CC':CC,'C_sq':C_sq,'MC2':MC2,'MMC3':MMC3,'P':P}
     return expected_random_mean, variance_random_mean, expected_random_var,\
             variance_random_var, intermeadiate_quantities

pyapprox/tests/test_gaussian_process.py
@@ -124,6 +124,21 @@ class TestGaussianProcess(unittest.TestCase):
         #print('Expected random var',expected_random_var)
         #assert np.allclose(expected_random_var,true_var,rtol=1e-3)
 
+        xx = np.random.normal(mu_scalar,sigma_scalar,(nvars,10000))
+        from scipy.spatial.distance import cdist
+        dists = cdist(train_samples.T/length_scale,xx.T/length_scale,
+                      metric='sqeuclidean')
+        t = np.exp(-.5*dists)
+        V_mc = (kernel_var - np.diag(t.T.dot(K_inv).dot(t))).mean()
+        print(t.shape)
+        P_mc = (t.dot(t.T))/xx.shape[1]
+
+        P_true = mean_of_mean_gaussian_P(train_samples,delta,mu,sigma)
+        print(P_true,intermediate_quantities['P'],P_mc)
+        assert np.allclose(P_true,intermediate_quantities['P'])
+        
+        V_true = kernel_var*(1-np.trace(A_inv.dot(P_true)))
+
         CC_true = variance_of_variance_gaussian_CC(delta,sigma)
         #print(CC_true,intermediate_quantities['CC'])
         assert np.allclose(CC_true,intermediate_quantities['CC'])
@@ -138,6 +153,12 @@ class TestGaussianProcess(unittest.TestCase):
 
         C_true = variance_of_variance_gaussian_C(delta,sigma)
         assert np.allclose(C_true,intermediate_quantities['C'])
+        assert np.allclose(V_mc,intermediate_quantities['V'],rtol=1e-2)
+
+        M_sq_mc = np.mean((t.T.dot(Kinv_y))**2)
+        print(M_sq_mc,intermediate_quantities['M_sq'])
+        assert np.allclose(M_sq_mc,intermediate_quantities['M_sq'],rtol=2e-2)
+        #assert False
 
         MMC3_true=variance_of_variance_gaussian_MMC3(
             train_samples,delta,mu,sigma)        
@@ -204,8 +225,8 @@ class TestGaussianProcess(unittest.TestCase):
         univariate_variables = [stats.uniform(-1,2)]
         variable=pya.IndependentMultivariateRandomVariable(univariate_variables)
 
-        expected_random_mean, variance_random_mean, expected_random_var=integrate_gaussian_process(
-            gp,variable)
+        expected_random_mean, variance_random_mean, expected_random_var, \
+            variance_random_var=integrate_gaussian_process(gp,variable)
 
         true_mean = 1/3
         true_var = 1/5-1/3**2