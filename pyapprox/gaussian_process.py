fixed bug in pivoted cholesky when initial pivots are specified. Also added pivoted cholesky algorithm for minimizing the trace norm of the error in the low-rank aproximation of a covariance matrix

pyapprox/gaussian_process.py
@@ -528,7 +528,7 @@ def generate_candidate_samples(nvars, num_candidate_samples,
         for v in variables.all_variables():
             lb, ub = v.interval(1)
             if not np.isfinite(lb) or not np.isfinite(ub):
-                lb, ub = variable.interval(1-1e-6)
+                lb, ub = v.interval(1-1e-6)
             marginal_icdfs.append(stats.uniform(lb, ub-lb).ppf)
 
     candidate_samples = transformed_halton_sequence(
@@ -580,10 +580,14 @@ class CholeskySampler(object):
 
     init_pivots : np.ndarray (ninit_pivots)
         The array indices of the candidate_samples to keep
+
+    econ : boolean
+        True - pivot based upon diagonal of schur complement
+        False - pivot to minimize trace norm of low-rank approximation
     """
     def __init__(self, num_vars, num_candidate_samples, variables=None,
                  generate_random_samples=None, init_pivots=None,
-                 nugget = 0):
+                 nugget=0, econ=True):
         self.nvars = num_vars
         self.kernel_theta = None
         self.chol_flag = None
@@ -596,6 +600,7 @@ class CholeskySampler(object):
         self.ntraining_samples = 0
         self.set_init_pivots(init_pivots)
         self.nugget = nugget
+        self.econ = econ
         
     def add_nugget(self):
         self.Kmatrix[np.arange(self.Kmatrix.shape[0]),
@@ -626,6 +631,11 @@ class CholeskySampler(object):
         if not hasattr(self, 'weight_function'):
             raise Exception('Must call set_weight_function')
 
+        if num_samples < self.training_samples.shape[1]:
+            msg = f'Requesting number of samples {num_samples} which is less '
+            msg += 'than number of training samples already generated '
+            msg += f'{self.training_samples.shape[1]}'
+            raise Exception(msg)
         if self.kernel_theta is None:
             assert self.kernel_changed
 
@@ -634,13 +644,20 @@ class CholeskySampler(object):
         if (self.weight_function_changed or self.kernel_changed or
             self.init_pivots_changed):
             self.Kmatrix = self.kernel(self.candidate_samples.T)
-            self.add_nugget()
+            if self.econ is False and self.pivot_weights is not None:
+                weights = np.sqrt(self.pivot_weights)
+                assert np.allclose(np.diag(weights).dot(self.Kmatrix.dot(np.diag(weights))), weights[:, np.newaxis]*self.Kmatrix*weights)
+                self.Kmatrix = weights[:, np.newaxis]*self.Kmatrix*weights
+                self.pivot_weights = None
+                    
+            if self.nugget > 0:
+                self.add_nugget()
             self.L, self.pivots, error, self.chol_flag, self.diag, \
                 self.init_error, self.ntraining_samples = \
                 pivoted_cholesky_decomposition(
                     self.Kmatrix, num_samples, init_pivots=self.init_pivots,
                     pivot_weights=self.pivot_weights,
-                    error_on_small_tol=False, return_full=True)
+                    error_on_small_tol=False, return_full=True, econ=self.econ)
             
             self.weight_function_changed = False
             self.kernel_changed = False
@@ -650,7 +667,7 @@ class CholeskySampler(object):
                 continue_pivoted_cholesky_decomposition(
                     self.Kmatrix, self.L, num_samples, self.init_pivots,
                     0., False, self.pivot_weights, self.pivots, self.diag,
-                    self.ntraining_samples, self.init_error)
+                    self.ntraining_samples, self.init_error, econ=self.econ)
 
         if self.chol_flag == 0:
             assert self.ntraining_samples == num_samples
@@ -708,7 +725,7 @@ class AdaptiveCholeskyGaussianProcessFixedKernel(object):
         return self.train_samples.shape[1]
 
     def condition_number(self):
-        nn = self.sampler.num_completed_pivots
+        nn = self.sampler.ntraining_samples
         chol_factor = self.sampler.L[self.sampler.pivots[:nn], :nn]
         return np.linalg.cond(chol_factor.dot(chol_factor.T))
 
@@ -1086,7 +1103,7 @@ class IVARSampler(object):
     def __init__(self, num_vars, nquad_samples,
                  ncandidate_samples, generate_random_samples, variables=None,
                  greedy_method='ivar', use_gauss_quadrature=False,
-                 nugget=0, kernels_1d=None):
+                 nugget=0):
         self.nvars = num_vars
         self.nquad_samples = nquad_samples
         self.greedy_method = greedy_method
@@ -1101,7 +1118,6 @@ class IVARSampler(object):
         self.nsamples_requested = []
         self.set_optimization_options(
             {'gtol':1e-8, 'ftol':0, 'disp':False, 'iprint':0})
-        self.kernels_1d = kernels_1d
         self.initialize_greedy_sampler()
 
         if use_gauss_quadrature:
@@ -1117,14 +1133,13 @@ class IVARSampler(object):
         if self.greedy_method == 'chol':
             self.greedy_sampler = CholeskySampler(
                 self.nvars, self.ncandidate_samples, self.variables,
-                generate_random_samples=self.generate_random_samples,
-                kernels_1d=self.kernels_1d)
+                generate_random_samples=self.generate_random_samples)
         elif self.greedy_method == 'ivar':
             self.greedy_sampler = GreedyIntegratedVarianceSampler(
                 self.nvars, self.nquad_samples, self.ncandidate_samples,
                 self.generate_random_samples, self.variables,
                 use_gauss_quadrature=self.use_gauss_quadrature, econ=True,
-                nugget=self.nugget, kernels_1d=self.kernels_1d)
+                nugget=self.nugget)
         else:
             msg = f'Incorrect greedy_method {greedy_method}'
             raise Exception(msg)
@@ -1213,16 +1228,17 @@ class IVARSampler(object):
     def set_weight_function(self, weight_function):
         self.greedy_sampler.set_weight_function(weight_function)
         
-    def set_kernel(self, kernel):
-        if (not type(kernel) == RBF and not
-            (type(kernel) == Matern and not np.isfinite(kernel.nu))):
-            # TODO: To deal with sum kernel with noise, need to ammend gradient
-            # computation which currently assumes no noise
+    def set_kernel(self, kernel, kernels_1d=None):
+        if ((self.use_gauss_quadrature is True) and (self.nvars != 1) and
+            ((type(kernel) != Matern) or (np.isfinite(kernel.nu)))):
+            # TODO: To deal with sum kernel with noise, need to ammend
+            # gradient computation which currently assumes no noise
             msg = f'GP Kernel type: {type(kernel)} '
-            msg += 'Only squared exponential kernel supported'
+            msg += 'Only squared exponential kernel supported when '
+            msg += 'use_gauss_quadrature is True and nvars > 1'
+            #TODO add other tensor product kernels
             raise Exception(msg)
-
-        self.greedy_sampler.set_kernel(copy.deepcopy(kernel))
+        self.greedy_sampler.set_kernel(copy.deepcopy(kernel), kernels_1d)
 
     def set_optimization_options(self, opts):
         self.optim_opts = opts.copy()
@@ -1360,7 +1376,7 @@ class GreedyVarianceOfMeanSampler(object):
     def __init__(self, num_vars, nquad_samples,
                  ncandidate_samples, generate_random_samples, variables=None,
                  use_gauss_quadrature=False, econ=True,
-                 compute_cond_nums=False, nugget=0, kernels_1d=None):
+                 compute_cond_nums=False, nugget=0):
         self.nvars = num_vars
         self.nquad_samples = nquad_samples
         self.variables = variables
@@ -1382,10 +1398,6 @@ class GreedyVarianceOfMeanSampler(object):
         self.initialize()
         self.best_obj_vals = []
         self.pred_samples = None
-
-        self.kernels_1d = kernels_1d
-        if self.kernels_1d is None:
-            self.kernels_1d = [partial(matern_kernel_1d,np.inf)]*self.nvars
         
     def initialize(self):
         self.L = np.zeros((0, 0))
@@ -1407,6 +1419,15 @@ class GreedyVarianceOfMeanSampler(object):
             self.nquad_samples)
         k = self.kernel(self.pred_samples.T, self.candidate_samples.T)
         self.tau = k.mean(axis=0)
+        assert self.tau.shape[0] == self.candidate_samples.shape[1]
+
+        # Note because tau is simplified down to one integral instead of their
+        # double used for u, it is possible for self.u - tau.dot(A_inv.dot(tau)
+        # to be negative if tau is comptued using an inaccurate quadrature rule.
+        # This is not important if using gauss quadrature
+        #pred_samples2 = self.generate_random_samples(self.pred_samples.shape[1])
+        #self.u = np.diag(
+        #    self.kernel(self.pred_samples.T, pred_samples2.T)).mean()
 
     def get_univariate_quadrature_rule(self, ii):
         xx_1d, ww_1d = self.univariate_quad_rules[ii](self.degrees[ii]+1)
@@ -1457,6 +1478,14 @@ class GreedyVarianceOfMeanSampler(object):
         for mm in range(self.candidate_samples.shape[1]):
             if mm not in self.pivots:
                 obj_vals[mm] = self.objective(mm)
+        # assert np.allclose(self.candidate_samples[:,self.pivots],self.training_samples)
+        # if len(self.pivots)>22:
+        #     I = np.argsort(self.candidate_samples[0,:])
+        #     plt.plot(self.candidate_samples[0,self.pivots],np.ones((len(self.pivots)))*obj_vals.min(),'ko')
+        #     plt.plot(self.candidate_samples[0,I],obj_vals[I])
+        #     J = np.argmin(obj_vals)
+        #     plt.plot(self.candidate_samples[0,J],obj_vals[J], 'rs')
+        #     plt.show()
         return obj_vals
     
     def refine_naive(self):
@@ -1568,15 +1597,25 @@ class GreedyVarianceOfMeanSampler(object):
                     solve_triangular(self.L.T, L_12*z_2, lower=False)))
         return val[0,0]
     
-    def set_kernel(self, kernel):
-        if (not type(kernel) == RBF and not
-            (type(kernel) == Matern and not np.isfinite(kernel.nu))):
-            # TODO: To deal with sum kernel with noise, need to ammend gradient
-            # computation which currently assumes no noise
-            msg = f'GP Kernel type: {type(kernel)} '
-            msg += 'Only squared exponential kernel supported'
-            raise Exception(msg)
+    def set_kernel(self, kernel, kernels_1d=None):
         self.kernel = kernel
+
+        self.kernels_1d = kernels_1d
+        if self.kernels_1d is None and self.use_gauss_quadrature:
+            #TODO: remove kernels 1D and just create tensor product
+            # kernel with this as a property.
+            assert self.kernel.nu == np.inf
+            self.kernels_1d = [partial(matern_kernel_1d,np.inf)]*self.nvars
+
+        if ((self.use_gauss_quadrature is True) and (self.nvars != 1) and
+            ((type(kernel) != Matern) or (np.isfinite(kernel.nu)))):
+            # TODO: To deal with sum kernel with noise, need to ammend
+            # gradient computation which currently assumes no noise
+            msg = f'GP Kernel type: {type(kernel)} '
+            msg += 'Only squared exponential kernel supported when '
+            msg += 'use_gauss_quadrature is True and nvars > 1'
+            #TODO add other tensor product kernels
+            raise Exception(msg)
 
         self.active_candidates = np.ones(
             self.candidate_samples.shape[1],dtype=bool)
@@ -1591,12 +1630,13 @@ class GreedyVarianceOfMeanSampler(object):
     def add_nugget(self):
         self.A[np.arange(self.A.shape[0]), np.arange(self.A.shape[1])] += \
             self.nugget
+        print(self.nugget)
 
     def set_init_pivots(self, init_pivots):
         assert len(self.pivots)==0
         self.init_pivots = list(init_pivots)
 
-    def __call__(self, nsamples):
+    def __call__(self, nsamples, verbosity=1):
         if not hasattr(self, 'kernel'):
             raise Exception('Must call set_kernel')
         if self.econ is True:
@@ -1607,22 +1647,27 @@ class GreedyVarianceOfMeanSampler(object):
         self.nsamples_requested.append(nsamples)
         ntraining_samples = self.ntraining_samples
         for nn in range(ntraining_samples, nsamples):
-            print('Iter', nn)
             pivot, obj_val = self.refine()
-            self.best_obj_vals.append(obj_val)
-            print(1+self.best_obj_vals[-1])
 
             if pivot < 0:
-                if self.econ is False:
-                    flag = 1
-                    break
-                else:
-                    self.econ = False
-                    # Switch of econ mode which struggles when condition number
-                    # is poor
-                    print('switching naive updating strategy on')
-                    self.refine = self.refine_naive
-                    pivot = self.refine()
+                flag = 1
+                break
+                # if self.econ is False:
+                #     flag = 1
+                #     break
+                # else:
+                #     self.econ = False
+                #     # Switch of econ mode which struggles when condition number
+                #     # is poor
+                #     print('switching naive updating strategy on')
+                #     self.refine = self.refine_naive
+                #     pivot, obj_val = self.refine()
+                #     if pivot < 0:
+                #         flag = 1
+                #         break
+            if verbosity>0:
+                print(f'Iter: {nn}, Objective: {obj_val}')
+            self.best_obj_vals.append(obj_val)
             
             self.pivots.append(pivot)
             new_sample = self.candidate_samples[:, pivot:pivot+1]
@@ -1638,6 +1683,8 @@ class GreedyVarianceOfMeanSampler(object):
                     self.cond_nums.append(
                         np.linalg.cond(
                             self.A[np.ix_(self.pivots, self.pivots)]))
+            print(np.linalg.cond(
+                self.A[np.ix_(self.pivots, self.pivots)]))
 
         new_samples = self.training_samples[:,ntraining_samples:]
         self.ntraining_samples = self.training_samples.shape[1]
@@ -1727,6 +1774,18 @@ class GreedyIntegratedVarianceSampler(GreedyVarianceOfMeanSampler):
         A = self.A[np.ix_(indices, indices)]
         A_inv = np.linalg.inv(A)
         P = self.P[np.ix_(indices,indices)]
+        # P1=1
+        # length_scale = self.kernel.length_scale
+        # if np.isscalar(length_scale):
+        #     length_scale = np.array([length_scale]*self.nvars)
+        # for ii in range(self.nvars):
+        #     xx_1d, ww_1d = self.get_univariate_quadrature_rule(ii)
+        #     xtr = self.candidate_samples[ii:ii+1, indices]
+        #     K = self.kernels_1d[ii](xx_1d[np.newaxis, :], xtr, length_scale[ii])
+        #     P_ii = K.T.dot(ww_1d[:, np.newaxis]*K)
+        #     P1*=P_ii
+        # assert np.allclose(P, P1)
+        
         return -np.trace(A_inv.dot(P))
 
     def objective_econ(self, new_sample_index):
@@ -1783,7 +1842,6 @@ class GreedyIntegratedVarianceSampler(GreedyVarianceOfMeanSampler):
         return vals
 
     def refine_econ(self):
-        print('a')
         if (self.init_pivots is not None and
             len(self.pivots) < len(self.init_pivots)):
             pivot = self.init_pivots[len(self.pivots)]
@@ -1791,11 +1849,13 @@ class GreedyIntegratedVarianceSampler(GreedyVarianceOfMeanSampler):
         else:
             training_samples = self.ntraining_samples
             obj_vals = self.vectorized_objective_vals_econ()
-            #obj_vals = self.objective_vals_econ()
+            # obj_vals = self.objective_vals_econ()
             pivot = np.argmin(obj_vals)
             obj_val = obj_vals[pivot]
 
-        if not np.isfinite(obj_val):
+        if not np.isfinite(obj_val):# or obj_val < -1:
+            # ill conditioning causes obj_val to go below -1 which should not
+            # be possible
             return -1, np.inf
         
 

pyapprox/tests/test_utilities.py
@@ -545,49 +545,51 @@ class TestUtilities(unittest.TestCase):
 
     def test_pivoted_cholesky_decomposition(self):
         nrows, npivots = 4, 4
-        A = np.random.normal(0.,1.,(nrows,nrows))
+        A = np.random.normal(0., 1., (nrows, nrows))
         A = A.T.dot(A)
-        L, pivots, error, flag = pivoted_cholesky_decomposition(A,npivots)
-        assert np.allclose(L.dot(L.T),A)
+        L, pivots, error, flag = pivoted_cholesky_decomposition(A, npivots)
+        assert np.allclose(L.dot(L.T), A)
 
         nrows, npivots = 4, 2
-        A = np.random.normal(0.,1.,(npivots,nrows))
+        A = np.random.normal(0., 1., (npivots, nrows))
         A = A.T.dot(A)
-        L, pivots, error, flag = pivoted_cholesky_decomposition(A,npivots)
-        assert L.shape == (nrows,npivots)
-        assert pivots.shape[0]==npivots
-        assert np.allclose(L.dot(L.T),A)
+        L, pivots, error, flag = pivoted_cholesky_decomposition(A, npivots)
+        assert L.shape == (nrows, npivots)
+        assert pivots.shape[0] == npivots
+        assert np.allclose(L.dot(L.T), A)
 
         # check init_pivots are enforced
         nrows, npivots = 4, 2
-        A = np.random.normal(0.,1.,(npivots+1,nrows))
+        A = np.random.normal(0., 1., (npivots+1, nrows))
         A = A.T.dot(A)
-        L, pivots, error, flag = pivoted_cholesky_decomposition(A,npivots+1)
+        L, pivots, error, flag = pivoted_cholesky_decomposition(A, npivots+1)
         L, new_pivots, error, flag = pivoted_cholesky_decomposition(
-            A,npivots+1,init_pivots=pivots[1:2])
-        assert np.allclose(new_pivots[:npivots+1],pivots[[1,0,2]])
+            A,npivots+1, init_pivots=pivots[1:2])
+        assert np.allclose(new_pivots[:npivots+1], pivots[[1,0,2]])
 
-        L = L[pivots,:]
-        assert np.allclose(A[pivots,:][:,pivots],L.dot(L.T))
+        L = L[pivots, :]
+        assert np.allclose(A[pivots, :][:, pivots], L.dot(L.T))
+
+        assert np.allclose(A[np.ix_(pivots, pivots)], L.dot(L.T))
 
         P = get_pivot_matrix_from_vector(pivots,nrows)
-        assert np.allclose(P.dot(A).dot(P.T),L.dot(L.T))
+        assert np.allclose(P.dot(A).dot(P.T), L.dot(L.T))
 
-        A = np.array([[4,12,-16],[12,37,-43],[-16,-43,98.]])
-        L, pivots, error, flag = pivoted_cholesky_decomposition(A,A.shape[0])
+        A = np.array([[4, 12, -16], [12, 37 ,-43], [-16, -43, 98.]])
+        L, pivots, error, flag = pivoted_cholesky_decomposition(A, A.shape[0])
 
         # reorder entries of A so that cholesky requires pivoting
-        true_pivots = np.array([2,1,0])
-        A_no_pivots = A[true_pivots,:][:,true_pivots]
+        true_pivots = np.array([2, 1, 0])
+        A_no_pivots = A[true_pivots, :][:, true_pivots]
         L_np = np.linalg.cholesky(A_no_pivots)
-        assert np.allclose(L[pivots,:],L_np)
+        assert np.allclose(L[pivots, :], L_np)
 
         # Create A with which needs cholesky with certain pivots
-        A = np.array([[4,12,-16],[12,37,-43],[-16,-43,98.]])
-        true_pivots = np.array([1,0,2])
-        A = A[true_pivots,:][:,true_pivots]
-        L, pivots, error, flag = pivoted_cholesky_decomposition(A,A.shape[0])
-        assert np.allclose(L[pivots,:],L_np)
+        A = np.array([[4, 12, -16], [12, 37, -43], [-16, -43, 98.]])
+        true_pivots = np.array([1, 0, 2])
+        A = A[true_pivots, :][:, true_pivots]
+        L, pivots, error, flag = pivoted_cholesky_decomposition(A, A.shape[0])
+        assert np.allclose(L[pivots, :], L_np)
 
     def test_restart_pivoted_cholesky(self):
         nrows = 10
@@ -677,6 +679,124 @@ class TestUtilities(unittest.TestCase):
         assert np.allclose(
             z_up_1,
             z_1 - solve_triangular(L_11.T, L_12.dot(z_up_2), lower=False))
+
+    def test_cholesky_decomposition_minimizing_trace_norm(self):
+        """
+        Test how to compute pivot that minimizes trace norm
+        """
+        n = 6
+        B = np.random.normal(0, 1, (n, n))
+        A = B.T.dot(B)
+        # pivots = [1,2,0] causes issues with pya.pivoted_cholesky_decomposition
+
+        if n == 4:
+            a1 = A[2, 2]
+            b1 = np.array([[A[1, 2], A[0, 2], A[3, 2]]]).T
+            C1 = np.array([[A[1, 1], A[1, 0], A[1, 3]],
+                           [A[0, 1], A[0, 0], A[0, 3]],
+                           [A[3, 1], A[3, 0], A[3, 3]]])
+            S1 = C1 - b1.dot(b1.T)/a1
+            L1 = np.zeros((n, n))
+            pivots1 = np.array([2,1,0,3])
+            L1[pivots1, 0] = A[pivots1[0], :]/np.sqrt(a1)
+            assert np.allclose(A[np.ix_(pivots1, pivots1)][1:, 1:], C1)
+            assert np.allclose(L1[:1, :1].dot(L1[:1, :1].T), A[2, 2])
+
+            raw_pivots2 = np.array([1, 0, 2]) # choose first remaining pivot
+            S2 = S1[np.ix_(raw_pivots2, raw_pivots2)]
+            a2 = S2[0, 0]
+            b2 = S2[1:, 0:1]
+            C2 = S2[1:, 1:]
+            S2 = C2 - b2.dot(b2.T)/a2
+            L2 = L1.copy()
+            swap_rows(L2, 1, raw_pivots2[0]+1)
+            L2[1:, 1:2] = np.vstack([[[a2]],b2])/np.sqrt(a2)
+            pivots = np.hstack([pivots1[0], pivots1[1:][raw_pivots2]])
+            assert np.allclose(
+                L2.dot(L2.T)[:2, :2], A[np.ix_(pivots[:2], pivots[:2])])
+
+            a_list = [a1, a2]
+            b_list = [b1, b2]
+            C_list = [C1, C2]
+            S_list = [S1, S2]
+
+        trace_A = np.trace(A)
+        S = A.copy()
+        lvecs = np.zeros(A.shape)
+        Smats = []
+        traces = [0]
+        pivots = np.arange(n)
+        #use_pivoting = False
+        use_pivoting = True
+        for ii in range(n):
+            # Given a new l vector we have
+            # A_ii = [L  l ] [L.T] = L.dot(L.T)+l.dot(l.T)
+            #                [l.T]
+            # Thus trace(A_ii) = trace(L.dot(L.T)+l.dot(l.T))
+            #                  = trace(L.dot(L.T))+trace(l.dot(l.T))
+
+            pivot_vals = np.linalg.norm(S[:, :], axis=0)**2/np.diag(S)
+            if use_pivoting is True:
+                raw_pivot = np.argmax(pivot_vals)
+            else:
+                raw_pivot = 0  # do not pivot
+
+            traces.append(traces[-1]+pivot_vals[raw_pivot])
+            pivot = raw_pivot+ii
+            S_pivots = np.arange(S.shape[0])
+            swap_rows(S_pivots, 0, raw_pivot)
+            S = S[np.ix_(S_pivots, S_pivots)]
+
+            a = S[0, 0]
+            indices = np.arange(1, S.shape[0])
+            b = S[1:, 0:1].copy()
+            C = S[1:, 1:].copy()
+            nonzero_l = np.vstack([[[a]], b])/np.sqrt(a)
+            swap_rows(lvecs, ii, pivot)
+            lvecs[ii:, ii:ii+1] = nonzero_l
+            swap_rows(pivots, ii, pivot)
+
+            L_ii = lvecs[:, :ii+1]
+            trace_S = trace_A - (
+                np.trace(L_ii[:, :-1].dot(L_ii[:, :-1].T))+
+                np.linalg.norm(S[:, 0])**2/S[0, 0])
+
+            S = C-1/a*(b.dot(b.T))
+            if ii < 2 and n == 4:
+                assert np.allclose(C, C_list[ii])
+                assert np.allclose(b, b_list[ii])
+                assert np.allclose(a, a_list[ii])
+                assert np.allclose(S, S_list[ii])
+                assert np.allclose(S, S.T)
+            Smats.append(S)
+            A_ii = L_ii.dot(L_ii.T)
+            assert np.allclose(
+                A[np.ix_(pivots[:ii+1], pivots[:ii+1])], A_ii[:ii+1, :ii+1])
+            assert np.allclose(
+                (A[np.ix_(pivots, pivots)]-A_ii)[ii+1:, ii+1:], S)
+            assert np.allclose(
+                np.trace(A), np.trace(A_ii)+np.trace(S))
+            assert np.allclose(
+                np.trace(S), np.trace(A)-np.trace(A_ii))
+            assert np.allclose(trace_S, np.trace(S))
+            assert np.allclose(trace_A-traces[-1], np.trace(S))
+
+        L_chol = np.linalg.cholesky(A[np.ix_(pivots, pivots)])
+        assert np.allclose(L_chol, lvecs)
+
+        L1, pivots1, error1, flag1 = pivoted_cholesky_decomposition(
+            A, A.shape[0], econ=True, init_pivots=pivots)
+        assert np.allclose(L_chol, L1[pivots])
+
+        if use_pivoting is False:
+            # This check only good if pivoting is not enforced
+            for ii in range(n):
+                E = A-L_chol[:, :ii+1].dot(L_chol[:, :ii+1].T)
+                assert np.allclose(E[ii+1:, ii+1:], Smats[ii])
+        L2, pivots2, error2, flag2 = pivoted_cholesky_decomposition(
+            A, A.shape[0], econ=False)
+        assert np.allclose(L2[pivots2], L_chol)
+        assert np.allclose(pivots1, pivots2)
 
     def test_beta_pdf_on_ab(self):
         from scipy.stats import beta as beta_rv
@@ -803,20 +923,20 @@ class TestUtilities(unittest.TestCase):
         nvars,nsamples = 3,10
         A = np.random.normal(0,1,nvars)
         A = A.T.dot(A)
-        samples = np.random.uniform(0,1,(nvars,nsamples))
-        values1 = evaluate_quadratic_form(A,samples)
+        samples = np.random.uniform(0, 1, (nvars,nsamples))
+        values1 = evaluate_quadratic_form(A, samples)
 
         values2 = np.zeros(samples.shape[1])
         for ii in range(samples.shape[1]):
-            values2[ii] = samples[:,ii:ii+1].T.dot(A).dot(samples[:,ii:ii+1])
+            values2[ii] = samples[:, ii:ii+1].T.dot(A).dot(samples[:, ii:ii+1])
 
         assert np.allclose(values1,values2)
 
     def test_weighted_pivoted_cholesky(self):
         nrows, npivots = 4, 3
-        A = np.random.normal(0.,1.,(nrows,nrows))
+        A = np.random.normal(0., 1., (nrows, nrows))
         A = A.T.dot(A)
-        weights = np.random.uniform(1,2,(nrows))
+        weights = np.random.uniform(1, 2, (nrows))
         L, pivots, error, flag = pivoted_cholesky_decomposition(
             A,npivots,pivot_weights=weights)
 
@@ -824,17 +944,17 @@ class TestUtilities(unittest.TestCase):
         C = np.sqrt(weights)[:,np.newaxis]*A*np.sqrt(weights)
         assert np.allclose(B,C)
         L2, pivots2, error2, flag2 = pivoted_cholesky_decomposition(
-            C,npivots,pivot_weights=None)
+            C, npivots, pivot_weights=None)
 
         # check pivots are the same
-        assert np.allclose(pivots,pivots2)
+        assert np.allclose(pivots, pivots2)
 
         # check cholesky factors are the same
         #we have L2.dot(L2.T)=S.dot(A).dot(S)= S.dot(L.dot(L.T)).dot(S)
         #where S = np.diag(np.sqrt(weights)). So L2=S.dot(L)
         assert np.allclose(
-            np.sqrt(weights[pivots,np.newaxis])*L[pivots,:npivots],
-            L2[pivots,:npivots])
+            np.sqrt(weights[pivots, np.newaxis])*L[pivots, :npivots],
+            L2[pivots, :npivots])
 
     def cholesky_qr_pivoting_equivalence(self):
         nrows, npivots = 4, 4

pyapprox/utilities.py
@@ -1102,7 +1102,8 @@ def cholesky_decomposition(Amat):
 def pivoted_cholesky_decomposition(A,npivots,init_pivots=None,tol=0.,
                                    error_on_small_tol=False,
                                    pivot_weights=None,
-                                   return_full=False):
+                                   return_full=False,
+                                   econ=True):
     r"""
     Return a low-rank pivoted Cholesky decomposition of matrix A.
 
@@ -1134,7 +1135,7 @@ def pivoted_cholesky_decomposition(A,npivots,init_pivots=None,tol=0.,
             Amat, L, npivots, init_pivots, tol,
             error_on_small_tol,
             pivot_weights, pivots, diag,
-            0, init_error)
+            0, init_error, econ)
 
     if not return_full:
         return L[:,:ncompleted_pivots], pivots[:ncompleted_pivots], error,\
@@ -1147,41 +1148,56 @@ def pivoted_cholesky_decomposition(A,npivots,init_pivots=None,tol=0.,
 def continue_pivoted_cholesky_decomposition(Amat, L, npivots, init_pivots, tol,
                                             error_on_small_tol,
                                             pivot_weights, pivots, diag,
-                                            ncompleted_pivots, init_error):
+                                            ncompleted_pivots, init_error,
+                                            econ):
+    Amat = Amat.copy()  # Do not overwrite incoming Amat
+    if econ is False and pivot_weights is not None:
+        msg = 'pivot weights not used when econ is False' 
+        raise Exception(msg)
     chol_flag = 0
-    assert ncompleted_pivots<npivots
-    for ii in range(ncompleted_pivots,npivots):
-        if init_pivots is None or ii>=len(init_pivots):
-            if pivot_weights is None:
-                pivot = np.argmax(diag[pivots[ii:]])+ii
+    assert ncompleted_pivots < npivots
+    for ii in range(ncompleted_pivots, npivots):
+        if init_pivots is None or ii >= len(init_pivots):
+            if econ:
+                if pivot_weights is None:
+                    pivot = np.argmax(diag[pivots[ii:]])+ii
+                else:
+                    pivot = np.argmax(
+                        pivot_weights[pivots[ii:]]*diag[pivots[ii:]])+ii
             else:
+                schur_complement = (
+                    Amat[np.ix_(pivots[ii:], pivots[ii:])]-
+                    L[pivots[ii:], :ii].dot(L[pivots[ii:], :ii].T))
+                schur_diag = np.diagonal(schur_complement)
                 pivot = np.argmax(
-                    pivot_weights[pivots[ii:]]*diag[pivots[ii:]])+ii
+                    np.linalg.norm(schur_complement, axis=0)**2/schur_diag)
+                pivot += ii
         else:
-            pivot = pivots[init_pivots[ii]]
+            pivot = np.where(pivots==init_pivots[ii])[0][0]
+            assert pivot >= ii
             
-        swap_rows(pivots,ii,pivot)
+        swap_rows(pivots, ii, pivot)
         if diag[pivots[ii]] <= 0:
             msg = 'matrix is not positive definite'
             if error_on_small_tol:
                 raise Exception (msg)
             else:
                 print(msg)
-                chol_flag=1
+                chol_flag = 1
                 break
 
         L[pivots[ii],ii] = np.sqrt(diag[pivots[ii]])
 
-        L[pivots[ii+1:],ii]=(Amat[pivots[ii+1:],pivots[ii]]-
-            L[pivots[ii+1:],:ii].dot(L[pivots[ii],:ii]))/L[pivots[ii],ii]
-        diag[pivots[ii+1:]] -= L[pivots[ii+1:],ii]**2
+        L[pivots[ii+1:], ii]=(Amat[pivots[ii+1:], pivots[ii]]-
+            L[pivots[ii+1:], :ii].dot(L[pivots[ii], :ii]))/L[pivots[ii], ii]
+        diag[pivots[ii+1:]] -= L[pivots[ii+1:], ii]**2
 
         # for jj in range(ii+1,nrows):
         #     L[pivots[jj],ii]=(Amat[pivots[ii],pivots[jj]]-
         #         L[pivots[ii],:ii].dot(L[pivots[jj],:ii]))/L[pivots[ii],ii]
         #     diag[pivots[jj]] -= L[pivots[jj],ii]**2
         error = diag[pivots[ii+1:]].sum()/init_error
-        #print(ii,'error',error)
+        # print(ii,'error',error)
         if error<tol:
             msg = 'Tolerance reached. ' 
             msg += f'Iteration:{ii}. Tol={tol}. Error={error}'