08db263be7813e986a78dcafdb9474cf0e7bd35e
ConnectedSystems <takuyai@gmail.com>
2020-12-22 12:56:28 +1100

    Change numpy version and linting
    NumPy currently has issues on windows platform:
    
    https://developercommunity.visualstudio.com/content/problem/1207405/fmod-after-an-update-to-windows-2004-is-causing-a.html

--- setup.py
+++ setup.py
@@ -26,18 +26,18 @@ def no_cythonize(extensions, **_ignore):
         extension.sources[:] = sources
     return extensions
 
-#try:
-from Cython.Build import cythonize
-USE_CYTHON=True
+
+# try:
+USE_CYTHON = True
 print('CYTHONIZING')
-#except:
+# except:
 #    USE_CYTHON=False
 #    print('USING PRECYTHONIZED FILES')
 
-#if USE_CYTHON:
+# if USE_CYTHON:
 extensions = cythonize(
     "pyapprox/cython/*.pyx",
-    compiler_directives={'language_level' : 3},
+    compiler_directives={'language_level': 3},
     annotate=True)
 # else:
 #     import glob
@@ -46,7 +46,7 @@ extensions = cythonize(
 #     extensions = []
 #     for pyx_file in pyx_files:
 #         name= pyx_file[:-4].replace('/', '.')
-#         ext = Extension(name=name,sources=[pyx_file],include_dirs=[np.get_include()])
+#         ext = Extension(name=name,sources=[pyx_file])
 #         extensions.append(ext)
 #     extensions = no_cythonize(extensions)
 
@@ -69,29 +69,16 @@ setuptools.setup(
     include_dirs=[np.get_include()],
     setup_requires=['numpy==1.19.3', 'Cython', 'scipy >= 1.0.0'],
     install_requires=requirements,
-        'numpy >= 1.16.4',
-        'matplotlib',
-        'scipy >= 1.0.0',
-        'Cython',
-        'sympy',
-        'seaborn',
-        'pymc3',
-        'numba',
-        'scikit-learn',
-        'pytest-cov',
-        'pytest',
-        'networkx',
-        ],
-    extras_require={'docs':['numpydoc','sphinx','sphinx_automodapi','sphinx_rtd_theme',
-                            'sphinx-gallery','jupyter']
-    #                'mfnets':['MFNetsSurrogates @ git+https://github.com/goroda/MFNetsSurrogates@master']
-      },
-    ext_modules = extensions,
+    extras_require={'docs': ['numpydoc', 'sphinx', 'sphinx_automodapi', 'sphinx_rtd_theme',
+                             'sphinx-gallery', 'jupyter']
+                    #                'mfnets':['MFNetsSurrogates @ git+https://github.com/goroda/MFNetsSurrogates@master']
+                    },
+    ext_modules=extensions,
     test_suite='nose.collector',
     tests_require=['nose'],
-    #for some reason pytest will not work on github actions. It is discovering a test which
-    #causes an error which I cannot reproduce on my machine or find a way which test
-    #is causing the issue
-    #tests_require=['pytest-cov'],
+    # for some reason pytest will not work on github actions. It is discovering a test which
+    # causes an error which I cannot reproduce on my machine or find a way which test
+    # is causing the issue
+    # tests_require=['pytest-cov'],
     license='MIT',
 )
