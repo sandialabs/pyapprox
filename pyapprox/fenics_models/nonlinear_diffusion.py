17c1956ef1d2569b9c45436e85922b17051e2994
jdjakem <29109026+jdjakem@users.noreply.github.com>
2020-12-30 16:21:45 -0700

    in the process of adding wrapper of nonlinear diffusion for modelig halvar ice-sheet analytical solution

--- pyapprox/fenics_models/nonlinear_diffusion.py
+++ pyapprox/fenics_models/nonlinear_diffusion.py
@@ -1,23 +1,11 @@
 import numpy as np
 from pyapprox.fenics_models.fenics_utilities import *
 
-def extract_options(options):
-    forcing = options.get("forcing", dla.Constant(0))
-    nonlinear_diffusion = options.get('nonlinear_diffusion', None)
-    second_order_timestepping = options.get('second_order_timestepping', True)
-    nlsparam = options.get('nlsparam', dict())
-    positivity_tol = options.get('positivity_tol', 0.0)
-    return options['time_step'], options['final_time'],\
-        forcing, options['init_condition'],\
-        options['boundary_conditions'], nonlinear_diffusion,\
-        second_order_timestepping, nlsparam, positivity_tol
 
+def run_model(function_space, time_step, final_time, forcing, boundary_conditions, init_condition, 
+              nonlinear_diffusion, second_order_timestepping=False, nlsparam=dict(), positivity_tol=0):
 
-def run_model(function_space, options):
-    dt, final_time, forcing, init_condition, boundary_conditions, \
-        nonlinear_diffusion, second_order_timestepping, nlsparam, \
-        positivity_tol = extract_options(options)
-
+    dt = time_step
     mesh = function_space.mesh()
     num_bndrys = len(boundary_conditions)
 
@@ -134,3 +122,242 @@ def run_model(function_space, options):
         u_1.assign(u_2)
 
     return u_1
+
+
+def compute_gamma(A, n=3):
+    """n = 3 # GlenExponent"""
+    g = 9.81 #GravityAcceleration
+    rho = 910 #IceDensity
+    gamma = 2.0*A*(rho*g)**n/(n+2)
+    return gamma
+
+
+def var_form_max(a, b): return (a+b+abs(a-b))/dl.Constant(2)
+
+
+def shallow_ice_diffusion(n, gamma, bed,positivity_tol, beta, thickness):
+    r"""
+    Nonlinear diffusivity function of the shallow ice equation
+
+    dH/dt -div(Gamma * H**(n+2) * |\nabla h|**(n-1) \nabla h) = f
+
+    n: integer
+        Glen exponent
+
+    gamma : float
+        Diffusivity strength
+
+    bed : Fenics Function
+        Bed elevation
+
+    thickness : Fenics Function
+        Thickness H of the ice-sheet
+    """
+    height = thickness
+    if bed is not None:
+        height += bed
+
+    var_form = gamma*thickness**(n+2)*dl.inner(
+        dl.grad(height),dl.grad(height))**((n-1)/2)
+    #var_form = gamma*thickness**(n+2)*(dl.inner(    
+    #    dl.grad(height),dl.grad(height))+positivity_tol)**((n-1)/2)
+
+    if beta is not None:
+        g = 9.81 #GravityAcceleration
+        rho = 910 #IceDensity
+        var_form += rho*g/2*height**2/dla.exp(beta)
+
+    # var_form = var_form_max(var_form,positivity_tol) # doesnt work well
+    var_form += positivity_tol
+    return var_form
+
+
+from pyapprox.fenics_models.advection_diffusion_wrappers import AdvectionDiffusionModel
+class HalfarShallowIceModel(AdvectionDiffusionModel):
+
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+    # Change the following functions to modify governing equations
+    #     initialize_random_expressions()
+    #     get_initial_condition()
+    #     get_boundary_conditions_and_function_space()
+    #     get_velocity()
+    #     get_forcing()
+    #     get_diffusivity()
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+    def set_constants(self):
+        self.secpera = 31556926  # seconds per anum
+        self.Gamma = 2.8457136065980445e-05
+        self.glen_exponent = 3
+        self.Lx = 1200e3
+        self.bed = None
+        self.beta = None
+
+        self.positivity_tol = 0
+
+    def initialize_random_expressions(self, random_sample):
+        r"""
+        Overide this class to split random_samples into the parts that effect
+        the 5 random quantities
+        """
+        self.set_constants()
+        init_condition = self.get_initial_condition(None)
+        boundary_conditions, function_space = \
+            self.get_boundary_conditions_and_function_space(None)
+        beta = self.get_velocity(None)
+        forcing = self.get_forcing(None)
+        kappa = self.get_diffusivity(random_sample)
+        return init_condition, boundary_conditions, function_space, beta, \
+            forcing, kappa
+
+    def get_initial_condition(self, random_sample):
+        r"""By Default the initial condition is deterministic and set to zero"""
+        assert random_sample is None
+        init_solution = get_shallow_ice_exact_solution(
+            self.Gamma, self.mesh, self.degree, self.nphys_dim)
+        init_solution.t = self.init_time
+        return initial_condition
+
+    def get_boundary_conditions_and_function_space(self, random_sample):
+        r"""By Default the boundary conditions are deterministic, Dirichlet and 
+           and set to zero"""
+        assert random_sample is None
+        exact_solution = get_shallow_ice_exact_solution(
+            self.Gamma, self.mesh, self.degree, self.nphys_dim)
+        exact_solution.t = self.init_time
+        if self.nphys_dim == 1:
+            boundary_conditions = \
+                get_1d_dirichlet_boundary_conditions_from_expression(
+                    exact_solution, -self.Lx, self.Lx)
+        elif self.nphys_dim == 2:
+            boundary_conditions =\
+                get_dirichlet_boundary_conditions_from_expression(
+                    exact_solution, -self.Lx, self.Lx, -self.Lx, self.Lx)
+
+        
+        function_space = dl.FunctionSpace(self.mesh, "CG", self.degree)
+        return boundary_conditions, function_space
+
+    def get_velocity(self, random_sample):
+        r"""By Default the advection is deterministic and set to zero"""
+        assert random_sample is None
+        beta = dla.Expression((str(0), str(0)), degree=self.degree)
+        return beta
+
+    def get_forcing(self, random_sample):
+        r"""By Default the forcing is deterministic and set to 
+
+        .. math:: (1.5+\cos(2\pi t))*cos(x_1)
+
+        where :math:`t` is time and :math:`x_1` is the first spatial dimension.
+        """
+        forcing = dla.Constant(0.0)
+        return forcing
+
+    def get_diffusivity(self, random_sample):
+        r"""
+        Use the random diffusivity specified in [JEGGIJNME2020].
+        """
+        assert random_sample is None
+        kappa = partial(
+            shallow_ice_diffusion, self.glen_exponent, self.Gamma, self.bed, self.positivity_tol, self.beta)
+        return kappa
+
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+    # Change the following functions to modify mapping of discretization
+    # parameters to mesh and timestep resolution
+    #     get_timestep()
+    #     get_mesh_resolution()
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+    def get_timestep(self, dt_level):
+        dt = self.final_time/2**(dt_level+2)
+        return dt
+
+    def get_mesh_resolution(self, mesh_levels):
+        nx_level, ny_level = mesh_levels
+        nx = 2**(nx_level+2)
+        ny = 2**(ny_level+2)
+        return nx, ny
+
+    def get_mesh(self, resolution_levels):
+        r"""The arguments to this function are the outputs of 
+        get_degrees_of_freedom_and_timestep()"""
+        nx, ny = np.asarray(resolution_levels, dtype=int)
+        mesh = dla.RectangleMesh(dl.Point(0, 0), dl.Point(1, 1), nx, ny)
+        return mesh
+
+    def set_num_config_vars(self):
+        r"""
+        Should be equal to the number of physical dimensions + 1 
+        (for the temporal resolution)
+        """
+        self.num_config_vars = 3
+
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+    # Do not change the following functions
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+    def __init__(self, init_time, final_time, degree, qoi_functional,
+                 second_order_timestepping=True, options={},
+                 qoi_functional_grad=None):
+        self.init_time = init_time
+        self.final_time = final_time
+        self.qoi_functional = qoi_functional
+        self.degree = degree
+        self.second_order_timestepping = second_order_timestepping
+        self.set_num_config_vars()
+        self.options = options
+        self.qoi_functional_grad = qoi_functional_grad
+
+    def solve(self, samples):
+        r"""
+        Run the simulation
+
+        Notes
+        -----
+        Dolfin objects must be initialized inside this function otherwise 
+        this object cannot be pickled and used with multiprocessing.Pool
+        """
+        assert samples.ndim == 2
+        assert samples.shape[1] == 1
+
+        resolution_levels = samples[-self.num_config_vars:, 0]
+        dt = self.get_timestep(resolution_levels[-1])
+        self.mesh = self.get_mesh(
+            self.get_mesh_resolution(resolution_levels[:-1]))
+
+        random_sample = samples[:-self.num_config_vars, 0]
+
+        init_condition, boundary_conditions, function_space, beta, \
+            forcing, kappa = self.initialize_random_expressions(
+                random_sample)
+        # when dla is dolfin_adjoint
+        # Must project dla.CompiledExpression to avoid error
+        # site-packages/pyadjoint/overloaded_type.py", line 136,
+        # in _ad_convert_type raise NotImplementedError
+        self.kappa = dla.interpolate(kappa, function_space)
+        # this is not necessary when just using dolfin
+
+        sol = run_model(
+            function_space, self.kappa, forcing,
+            init_condition, dt, self.final_time,
+            boundary_conditions, velocity=beta,
+            second_order_timestepping=self.second_order_timestepping,
+            intermediate_times=self.options.get('intermediate_times', None))
+        return sol
+
+    def __call__(self, samples, jac=False):
+        sol = self.solve(samples)
+        vals = np.atleast_1d(self.qoi_functional(sol))
+        if vals.ndim == 1:
+            vals = vals[:, np.newaxis]
+
+        if jac is True:
+            assert self.qoi_functional_grad is not None
+            grad = self.qoi_functional_grad(sol, self)
+            return vals, grad
+
+        return vals
+
+
--- pyapprox/fenics_models/tests/test_nonlinear_diffusion.py
+++ pyapprox/fenics_models/tests/test_nonlinear_diffusion.py
@@ -41,50 +41,6 @@ def get_diffusion_forcing(q, sol_func, mesh, degree):
     return forcing
 
 
-def get_shallow_ice_exact_solution_sympy(Gamma, ndim):
-    from sympy.abc import t
-    x, y = sp.symbols('x[0] x[1]')
-
-    n, H0, R0 = 3, 3600., 750e3
-
-    if ndim == 2:
-        beta = 1./(5*n+3)
-        alpha = 2*beta
-        r = sp.sqrt(x**2+y**2)/R0
-    elif ndim == 1:
-        beta = 1./(3*n+2)
-        alpha = beta
-        r = sp.sqrt(x**2)/R0
-
-    t0 = (beta/Gamma) * (7/4)**3 * (R0**4/H0**7)
-    tt = t/t0
-
-    inside = sp.Max(0, 1-(r/tt**beta)**((n+1)/n))
-    H = H0*inside**(n/(2*n+1))/tt**alpha
-    return H, x, y, t
-
-
-def get_shallow_ice_exact_solution(Gamma, mesh, degree, ndim):
-    H, x, y, t = get_shallow_ice_exact_solution_sympy(Gamma, ndim)
-    exact_sol = Expression(
-        sp.printing.ccode(H), cell=mesh.ufl_cell(),
-        domain=mesh, t=0, degree=degree)
-    return exact_sol
-
-
-def shallow_ice_diffusion_sympy(Gamma, H):
-    """
-    Only valid for n=3
-    """
-    from sympy.abc import t
-    x, y = sp.symbols('x[0] x[1]')
-    bed = 0
-    grad_h = [sp.diff(H+b, xi) for xi in (x, y)]
-    diff = Gamma*H**(5)*(grad_h[0]**2+grad_h[1]**2)
-    diff = sp.simplify(diff)
-    return diff
-
-
 class TestNonlinearDiffusion(unittest.TestCase):
     def test_quadratic_diffusion_dirichlet_boundary_conditions(self):
         """
@@ -136,7 +92,7 @@ class TestNonlinearDiffusion(unittest.TestCase):
                    'init_condition': get_advec_exact_solution(mesh, degree),
                    'nlsparam': nlsparam,
                    'nonlinear_diffusion': constant_diffusion}
-        sol = run_model(function_space, options)
+        sol = run_model(function_space, **options)
 
         exact_sol = get_advec_exact_solution(mesh, degree)
         exact_sol.t = options['final_time']
@@ -144,6 +100,119 @@ class TestNonlinearDiffusion(unittest.TestCase):
         print('Abs. Error', error)
         assert error <= 1e-4
 
+    #@skiptest
+    def test_adjoint(self):
+
+        np_kappa = 2
+        nx, ny, degree = 11, 11, 2
+        mesh = dla.RectangleMesh(dl.Point(0, 0), dl.Point(1, 1), nx, ny)
+        function_space = dl.FunctionSpace(mesh, "Lagrange", degree)
+
+
+        def constant_diffusion(kappa, u):
+            return kappa
+
+        boundary_conditions = \
+            get_dirichlet_boundary_conditions_from_expression(
+                get_advec_exact_solution(mesh, degree), 0, 1, 0, 1)
+
+        class NonlinearDiffusivity(object):
+            def __init__(self, kappa):
+                self.kappa = kappa
+
+            def __call__(self, u):
+                return (self.kappa+u**2)
+        
+        dl_kappa = dla.Constant(np_kappa)
+        options = {'time_step': 0.05, 'final_time': 1.,
+                   'forcing': dla.Constant(1),
+                   'boundary_conditions': boundary_conditions,
+                   'init_condition': get_advec_exact_solution(mesh, degree),
+                   'nonlinear_diffusion': NonlinearDiffusivity(dl_kappa),
+                   'second_order_timestepping': True,
+                   'nlsparam': dict()} 
+
+        def dl_qoi_functional(sol):
+            return dla.assemble(sol*dl.dx)
+        
+        def dl_fun(np_kappa):
+            kappa = dla.Constant(np_kappa)
+            options_copy = options.copy()
+            options_copy['forcing'] = dla.Constant(1.0)
+            # using class avoids pickling
+            options_copy['nonlinear_diffusion'] = NonlinearDiffusivity(kappa) 
+            sol = run_model(function_space, **options_copy)
+            return sol, kappa
+
+        def fun(np_kappa):
+            np_kappa = np_kappa[0, 0]
+            sol, kappa = dl_fun(np_kappa)
+            J = dl_qoi_functional(sol)
+            control = dla.Control(kappa)
+            dJd_kappa = dla.compute_gradient(J, [control])[0]
+            print(float(dJd_kappa))
+            return np.atleast_1d(float(J)), np.atleast_2d(float(dJd_kappa))
+
+        sol, kappa = dl_fun(np_kappa)
+
+        J = dl_qoi_functional(sol)
+        control = dla.Control(kappa)
+        Jhat = dla.ReducedFunctional(J, control)
+        h = dla.Constant(np.random.normal(0, 1, 1))
+        # conv_rate = dla.taylor_test(Jhat, kappa, h)
+        # assert np.allclose(conv_rate, 2.0, atol=1e-2)
+
+        from pyapprox.optimization import check_gradients
+        x0 = np.atleast_2d(np_kappa)
+        check_gradients(fun, True, x0)
+
+
+
+def get_shallow_ice_exact_solution_sympy(Gamma, ndim):
+    from sympy.abc import t
+    x, y = sp.symbols('x[0] x[1]')
+
+    n, H0, R0 = 3, 3600., 750e3
+
+    if ndim == 2:
+        beta = 1./(5*n+3)
+        alpha = 2*beta
+        r = sp.sqrt(x**2+y**2)/R0
+    elif ndim == 1:
+        beta = 1./(3*n+2)
+        alpha = beta
+        r = sp.sqrt(x**2)/R0
+
+    t0 = (beta/Gamma) * (7/4)**3 * (R0**4/H0**7)
+    tt = t/t0
+
+    inside = sp.Max(0, 1-(r/tt**beta)**((n+1)/n))
+    H = H0*inside**(n/(2*n+1))/tt**alpha
+    return H, x, y, t
+
+
+def get_shallow_ice_exact_solution(Gamma, mesh, degree, ndim):
+    H, x, y, t = get_shallow_ice_exact_solution_sympy(Gamma, ndim)
+    exact_sol = dla.Expression(
+        sp.printing.ccode(H), cell=mesh.ufl_cell(),
+        domain=mesh, t=0, degree=degree)
+    return exact_sol
+
+
+def shallow_ice_diffusion_sympy(Gamma, H):
+    """
+    Only valid for n=3
+    """
+    from sympy.abc import t
+    x, y = sp.symbols('x[0] x[1]')
+    bed = 0
+    grad_h = [sp.diff(H+b, xi) for xi in (x, y)]
+    diff = Gamma*H**(5)*(grad_h[0]**2+grad_h[1]**2)
+    diff = sp.simplify(diff)
+    return diff
+
+
+class TestShallowIceEquation(unittest.TestCase):
     def run_shallow_ice_halfar(self, nphys_dim):
         """
         See 'Exact time-dependent similarity solutions for isothermal shallow 
@@ -163,12 +232,12 @@ class TestNonlinearDiffusion(unittest.TestCase):
         Lx = 1200e3
 
         if nphys_dim == 1:
-            mesh = IntervalMesh(nx, -Lx, Lx)
+            mesh = dla.IntervalMesh(nx, -Lx, Lx)
 
         elif nphys_dim == 2:
             ny, Ly = nx, Lx
-            mesh = RectangleMesh(Point(-Lx, -Ly), Point(Lx, Ly), nx, ny)
-        function_space = FunctionSpace(mesh, "Lagrange", degree)
+            mesh = dla.RectangleMesh(dl.Point(-Lx, -Ly), dl.Point(Lx, Ly), nx, ny)
+        function_space = dl.FunctionSpace(mesh, "Lagrange", degree)
 
         bed = None
         forcing = dla.Constant(0.0)
@@ -213,21 +282,21 @@ class TestNonlinearDiffusion(unittest.TestCase):
 
         # exact_solution.t=forcing.t+options['time_step']
         exact_solution.t = options['final_time']
-        error = errornorm(exact_solution, sol, mesh=mesh)
+        error = dl.errornorm(exact_solution, sol, mesh=mesh)
         print('Abs. Error', error)
-        rel_error = error/sqrt(assemble(exact_solution**2*dx(degree=5)))
+        rel_error = error/dl.sqrt(dla.assemble(exact_solution**2*dl.dx(degree=5)))
         print('Rel. Error', rel_error)
 
         plot = False
         if plot and nphys_dim == 1:
-            function_space = FunctionSpace(mesh, "CG", degree)
+            function_space = dl.FunctionSpace(mesh, "CG", degree)
             x = function_space.tabulate_dof_coordinates()
             indices = np.argsort(x)
             x = x[indices]
             values = sol.vector().get_local()
             values = values[indices]
             plt.plot(x, values)
-            exact_values = interpolate(
+            exact_values = dla.interpolate(
                 exact_solution, function_space).vector().get_local()
             plt.plot(x, exact_values[indices])
             plt.show()
@@ -249,81 +318,8 @@ class TestNonlinearDiffusion(unittest.TestCase):
     def test_shallow_ice_halfar_1d(self):
         self.run_shallow_ice_halfar(1)
 
-    def test_shallow_ice_halfar_1d(self):
+    def test_shallow_ice_halfar_2d(self):
         self.run_shallow_ice_halfar(2)
-
-    #@skiptest
-    def test_adjoint(self):
-
-        np_kappa = 2
-        nx, ny, degree = 31, 31, 2
-        mesh = dla.RectangleMesh(dl.Point(0, 0), dl.Point(1, 1), nx, ny)
-        function_space = dl.FunctionSpace(mesh, "Lagrange", degree)
-
-
-        def constant_diffusion(kappa, u):
-            return kappa
-
-        boundary_conditions = \
-            get_dirichlet_boundary_conditions_from_expression(
-                get_advec_exact_solution(mesh, degree), 0, 1, 0, 1)
-
-        dl_kappa = dla.Constant(np_kappa)
-        options = {'time_step': 0.05, 'final_time': 1.,
-                   'forcing': dla.Constant(1),#get_advec_forcing(dl_kappa, mesh, degree),
-                   'boundary_conditions': boundary_conditions,
-                   'second_order_timestepping': True,
-                   'init_condition': get_advec_exact_solution(mesh, degree),
-                   'nlsparam': dict(),
-                   'nonlinear_diffusion': partial(constant_diffusion, dl_kappa)} 
-
-        def dl_qoi_functional(sol):
-            return dla.assemble(sol*dl.dx)
-
-        class ConstantDiffusion(object):
-            def __init__(self, kappa):
-                self.kappa = kappa
-
-            def __call__(self, u):
-                return self.kappa
-        
-        def dl_fun(np_kappa):
-            kappa = dla.Constant(np_kappa)
-            options_copy = options.copy()
-            options_copy['forcing'] = dla.Constant(1.0)#get_advec_forcing(kappa, mesh, degree)
-            #options_copy['nonlinear_diffusion'] = partial(constant_diffusion, kappa)
-            # see if works with partial above
-            # using class avoids pickling
-            options_copy['nonlinear_diffusion'] = ConstantDiffusion(kappa) 
-            sol = run_model(function_space, options_copy)
-            return sol, kappa
-
-        def fun(np_kappa):
-            np_kappa = np_kappa[0, 0]
-            sol, kappa = dl_fun(np_kappa)
-            J = dl_qoi_functional(sol)
-            control = dla.Control(kappa)
-            dJd_kappa = dla.compute_gradient(J, [control])[0]
-            print(float(dJd_kappa))
-            return np.atleast_1d(float(J)), np.atleast_2d(float(dJd_kappa))
-
-        sol, kappa = dl_fun(np_kappa)
-        exact_sol = get_advec_exact_solution(mesh, 5)
-        exact_sol.t = options['final_time']
-        error = dl.errornorm(exact_sol, sol, mesh=mesh)
-        print('Abs. Error', error)
-        # assert error <= 1e-4
-
-        J = dl_qoi_functional(sol)
-        control = dla.Control(kappa)
-        Jhat = dla.ReducedFunctional(J, control)
-        h = dla.Constant(np.random.normal(0, 1, 1))
-        # conv_rate = dla.taylor_test(Jhat, kappa, h)
-        # assert np.allclose(conv_rate, 2.0, atol=1e-2)
-
-        from pyapprox.optimization import check_gradients
-        x0 = np.atleast_2d(np_kappa)
-        check_gradients(fun, True, x0)
 
         
 
@@ -331,3 +327,7 @@ if __name__ == "__main__":
     nonlinear_diffusion_test_suite = unittest.TestLoader().loadTestsFromTestCase(
         TestNonlinearDiffusion)
     unittest.TextTestRunner(verbosity=2).run(nonlinear_diffusion_test_suite)
+
+    shallow_ice_test_suite = unittest.TestLoader().loadTestsFromTestCase(
+        TestShallowIceEquation)
+    unittest.TextTestRunner(verbosity=2).run(shallow_ice_test_suite)
