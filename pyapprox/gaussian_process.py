Fix analytical calculation of mean of sobol indices of a gaussian process when using centered data

pyapprox/gaussian_process.py
@@ -2411,10 +2411,10 @@ def _compute_expected_sobol_indices(
         variance_random_mean[ii] = variance_of_mean(kernel_var, varsigma_sq)
         zeta = compute_zeta_econ(
             y_train[:, ii:ii+1], A_inv_y[:, ii:ii+1], A_inv_P)
+        zeta += 2*tau.dot(A_inv_y)*y_train_mean+y_train_mean**2
         expected_random_var[ii] = mean_of_variance(
             zeta, v_sq, kernel_var, expected_random_mean[ii],
             variance_random_mean[ii])
-    expected_random_var += 2*tau.dot(A_inv_y)*y_train_mean+y_train_mean**2
 
     from pyapprox.probability_measure_sampling import \
         generate_independent_random_samples
@@ -2453,12 +2453,14 @@ def _compute_expected_sobol_indices(
                 U_p *= u_list[ii]
         trace_A_inv_Pp = np.sum(A_inv*P_p)# U_p-np.trace(A_inv.dot(P_p))
         for ii in range(y_train.shape[1]):
-            unnormalized_interaction_values[jj, ii] = kernel_var*(
-                # U_p-np.trace(A_inv.dot(P_p))) + \
-                U_p-trace_A_inv_Pp) + \
-                A_inv_y[:, ii:ii+1].T.dot(P_p.dot(A_inv_y[:, ii:ii+1]))
-            unnormalized_interaction_values[jj, ii] -= \
-                variance_random_mean[ii]+expected_random_mean[ii]**2
+            # varsigma_ii = U_p-np.trace(A_inv.dot(P_p)))
+            v_sq_ii = U_p-trace_A_inv_Pp
+            zeta_ii = A_inv_y[:, ii:ii+1].T.dot(P_p.dot(A_inv_y[:, ii:ii+1]))
+            zeta_ii += 2*tau.dot(A_inv_y[:, ii:ii+1])*y_train_mean+\
+                y_train_mean**2
+            unnormalized_interaction_values[jj, ii] = mean_of_variance(
+                zeta_ii, v_sq_ii, kernel_var, expected_random_mean[ii],
+                variance_random_mean[ii])
     unnormalized_total_effect_values = \
         unnormalized_interaction_values[interaction_terms.shape[1]:]
     unnormalized_interaction_values = \

pyapprox/tests/test_gaussian_process.py
@@ -460,7 +460,8 @@ class TestGaussianProcess(unittest.TestCase):
         print('MC expected random mean', np.mean(random_means))
         print('MC variance random mean', np.var(random_means))
         print('MC expected random var', np.mean(random_variances))
-        print('MC variance random var', "{:e}".format(np.var(random_variances)))
+        print('MC variance random var', "{:e}".format(
+            np.var(random_variances)))
 
         assert np.allclose(true_mean, expected_random_mean, rtol=1e-3)
         assert np.allclose(expected_random_var, true_var, rtol=1e-3)
@@ -545,7 +546,8 @@ class TestGaussianProcess(unittest.TestCase):
         nvars = 2
         a = np.array([1, 0.25])
         # a = np.array([1, 1])
-        def func(x): return np.sum(a[:, None]*(2*x-1)**2, axis=0)[:, np.newaxis]
+        def func(x):
+            return np.sum(a[:, None]*(2*x-1)**2, axis=0)[:, np.newaxis]
 
         ntrain_samples = 20
         #train_samples = np.random.uniform(0, 1, (nvars, ntrain_samples))
@@ -673,7 +675,8 @@ class TestGaussianProcess(unittest.TestCase):
         nvars = 2
         a = np.array([1, 0.25])
         # a = np.array([1, 1])
-        def func(x): return np.sum(a[:, None]*(2*x-1)**2, axis=0)[:, np.newaxis]
+        def func(x):
+            return np.sum(a[:, None]*(2*x-1)**2, axis=0)[:, np.newaxis]
 
         ntrain_samples = 100
         #train_samples = np.random.uniform(0, 1, (nvars, ntrain_samples))
@@ -706,7 +709,7 @@ class TestGaussianProcess(unittest.TestCase):
         expected_random_mean, variance_random_mean, expected_random_var,\
             variance_random_var = integrate_gaussian_process(
                 gp, variable, nquad_samples=nquad_samples)
-        print('v',variance_random_mean, expected_random_var)
+        # print('v',variance_random_mean, expected_random_var)
 
         true_mean = 1/3*a.sum()
         unnormalized_main_effect_0 = a[0]**2/5+(2*a[0]*a[1])/9+a[1]**2/9 -\
@@ -728,10 +731,10 @@ class TestGaussianProcess(unittest.TestCase):
                 gp, variable, interaction_terms, nquad_samples=nquad_samples)
         true_unnormalized_sobol_indices = np.vstack((
             true_unnormalized_main_effects, [[0]]))
-        # print(np.absolute(unnormalized_sobol_indices-true_unnormalized_sobol_indices) - 1e-3*true_unnormalized_sobol_indices)
         true_sobol_indices = true_unnormalized_sobol_indices/true_var
+        # print(np.absolute(sobol_indices-true_sobol_indices) - 3e-5-1e-5*true_sobol_indices)
         assert np.allclose(
-            sobol_indices, true_sobol_indices, rtol=1e-4, atol=1e-4)
+            sobol_indices, true_sobol_indices, rtol=1e-5, atol=3e-5)
 
     def test_compute_sobol_indices_gaussian_process_uniform_3d(self):
         nvars = 3