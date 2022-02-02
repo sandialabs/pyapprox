Fix error in numba function used to compute halton sequences. Modularize spectral collocation pde code. Make log-likelihood function for bayesian oed take a vector of noises. The last is not yet tested or used within the code

pyapprox/bayesian_oed.py
@@ -10,6 +10,34 @@ from pyapprox.probability_measure_sampling import (
 
 @njit(cache=True)
 def sq_dists_numba_3d(XX, YY, a=1, b=0, active_indices=None):
+    """
+    Compute the scaled l2-norm distance between two sets of samples
+    E.g. for one point
+
+
+    a[ii]*(XX[ii]-YY[ii])+b[ii]
+
+
+    Parameters
+    ----------
+    XX : np.ndarray (nvars, 1, NN)
+        The first set of samples
+
+    YY : np.ndarray (LL, MM, NN)
+        The second set of samples. The 3D arrays are useful when computing
+        squared distances for multiple sets of samples
+
+    a : float or np.ndarray (NN, 1)
+        scalar multiplying l2 distance
+
+    a : float or np.ndarray (NN, 1)
+        scalar added to l2 distance
+
+    Returns
+    -------
+    ss : np.ndarray (LL, MM)
+        The scaled distances
+    """
     Yshape = YY.shape
     assert XX.shape[1] == 1
     ss = np.empty(Yshape[:2])
@@ -31,20 +59,57 @@ def sq_dists_numba_3d(XX, YY, a=1, b=0, active_indices=None):
 def gaussian_loglike_fun_broadcast(
         obs, pred_obs, noise_stdev, active_indices=None):
     """
-    This can handle all cases 1d,2d, 3d arrays but is slower
+    Conmpute the log-likelihood values from a set of real and predicted
+    observations
+
+    Parameters
+    ----------
+    obs : np.ndarray (nsamples, nobs)
+        The real observations repeated for each sample
+
+    pred_obs : np.ndarray (nsamples, nobs)
+        The observations predicited by the model for a set of samples
+
+    noise_stdev : float or np.ndarray (nobs, 1)
+        The standard deviation of Gaussian noise added to each observation
+
+    active_indices : np.ndarray (nobs, 1)
+        The subset of indices of the observations used to compute the
+        likelihood
+
+    Returns
+    -------
+    llike : np.ndaray (nsamples, 1)
+
+    Notes
+    -----
+    This can handle 1d, 2d, 3d arrays but is slower
     due to broadcasting when computing obs-pred_obs
     """
+    if (type(noise_stdev) == np.ndarray and
+            noise_stdev.shape[0] != obs.shape[1]):
+        raise ValueError("noise must be provided for each observation")
+
     if active_indices is None:
         nobs = obs.shape[-1]
-        # avoid copy is possible
-        # using special indexing with array makes a copy which is slow
-        llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs+np.sum(
-            -(obs-pred_obs)**2, axis=-1)/(2*noise_stdev**2)
+        # avoid copy if possible
+        # using special indexing with array e.g array[:, I] where I is an array
+        # makes a copy which is slow
+        if (type(noise_stdev) != np.ndarray):
+            llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs
+        else:
+            llike = 0.5*np.sum(np.log(1/(2*np.pi*noise_stdev**2)))
+        llike += np.sum(-(obs-pred_obs)**2/(2*noise_stdev**2), axis=-1)
     else:
         nobs = active_indices.shape[0]
-        llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs+np.sum(
-            -(obs[..., active_indices]-pred_obs[..., active_indices])**2,
-            axis=-1)/(2*noise_stdev**2)
+        if (type(noise_stdev) != np.ndarray):
+            llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs+np.sum(
+                -(obs[..., active_indices]-pred_obs[..., active_indices])**2
+                / (2*noise_stdev**2), axis=-1)
+        else:
+            llike = 0.5*np.sum(np.log(1/(2*np.pi*noise_stdev**2)))+np.sum(
+                -(obs[..., active_indices]-pred_obs[..., active_indices])**2
+                / (2*noise_stdev[active_indices]**2), axis=-1)
     if llike.ndim == 1:
         llike = llike[:, None]
     return llike
@@ -61,7 +126,10 @@ def gaussian_loglike_fun_economial_3D(
     else:
         nobs = active_indices.shape[-1]
 
-    tmp1 = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs
+    if (type(noise_stdev) != np.ndarray):
+        tmp1 = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs
+    else:
+        tmp1 = 0.5*np.sum(np.log(1/(2*np.pi*noise_stdev**2)))
     tmp2 = -1/(2*noise_stdev**2)
     llike = sq_dists_numba_3d(obs, pred_obs, tmp2, tmp1, active_indices)
     if llike.ndim == 1:
@@ -83,7 +151,11 @@ def gaussian_loglike_fun_economial_2D(
         sq_dists = cdist(
             obs[..., active_indices], pred_obs[..., active_indices],
             "sqeuclidean")
-    llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs-sq_dists[0, :]/(
+    if (type(noise_stdev) != np.ndarray):
+        llike = 0.5*np.log(1/(2*np.pi*noise_stdev**2))*nobs-sq_dists[0, :]/(
+            2*noise_stdev**2)
+    else:
+        llike = 0.5*np.sum(np.log(1/(2*np.pi*noise_stdev**2)))-sq_dists[0, :]/(
             2*noise_stdev**2)
     if llike.ndim == 1:
         llike = llike[:, None]
@@ -140,7 +212,8 @@ def __compute_expected_kl_utility_monte_carlo(
                          outer_loop_weights)
     if not return_all:
         return utility_val
-    return utility_val, evidences
+    weights = np.exp(inner_log_likelihood_vals)*inner_loop_weights/evidences
+    return utility_val, evidences, weights
 
 
 def precompute_compute_expected_kl_utility_data(
@@ -391,7 +464,7 @@ def __compute_negative_expected_deviation_monte_carlo(
     if not return_all:
         return -expected_deviation
     else:
-        return -expected_deviation, deviations, evidences, weights
+        return -expected_deviation, evidences, weights, deviations
 
 
 def compute_negative_expected_deviation_monte_carlo(
@@ -571,7 +644,7 @@ class BayesianBatchKLOED(object):
     def populate(self):
         (self.outer_loop_obs, self.outer_loop_pred_obs,
          self.inner_loop_pred_obs, self.inner_loop_weights,
-         self.outer_loop_prior_samples, __) = \
+         self.outer_loop_prior_samples, self.inner_loop_prior_samples) = \
              precompute_compute_expected_kl_utility_data(
                  self.generate_prior_samples, self.nouter_loop_samples,
                  self.obs_fun, self.noise_fun, self.ninner_loop_samples,

pyapprox/benchmarks/spectral_diffusion.py
@@ -88,13 +88,17 @@ class SteadyStateAdvectionDiffusionEquation1D(object):
         self.adjoint_bndry_conds = None
         self.adjoint_dirichlet_bndry_indices = None
         self.adjoint_neumann_bndry_indices = None
-
+        self.qoi_functional = None
         # default qoi functional is integral of solution over entire domain
-        self.qoi_functional = self.integrate
-        self.qoi_functional_deriv = lambda x: x*0.+1.
+        self.set_qoi_functional(self.integrate, lambda sol: sol*0.+1.)
+
+    def set_qoi_functional(self, qoi_functional, qoi_functional_deriv=None):
+        self.qoi_functional = qoi_functional
+        self.qoi_functional_deriv = qoi_functional_deriv
 
     def map_samples_from_canonical_domain(self, canonical_pts):
-        pts = map_hypercube_samples(canonical_pts, self.canonical_domain, self.domain)
+        pts = map_hypercube_samples(
+            canonical_pts, self.canonical_domain, self.domain)
         return pts
 
     def initialize(self, bndry_conds, diffusivity_fun, forcing_fun,
@@ -241,7 +245,8 @@ class SteadyStateAdvectionDiffusionEquation1D(object):
     def solve(self, diffusivity, forcing, advection):
         assert diffusivity.ndim == 2 and diffusivity.shape[1] == 1
         assert forcing.ndim == 2 and forcing.shape[1] == 1
-        assert advection.ndim == 2 and advection.shape[1] == self.mesh_pts.shape[0]
+        assert advection.ndim == 2 and (
+            advection.shape[1] == self.mesh_pts.shape[0])
         # forcing will be overwritten with bounary values so must take a
         # deep copy
         forcing = forcing.copy()
@@ -625,6 +630,35 @@ class TransientAdvectionDiffusionEquation1D(
         return sols[:, :sol_cntr]
 
 
+def integrate_subdomain(mesh_values, subdomain, order, interpolate,
+                        quad_rule=None):
+    # Keep separate from model class so that pre-computed so
+    # it can also be used by QoI functions
+    if quad_rule is None:
+        quad_pts, quad_wts = get_2d_sub_domain_quadrature_rule(
+            subdomain, order)
+    else:
+        quad_pts, quad_wts = quad_rule
+    quad_vals = interpolate(mesh_values, quad_pts)
+    # Compute and return integral
+    return np.dot(quad_vals[:, 0], quad_wts)
+
+
+def get_2d_sub_domain_quadrature_rule(subdomain, order):
+    gl_pts, gl_wts = gauss_jacobi_pts_wts_1D(order, 0, 0)
+    pts_1d, wts_1d = [], []
+    for ii in range(2):
+        # Scale points from [-1,1] to to physical domain
+        x_range = subdomain[2*ii+1]-subdomain[2*ii]
+        # Remove factor of 0.5 from weights and shift to [a,b]
+        wts_1d.append(gl_wts*x_range)
+        pts_1d.append(x_range*(gl_pts+1.)/2.+subdomain[2*ii])
+    # Interpolate mesh values onto quadrature nodes
+    pts = cartesian_product(pts_1d)
+    wts = outer_product(wts_1d)
+    return pts, wts
+
+
 class SteadyStateAdvectionDiffusionEquation2D(
         SteadyStateAdvectionDiffusionEquation1D):
     """
@@ -735,22 +769,7 @@ class SteadyStateAdvectionDiffusionEquation2D(
         return qoi_deriv
 
     def integrate(self, mesh_values, order=None):
-        return self.integrate_subdomain(mesh_values, self.domain, order)
-
-    def integrate_subdomain(self, mesh_values, subdomain, order=None):
         if order is None:
             order = self.order
-
-        pts_1d, wts_1d = [], []
-        for ii in range(2):
-            # Scale points from [-1,1] to to physical domain
-            x_range = subdomain[2*ii+1]-subdomain[2*ii]
-            # Remove factor of 0.5 from weights and shift to [a,b]
-            wts_1d.append(self.gl_wts*x_range)
-            pts_1d.append(x_range*(self.gl_pts+1.)/2.+subdomain[2*ii])
-        # Interpolate mesh values onto quadrature nodes
-        pts = cartesian_product(pts_1d)
-        wts = outer_product(wts_1d)
-        gl_vals = self.interpolate(mesh_values, pts)
-        # Compute and return integral
-        return np.dot(gl_vals[:, 0], wts)
+        return integrate_subdomain(
+            mesh_values, self.domain, order, self.interpolate)

pyapprox/low_discrepancy_sequences.py
@@ -17,11 +17,11 @@ def halton_sequence(num_vars, index1, index2):
     except Exception as e:
         trace_error_with_msg('halton_sequence extension failed', e)
 
-    return __halton_sequence(num_vars, index1, index2)
+    return __halton_sequence(num_vars, index1, index2, primes)
 
 
 @njit(cache=True)
-def __halton_sequence(num_vars, index1, index2):
+def __halton_sequence(num_vars, index1, index2, primes):
     num_samples = index2-index1
     sequence = np.zeros((num_vars, num_samples))
     ones = np.ones(num_vars)
@@ -75,13 +75,13 @@ def index_of_first_zero_bit_moving_right_to_left(ii):
     while ((ii & 1) > 0):
         ii >>= 1
         jj += 1
-        #print(ii, "{0:b}".format(ii), jj, (ii&1)>0)
+        # print(ii, "{0:b}".format(ii), jj, (ii&1)>0)
     return jj-1
 
 
 def load_direction_sequence(nvars):
     """
-    Read direction_numbers from 
+    Read direction_numbers from
     https://web.maths.unsw.edu.au/~fkuo/sobol/
     """
     import os
@@ -95,8 +95,7 @@ def load_direction_sequence(nvars):
     while ii < nvars:
         line = dir_seq_file.readline()
         if not line:
-            msg = 'Requested to many dimension. Can only compute sequences for '
-            msg += f'dimensions up to {max_nvars}.'
+            msg = 'Requested to many dimension.'
             raise Exception(msg)
         line = line.split()
         dim, s, a_vals[ii-1] = line[:3]
@@ -139,12 +138,12 @@ def compute_direction_numbers(seq, max_nbits, power, a_val):
 
 def _sobol_sequence(nvars, nsamples):
     """
-    Compute Sobol sequence using 
+    Compute Sobol sequence using
     Algorithm 659: Implementing Sobol’s quasirandom sequence generator
-    with direction_numbers obtain from 
+    with direction_numbers obtain from
     https://web.maths.unsw.edu.au/~fkuo/sobol/
 
-    See Section 1 of notes at 
+    See Section 1 of notes at
     https://web.maths.unsw.edu.au/~fkuo/sobol/joe-kuo-notes.pdf
     """
     power = 32
@@ -166,7 +165,7 @@ def _sobol_sequence(nvars, nsamples):
     samples[:, 0] = 0
 
     tmp1 = 0
-    seq = np.ones((max_nbits), dtype=np.int64)
+    # seq = np.ones((max_nbits), dtype=np.int64)
     dir_nums = compute_direction_numbers(None, max_nbits, power, None)
     for ii in range(1, nsamples):
         tmp2 = tmp1 ^ dir_nums[indices[ii-1]]
@@ -180,11 +179,12 @@ def _sobol_sequence(nvars, nsamples):
             tmp2 = tmp1 ^ dir_nums[indices[ii-1]]
             samples[dd, ii] = tmp2/const
             tmp1 = tmp2
-    assert samples.max()<=1 and samples.min()>=0
+    assert samples.max() <= 1 and samples.min() >= 0
     return samples
 
+
 def sobol_sequence(nvars, nsamples, start_index=0, variable=None):
-    samples =  _sobol_sequence(nvars, nsamples+start_index)[:, start_index:]
+    samples = _sobol_sequence(nvars, nsamples+start_index)[:, start_index:]
     if variable is None:
         return samples
     samples = variable.evaluate('ppf', samples)

pyapprox/tests/test_bayesian_oed.py
@@ -90,6 +90,7 @@ class TestBayesianOED(unittest.TestCase):
 
         prior_pdf = stats.norm(prior_mean[0], np.sqrt(prior_cov[0])).pdf
         pred_obs = fun(design, xx[None, :])
+        print(obs.shape)
         lvals = np.exp(
             gaussian_loglike_fun(obs, pred_obs, noise_std))*prior_pdf(
                 xx)[:, None]
@@ -750,7 +751,7 @@ class TestBayesianOED(unittest.TestCase):
             # Update the design
             utility_vals, selected_indices = oed.update_design()
 
-            utility, deviations, evidences, weights = \
+            utility, evidences, weights, deviations  = \
                 oed_copy.compute_expected_utility(
                     oed_copy.collected_design_indices, selected_indices, True)
 