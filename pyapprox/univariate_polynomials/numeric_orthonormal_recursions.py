Fix bug in numerical computation of univariate orthogonal recursion coefficients where last a coefficient was not computed

pyapprox/univariate_polynomials/numeric_orthonormal_recursions.py
@@ -411,13 +411,15 @@ def predictor_corrector(nterms, measure, lb, ub, quad_options={}):
         else:
             integrate = integrate_continuous
 
-    ab = np.zeros((nterms, 2))
+    # To get ab[nterms-1, 0] we must compute ab[nterms, 1]
+    # we will truncate ab at the end
+    ab = np.zeros((nterms+1, 2))
 
     # for probablity measures the following will always be one, but
     # this is not true for other measures
     ab[0, 1] = np.sqrt(integrate(measure))
 
-    for ii in range(1, nterms):
+    for ii in range(1, nterms+1):
         # predict
         ab[ii, 1] = ab[ii-1, 1]
         if ii > 1:
@@ -443,7 +445,7 @@ def predictor_corrector(nterms, measure, lb, ub, quad_options={}):
         G_ii_ii = integrate(partial(integrand, measure))
         ab[ii, 1] *= np.sqrt(G_ii_ii)
 
-    return ab
+    return ab[:nterms, :]
 
 
 def predictor_corrector_function_of_independent_variables(

pyapprox/univariate_polynomials/tests/test_orthonormal_polynomials.py
@@ -11,7 +11,8 @@ from pyapprox.univariate_polynomials.orthonormal_polynomials import \
     convert_orthonormal_expansion_to_monomial_expansion_1d
 from pyapprox.univariate_polynomials.orthonormal_recursions import \
     jacobi_recurrence, hermite_recurrence, krawtchouk_recurrence, \
-    discrete_chebyshev_recurrence, hahn_recurrence, charlier_recurrence
+    discrete_chebyshev_recurrence, hahn_recurrence, charlier_recurrence, \
+    laguerre_recurrence
 from pyapprox.univariate_polynomials.orthonormal_recursions import \
     convert_orthonormal_recurence_to_three_term_recurence
 from pyapprox.monomial import univariate_monomial_basis_matrix
@@ -357,6 +358,37 @@ class TestOrthonormalPolynomials1D(unittest.TestCase):
             ortho_coef, ab, mu, sigma)
         true_mono_coefs = np.array([-mu**3, 3*mu**2, -3*mu, 1])/sigma**3
         assert np.allclose(mono_coefs, true_mono_coefs)
+
+    def test_orthonormality_laguerre_polynomial(self):
+        a = 3
+        rho = a-1
+        degree = 2
+        probability_measure = True
+        ab = laguerre_recurrence(
+            rho, degree+1, probability=probability_measure)
+        print(ab)
+        from scipy import stats
+        x = stats.gamma(a).rvs(int(1e6))
+        w = np.ones(x.shape[0])/x.shape[0]
+        p = evaluate_orthonormal_polynomial_1d(x, degree, ab)
+
+        p_exact = np.asarray(
+            [1+0.*x, -(-x+rho+1),
+             0.5*x**2-x*(rho+2)+(rho+1)*(rho+2)/2]).T[:, :degree+1]
+        p_exact /= np.sqrt(sp.gamma(np.arange(degree+1)+rho+1)/sp.factorial(
+            np.arange(degree+1))/sp.gamma(rho+1))
+        #print(p_exact.T.dot(w), 'p')
+        #print(np.dot(p_exact.T*w, p_exact)-np.eye(degree+1))
+        assert np.allclose(p, p_exact)
+
+        # test orthogonality
+        exact_moments = np.zeros((degree+1))
+        exact_moments[0] = 1.0
+        #print(np.dot(p.T, w)-exact_moments)
+        assert np.allclose(np.dot(p.T, w), exact_moments, atol=2e-3)
+        # test orthonormality
+        # print(np.dot(p.T*w, p)-np.eye(degree+1))
+        assert np.allclose(np.dot(p.T*w, p), np.eye(degree+1), atol=2e-2)
 
 
 if __name__ == "__main__":

pyapprox/univariate_polynomials/tests/test_quadrature.py
@@ -8,12 +8,14 @@ import warnings
 from pyapprox.univariate_polynomials.quadrature import \
     gauss_jacobi_pts_wts_1D, gauss_hermite_pts_wts_1D, \
     clenshaw_curtis_pts_wts_1D, leja_growth_rule, \
-    constant_increment_growth_rule
+    constant_increment_growth_rule, gauss_quadrature
 from pyapprox.utilities import beta_pdf_on_ab, gaussian_pdf
 from pyapprox.variables import float_rv_discrete, get_probability_masses
 from pyapprox.univariate_polynomials.leja_quadrature import \
     get_univariate_leja_quadrature_rule
 from pyapprox.variables import transform_scale_parameters
+from pyapprox.univariate_polynomials.orthonormal_recursions import \
+    laguerre_recurrence
 
 
 class TestQuadrature(unittest.TestCase):
@@ -236,6 +238,25 @@ class TestQuadrature(unittest.TestCase):
 
         assert np.allclose(
             (quad_samples**2).dot(weights[-1]), (samples**2).mean())
+
+    def test_gauss_laguerre_quadrature(self):
+        a = 1
+        rho = a-1
+        N = 10
+
+        true_mean = a
+        true_variance = a
+
+        ab = laguerre_recurrence(rho, N)
+        print(ab)
+        x, w = gauss_quadrature(ab, N)
+
+        def function(x): return x**2
+        # print(stats.gamma(a).mean())
+        # print(x.dot(w), true_mean)
+        # print((function(x).dot(w)-true_mean**2, true_variance))
+        assert x.min() >= 0
+        assert np.allclose(function(x).dot(w)-true_mean**2, true_variance)
 
 
 if __name__ == "__main__":

pyapprox/univariate_polynomials/tests/test_recursion_factory.py
@@ -13,6 +13,8 @@ from pyapprox.univariate_polynomials.numeric_orthonormal_recursions import \
     native_recursion_integrate_fun
 from pyapprox.univariate_polynomials.orthonormal_polynomials import \
     evaluate_orthonormal_polynomial_1d
+from pyapprox.univariate_polynomials.orthonormal_recursions import \
+    laguerre_recurrence
 
 
 class TestRecursionFactory(unittest.TestCase):
@@ -22,7 +24,7 @@ class TestRecursionFactory(unittest.TestCase):
     def test_get_recursion_coefficients_from_variable_discrete(self):
         degree = 4
         N = 10
-        
+
         discrete_var_names = [
             "binom", "bernoulli", "nbinom", "geom", "hypergeom", "logser",
             "poisson", "planck", "boltzmann", "randint", "zipf",
@@ -208,6 +210,30 @@ class TestRecursionFactory(unittest.TestCase):
             # import matplotlib.pyplot as plt
             # plt.semilogy(xx, var.pdf(xx))
             # plt.show()
+
+    def test_special_cases(self):
+        a = 3
+        rho = a-1
+        degree = 5
+        ab = laguerre_recurrence(rho, degree+1, probability=True)
+        print(ab)
+        var = stats.gamma(a)
+
+        tol = 1e-8
+        quad_opts = {"epsrel": tol, "epsabs": tol, "limit": 100}
+        opts = {"numeric": True, "quad_options": quad_opts}
+        ab_numeric = get_recursion_coefficients_from_variable(
+            var, degree+1, opts)
+
+        # The last coefficient of ab[:, 0] is never used so it is not computed
+        # by the numerical routine
+        assert np.allclose(ab_numeric, ab)
+
+        var = stats.expon()
+        ab = laguerre_recurrence(0, degree+1, probability=True)
+        ab_numeric = get_recursion_coefficients_from_variable(
+            var, degree+1, opts)
+        assert np.allclose(ab_numeric, ab)
 
 
 if __name__ == "__main__":