ca4bd572410aef556971558dafc2d9abe5dead67
John Jakeman <jdjakem@users.noreply.github.com>
2022-06-01 17:40:01 -0600

    Add code that uses automatic differentiation to solve PDEs using spectral collocation

---
+++ pyapprox/pde/autopde/autopde.py
@@ -0,0 +1,452 @@
+import torch
+from abc import ABC, abstractmethod
+import numpy as np
+
+from pyapprox.util.utilities import cartesian_product
+from pyapprox.variables.transforms import _map_hypercube_samples
+from pyapprox.surrogates.orthopoly.quadrature import gauss_jacobi_pts_wts_1D
+from pyapprox.surrogates.interp.barycentric_interpolation import (
+    compute_barycentric_weights_1d, barycentric_interpolation_1d,
+    multivariate_barycentric_lagrange_interpolation
+)
+from pyapprox.pde.spectralcollocation.spectral_collocation import (
+    chebyshev_derivative_matrix
+)
+from pyapprox.util.visualization import get_meshgrid_function_data, plt
+
+
+class IntervalMeshBoundary():
+    def __init__(self, bndry_name, inactive_coord, tol=1e-15):
+        self._bndry_index = {"left": 0, "right": 1}[bndry_name]
+        self._inactive_coord = inactive_coord
+        self._tol = tol
+
+    def bndry_normals(self, samples):
+        normals = np.array([[-1], [1]])
+        return np.tile(normals[self._bndry_index, (1, samples.shape[1])]).T
+
+    def quadrature_rule(self):
+        return np.ones((1, 1)), np.ones((1, 1))
+
+    def samples_on_boundary(self, samples):
+        return np.where(
+            np.absolute(self._inactive_coord-samples[0, :]) < self._tol)[0]
+
+
+class RectangularMeshBoundary():
+    def __init__(self, bndry_name, inactive_coord, order, active_bounds,
+                 tol=1e-15):
+        if len(active_bounds) != 2:
+            msg = "Bounds must be specfied for the dimension with the "
+            msg += "varying coordinates"
+            raise ValueError(msg)
+
+        self._bndry_index = {"left": 0, "right": 1, "bottom": 2, "top": 3}[
+            bndry_name]
+        self._order = order
+        self._active_bounds = active_bounds
+        self._inactive_coord = inactive_coord
+        self._tol = tol
+
+    def bndry_normals(self, samples):
+        normals = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
+        return np.tile(normals[self._bndry_index, (1, samples.shape[1])]).T
+
+    def quadrature_rule(self):
+        nsamples = self._order+3
+        xquad_1d, wquad_1d = gauss_jacobi_pts_wts_1D(nsamples, 0, 0)
+        xlist = [None, None]
+        xlist[int(self._bndry_index < 2)] = xquad_1d
+        xlist[int(self._bndry_index >= 2)] = self._inactive_coord
+        xquad = cartesian_product(xlist)
+        wquad = wquad_1d[:, None]*np.prod(self.bounds[1]-self._bounds[0])
+        return xquad, wquad
+
+    def samples_on_boundary(self, samples):
+        dd = int(self._bndry_index >= 2)
+        indices = np.where(
+            np.absolute(self._inactive_coord-samples[dd, :]) < self._tol)[0]
+        return indices
+
+
+class CartesianProductCollocationMesh():
+    def __init__(self, domain_bounds, orders, bndry_conds):
+
+        if len(orders) != len(domain_bounds)//2:
+            raise ValueError("Order must be specified for each dimension")
+        if len(orders) > 2:
+            raise ValueError("Only 1D and 2D meshes supported")
+
+        super().__init__()
+        self._domain_bounds = np.asarray(domain_bounds)
+        self._orders = orders
+        self.nphys_vars = len(self._domain_bounds)//2
+
+        self._canonical_domain_bounds = np.ones(2*self.nphys_vars)
+        self._canonical_domain_bounds[::2] = -1.
+
+        (self._canonical_mesh_pts_1d, self._canonical_derivative_matrices_1d,
+         self._canonical_mesh_pts_1d_baryc_weights, self.mesh_pts,
+         self._derivative_matrices) = (
+             self._form_derivative_matrices())
+
+        self._bndrys = self._form_boundaries()
+        self._bndry_indices = self._determine_boundary_indices()
+        if len(self._bndrys) != len(bndry_conds):
+            raise ValueError(
+                "Incorrect number of boundary conditions provided")
+        self._bndry_conds = bndry_conds
+
+    def _form_boundaries(self):
+        if self.nphys_vars == 1:
+            return [IntervalMeshBoundary(name, inactive_coord)
+                    for name, inactive_coord in zip(
+                            ["left", "right"], self._domain_bounds)]
+        return [
+            RectangularMeshBoundary(
+                name, self._domain_bounds[ii], self._orders[int(ii < 2)],
+                self._domain_bounds[2*int(ii < 2): 2*int(ii < 2)+2])
+            for ii, name in enumerate(["left", "right", "bottom", "top"])]
+
+    def _determine_boundary_indices(self):
+        bndry_indices = [[] for ii in range(2*self.nphys_vars)]
+        for ii in range(2*self.nphys_vars):
+            bndry_indices[ii] = self._bndrys[ii].samples_on_boundary(
+                self.mesh_pts)
+        return bndry_indices
+
+    def _map_samples_from_canonical_domain(self, canonical_samples):
+        return _map_hypercube_samples(
+            canonical_samples, self._canonical_domain_bounds,
+            self._domain_bounds)
+
+    def _map_samples_to_canonical_domain(self, samples):
+        return _map_hypercube_samples(
+            samples, self._domain_bounds, self._canonical_domain_bounds)
+
+    @staticmethod
+    def _form_1d_derivative_matrices(order):
+        return chebyshev_derivative_matrix(order)
+
+    def _form_derivative_matrices(self):
+        canonical_mesh_pts_1d, canonical_derivative_matrices_1d = [], []
+        for ii in range(self.nphys_vars):
+            mpts, der_mat = self._form_1d_derivative_matrices(self._orders[ii])
+            canonical_mesh_pts_1d.append(mpts)
+            canonical_derivative_matrices_1d.append(der_mat)
+
+        canonical_mesh_pts_1d_baryc_weights = [
+            compute_barycentric_weights_1d(xx) for xx in canonical_mesh_pts_1d]
+
+        mesh_pts = self._map_samples_from_canonical_domain(
+            cartesian_product(canonical_mesh_pts_1d))
+
+        if self.nphys_vars == 1:
+            derivative_matrices = [
+                canonical_derivative_matrices_1d[0]*2./(
+                    self._domain_bounds[1]-self._domain_bounds[0])]
+        else:
+            ident_mats = [np.eye(o+1) for o in self._orders]
+            # assumes that 2d-mesh_pts varies in x1 faster than x2,
+            # e.g. points are
+            # [[x11,x21],[x12,x21],[x13,x12],[x11,x22],[x12,x22],...]
+            derivative_matrices = [
+                np.kron(np.eye(self._orders[1]+1),
+                        canonical_derivative_matrices_1d[0]*2./(
+                            self._domain_bounds[1]-self._domain_bounds[0])),
+                np.kron(canonical_derivative_matrices_1d[1]*2./(
+                    self._domain_bounds[3]-self._domain_bounds[2]),
+                        np.eye(self._orders[0]+1))]
+        derivative_matrices = [torch.tensor(mat) for mat in derivative_matrices]
+
+        return (canonical_mesh_pts_1d, canonical_derivative_matrices_1d,
+                canonical_mesh_pts_1d_baryc_weights,
+                mesh_pts, derivative_matrices)
+
+    def _interpolate(self, canonical_abscissa_1d,
+                     canonical_barycentric_weights_1d,
+                     values, eval_samples):
+        if eval_samples.ndim == 1:
+            eval_samples = eval_samples[None, :]
+            assert eval_samples.shape[0] == self.mesh_pts.shape[0]
+        if values.ndim == 1:
+            values = values[:, None]
+            assert values.ndim == 2
+        canonical_eval_samples = self._map_samples_to_canonical_domain(
+            eval_samples)
+        interp_vals = multivariate_barycentric_lagrange_interpolation(
+            canonical_eval_samples, canonical_abscissa_1d,
+            canonical_barycentric_weights_1d, values,
+            np.arange(self.nphys_vars))
+        return interp_vals
+
+    def interpolate(self, mesh_values, eval_samples):
+        canonical_abscissa_1d = self._canonical_mesh_pts_1d
+        return self._interpolate(
+            canonical_abscissa_1d, self._canonical_mesh_pts_1d_baryc_weights,
+            mesh_values, eval_samples)
+
+    def _plot_2d(self, mesh_values, num_pts_1d=100, ncontour_levels=20,
+                 ax=None):
+        if ax is None:
+            ax = plt.subplots(1, 1, figsize=(8, 6))[1]
+        # interpolate values onto plot points
+
+        def fun(x):
+            return self.interpolate(mesh_values, x)
+
+        X, Y, Z = get_meshgrid_function_data(
+            fun, self._domain_bounds, num_pts_1d, qoi=0)
+        return ax.contourf(
+            X, Y, Z, levels=np.linspace(Z.min(), Z.max(), ncontour_levels))
+
+    def _plot_1d(self, mesh_values, nplot_pts_1d=None, ax=None,
+                 **kwargs):
+        if ax is None:
+            ax = plt.subplots(1, 1, figsize=(8, 6))[1]
+
+        if nplot_pts_1d is not None:
+            # interpolate values onto plot points
+            plot_mesh = np.linspace(
+                self._domain_bounds[0], self._domain_bounds[1], nplot_pts_1d)
+            interp_vals = self.interpolate(mesh_values, plot_mesh)
+            return ax.plot(plot_mesh, interp_vals, **kwargs)
+        # just plot values on mesh points
+        return ax.plot(self.mesh_pts[0, :], mesh_values, **kwargs)
+
+    def plot(self, mesh_values, nplot_pts_1d=None, ax=None,
+             **kwargs):
+        if self.nphys_vars == 1:
+            return self._plot_1d(
+                mesh_values, nplot_pts_1d, ax, **kwargs)
+        return self._plot_2d(
+            mesh_values, nplot_pts_1d, 30, ax=None)
+
+    def _apply_dirichlet_boundary_conditions_to_residual(self, residual, sol):
+        for ii, bndry_cond in enumerate(self._bndry_conds):
+            if bndry_cond[1] == "D":
+                idx = self._bndry_indices[ii]
+                residual[idx] = sol[idx]-bndry_cond[0](
+                    self.mesh_pts[:, idx])[:, 0]
+        return residual
+
+    def _apply_neumann_and_robin_boundary_conditions_to_residual(
+            self, residual, sol):
+        for ii, bndry_cond in enumerate(self._bndry_conds):
+            if bndry_cond[1] == "N" or bndry_cond[1] == "R":
+                idx = self._bndry_indices[ii]
+                bndry_vals = bndry_cond[0](self.mesh_pts[:, idx])[:, 0]
+                normal = (-1)**(ii+1)
+                if ii < 2:
+                    # warning flux is not dependent on diffusivity (
+                    # diffusion equation not a standard boundary formulation)
+                    flux = torch.linalg.multi_dot(
+                        (self._derivative_matrices[0][idx, :], sol))
+                else:
+                    flux = torch.linalg.multi_dot(
+                        (self._derivative_matrices[1][idx, :], sol))
+                residual[idx] = normal*flux-bndry_vals
+                if bndry_cond[1] == "R":
+                    residual[idx] += bndry_cond[2]*sol[idx]
+        return residual
+
+    def _apply_boundary_conditions_to_residual(self, residual, sol):
+        residual = self._apply_dirichlet_boundary_conditions_to_residual(
+            residual, sol)
+        residual = (
+            self._apply_neumann_and_robin_boundary_conditions_to_residual(
+                residual, sol))
+        return residual
+
+
+class AbstractFunction(ABC):
+    def __init__(self, requires_grad=False):
+        self._requires_grad = requires_grad
+
+    @abstractmethod
+    def _eval(self, samples):
+        raise NotImplementedError()
+
+    def __call__(self, samples):
+        vals = self._eval(samples)
+        if vals.ndim != 2:
+            raise ValueError("Function must return a 2D np.ndarray")
+        return torch.tensor(vals, requires_grad=self._requires_grad)
+
+
+class Function(AbstractFunction):
+    def __init__(self, fun, requires_grad=False):
+        super().__init__(requires_grad)
+        self._fun = fun
+
+    def _eval(self, samples):
+        return self._fun(samples)
+
+
+class AbstractSpectralCollocationSolver(ABC):
+    def __init__(self, mesh):
+        self.mesh = mesh
+
+    @abstractmethod
+    def _raw_residual(self, sol):
+        raise NotImplementedError()
+
+    def _residual(self, sol):
+        # correct equations for boundary conditions
+        raw_residual = self._raw_residual(sol)
+        return self.mesh._apply_boundary_conditions_to_residual(
+            raw_residual, sol)
+
+    def _compute_jacobian(self, residual_fun, sol):
+        assert sol.requires_grad
+        return torch.autograd.functional.jacobian(
+            residual_fun, sol, strict=True)#, create_graph=True)
+
+    def solve(self, initial_guess, tol=1e-8, maxiters=10,
+              verbosity=2):
+        initial_guess = initial_guess.squeeze()
+        if type(initial_guess) == np.ndarray:
+            sol = torch.tensor(initial_guess.clone(), requires_grad=True)
+        else:
+            sol = initial_guess.clone().detach().requires_grad_(True)
+        residual_norms = []
+        it = 0
+        while True:
+            residual = self._residual(sol)
+            residual_norm = torch.linalg.norm(residual)
+            residual_norms.append(residual_norm)
+            if verbosity > 1:
+                print("Iter", it, "rnorm", residual_norm.detach().numpy())
+            if residual_norm < tol:
+                exit_msg = f"Tolerance {tol} reached"
+                break
+            if it > maxiters:
+                exit_msg = f"Max iterations {maxiters} reached"
+                break
+            if it > 4 and (residual_norm > residual_norms[it-5]):
+                raise RuntimeError("Newton solve diverged")
+            jac = self._compute_jacobian(self._residual, sol)
+            sol = sol-torch.linalg.solve(jac, residual)
+            it += 1
+
+        if verbosity > 0:
+            print(exit_msg)
+        return sol.detach().numpy()[:, None]
+
+
+class AdvectionDiffusionReaction(AbstractSpectralCollocationSolver):
+    def __init__(self, mesh, diff_fun, vel_fun, react_fun, forc_fun):
+        super().__init__(mesh)
+
+        self._diff_fun = diff_fun
+        self._vel_fun = vel_fun
+        self._react_fun = react_fun
+        self._forc_fun = forc_fun
+
+        self._diff_vals, self._vel_vals, self._forc_vals = (
+            self._precompute_data())
+
+    def _precompute_data(self):
+        return (self._diff_fun(self.mesh.mesh_pts),
+                self._vel_fun(self.mesh.mesh_pts),
+                self._forc_fun(self.mesh.mesh_pts))
+
+    def _raw_residual(self, sol):
+        # torch requires 1d arrays but to multiply each row of derivative
+        # matrix by diff_vals we must use [:, None] when computing residual
+        residual = 0
+        for dd in range(self.mesh.nphys_vars):
+            residual -= torch.linalg.multi_dot(
+                (self.mesh._derivative_matrices[dd],
+                 self._diff_vals*self.mesh._derivative_matrices[dd],
+                 sol))
+            residual += torch.linalg.multi_dot(
+                (self._vel_vals[:, dd:dd+1]*self.mesh._derivative_matrices[dd],
+                 sol))
+        residual += self._react_fun(sol)
+        residual -= self._forc_vals[:, 0]
+        return residual
+
+    def solve(self):
+        init_guess = torch.ones(
+            (self.mesh.mesh_pts.shape[1], 1), dtype=torch.double)
+        return super().solve(init_guess)
+
+
+class EulerBernoulliBeam(AbstractSpectralCollocationSolver):
+    def __init__(self, mesh, emod_fun, smom_fun, forc_fun):
+        if mesh.nphys_vars > 1:
+            raise ValueError("Only 1D meshes supported")
+        
+        super().__init__(mesh)
+
+        self._emod_fun = emod_fun
+        self._smom_fun = smom_fun
+        self._forc_fun = forc_fun
+
+        self._emod_vals, self._smom_vals, self._forc_vals = (
+            self._precompute_data())
+
+    def _precompute_data(self):
+        return (self._emod_fun(self.mesh.mesh_pts),
+                self._smom_fun(self.mesh.mesh_pts),
+                self._forc_fun(self.mesh.mesh_pts))
+
+    def _raw_residual(self, sol):
+        # torch requires 1d arrays but to multiply each row of derivative
+        # matrix by diff_vals we must use [:, None] when computing residual
+        residual = 0
+        dmat = self.mesh._derivative_matrices[0]
+        print(self._forc_vals.shape)
+        residual = torch.linalg.multi_dot(
+            (dmat, dmat, self._emod_vals*self._smom_vals*dmat, dmat, sol))
+        residual -= self._forc_vals[:, 0]
+        return residual
+
+    def _residual(self, sol):
+        # correct equations for boundary conditions
+        raw_residual = self._raw_residual(sol)
+        dmat = self.mesh._derivative_matrices[0]
+        print(sol.shape, dmat.shape)
+        raw_residual[0] = sol[0]-0
+        raw_residual[1] = torch.linalg.multi_dot((dmat[0, :], sol))-0
+        raw_residual[-1] = torch.linalg.multi_dot((torch.linalg.multi_dot(
+            (dmat, dmat))[-1, :], sol))-0
+        raw_residual[-2] = torch.linalg.multi_dot((torch.linalg.multi_dot(
+            (dmat, dmat, dmat))[-1, :], sol))-0
+        return raw_residual
+
+    def solve(self):
+        init_guess = torch.ones(
+            (self.mesh.mesh_pts.shape[1], 1), dtype=torch.double)
+        return super().solve(init_guess)
+
+
+class Helmholtz(AbstractSpectralCollocationSolver):
+    def __init__(self, mesh, wnum_fun, forc_fun):
+        super().__init__(mesh)
+
+        self._wnum_fun = wnum_fun
+        self._forc_fun = forc_fun
+        self._wnum_vals, self._forc_vals = self._precompute_data()
+
+    def _precompute_data(self):
+        return (self._wnum_fun(self.mesh.mesh_pts),
+                self._forc_fun(self.mesh.mesh_pts))
+
+    def _raw_residual(self, sol):
+        # torch requires 1d arrays but to multiply each row of derivative
+        # matrix by diff_vals we must use [:, None] when computing residual
+        residual = 0
+        for dd in range(self.mesh.nphys_vars):
+            dmat = self.mesh._derivative_matrices[dd]
+            residual += torch.linalg.multi_dot((dmat, dmat, sol))
+        residual += self._wnum_vals[:, 0]*sol
+        residual -= self._forc_vals[:, 0]
+        return residual
+
+    def solve(self):
+        init_guess = torch.ones(
+            (self.mesh.mesh_pts.shape[1], 1), dtype=torch.double)
+        return super().solve(init_guess)
+    
---
+++ pyapprox/pde/autopde/test_autopde.py
@@ -0,0 +1,184 @@
+import torch
+import unittest
+import numpy as np
+from functools import partial
+
+from pyapprox.pde.autopde.manufactured_solutions import (
+    setup_steady_advection_diffusion_reaction_manufactured_solution,
+    setup_helmholtz_manufactured_solution
+)
+from pyapprox.pde.autopde.autopde import (
+    CartesianProductCollocationMesh, AdvectionDiffusionReaction,
+    Function, EulerBernoulliBeam, Helmholtz
+)
+
+def normal_flux(flux_funs, active_var, sign, xx):
+    vals = sign*flux_funs(xx)[:, active_var:active_var+1]
+    return vals
+
+
+def robin_bndry_fun(sol_fun, flux_funs, active_var, sign, alpha, xx):
+    vals = alpha*sol_fun(xx) + normal_flux(flux_funs, active_var, sign, xx)
+    return vals
+
+
+class TestAutoPDE(unittest.TestCase):
+    def setUp(self):
+      torch.manual_seed(1)
+      np.random.seed(1)
+
+    def check_advection_diffusion_reaction(
+            self, domain_bounds, orders, sol_string, diff_string, vel_strings,
+            react_fun, bndry_types):
+        sol_fun, diff_fun, vel_fun, forc_fun, flux_funs = (
+            setup_steady_advection_diffusion_reaction_manufactured_solution(
+                sol_string, diff_string, vel_strings, react_fun))
+
+        diff_fun = Function(diff_fun)
+        vel_fun = Function(vel_fun)
+        forc_fun = Function(forc_fun)
+        sol_fun = Function(sol_fun)
+
+        nphys_vars = len(orders)
+        bndry_conds = []
+        for dd in range(2*nphys_vars):
+            if bndry_types[dd] == "D":
+                bndry_conds.append([sol_fun, "D"])
+            else:
+                if bndry_types[dd] == "R":
+                    alpha = 1
+                else:
+                    alpha = 0
+                bndry_conds.append(
+                    [partial(robin_bndry_fun, sol_fun, flux_funs, dd//2,
+                             (-1)**(dd+1), alpha), "R", alpha])
+
+        mesh = CartesianProductCollocationMesh(
+            domain_bounds, orders, bndry_conds)
+        solver = AdvectionDiffusionReaction(
+            mesh, diff_fun, vel_fun, react_fun, forc_fun)
+        sol = solver.solve()
+
+        # import matplotlib.pyplot as plt
+        # ax = plt.subplots(1, 1, figsize=(8, 6))[1]
+        # solver.mesh.plot(sol, ax=ax, marker='o', ls=None)
+        # solver.mesh.plot(
+        #     sol_fun(solver.mesh.mesh_pts).numpy(), ax=ax, marker='s',
+        #     ls=None)
+        # solver.mesh.plot(sol, ax=ax, nplot_pts_1d=100, label="Collocation")
+        # solver.mesh.plot(
+        #     sol_fun(solver.mesh.mesh_pts).numpy(), ax=ax, nplot_pts_1d=100,
+        #     ls="--", label="Exact")
+        # plt.legend()
+        # plt.show()
+
+        print(np.linalg.norm(
+            sol_fun(solver.mesh.mesh_pts)-sol))
+        assert np.linalg.norm(
+            sol_fun(solver.mesh.mesh_pts)-sol) < 1e-9
+
+        
+        # normals = solver.mesh._get_bndry_normals(np.arange(nphys_vars*2))
+        # if nphys_vars == 2:
+        #     assert np.allclose(
+        #         normals, np.array([[-1, 0], [1, 0], [0, -1], [0, 1]]))
+        # else:
+        #     assert np.allclose(normals, np.array([[-1], [1]]))
+        # normal_fluxes = solver.compute_bndry_fluxes(
+        #     sol, np.arange(nphys_vars*2))
+        # for ii, indices in enumerate(solver.mesh.boundary_indices):
+        #     assert np.allclose(
+        #         np.array(normal_fluxes[ii]),
+        #         flux_funs(solver.mesh.mesh_pts[:, indices],).dot(
+        #             normals[ii]))
+    
+    def test_advection_diffusion_reaction(self):
+        test_cases = [
+            [[0, 1], [4], "0.5*(x-3)*x", "1", ["0"], lambda x: 0*x**2,
+             ["D", "D"]],
+            [[0, 1], [4], "0.5*(x-3)*x", "1", ["0"], lambda x: 0*x**2,
+             ["N", "D"]],
+            [[0, 1], [4], "0.5*(x-3)*x", "1", ["0"], lambda x: 0*x**2,
+             ["R", "D"]],
+            [[0, 1], [4], "0.5*(x-3)*x", "1", ["0"], lambda x: x**2,
+             ["D", "D"]],
+            [[0, 1, 0, 1], [4, 4], "y**2*x**2", "1", ["0", "0"],
+             lambda x: 0*x**2, ["D", "N", "N", "D"]],
+            [[0, .5, 0, 1], [14, 16], "y**2*sin(pi*x)", "1", ["0", "0"],
+             lambda x: 0*x**2, ["D", "N", "N", "D"]],
+            [[0, .5, 0, 1], [16, 16], "y**2*sin(pi*x)", "1", ["0", "0"],
+             lambda x: 0*x**2, ["D", "R", "D", "D"]]
+        ]
+        for test_case in test_cases:
+            self.check_advection_diffusion_reaction(*test_case)
+
+    def test_euler_bernoulli_beam(self):
+        # bndry_conds are None because they are imposed in the solver
+        # This 4th order equation requires 4 boundary conditions, two at each
+        # end of the domain. This cannot be done with usual boundary condition
+        # functions and msut be imposed on the residual exactly
+        domain_bounds, orders, bndry_conds = [0, 1], [4], [None]*2
+        emod_val, smom_val, forcing_val = 1., 1., -2. 
+        mesh = CartesianProductCollocationMesh(
+            domain_bounds, orders, bndry_conds)
+        solver = EulerBernoulliBeam(
+            mesh, Function(lambda x: np.full((x.shape[1], 1), 1)),
+            Function(lambda x: np.full((x.shape[1], 1), 1)),
+            Function(lambda x: np.full((x.shape[1], 1), forcing_val)))
+        sol = solver.solve()
+
+        def sol_fun(x):
+            length = domain_bounds[1]-domain_bounds[0]
+            return (forcing_val*x**2*(6*length**2-4*length*x+x**2)/(
+                24*emod_val*smom_val)).T
+        assert np.allclose(sol, sol_fun(solver.mesh.mesh_pts))
+        solver.mesh.plot(sol, nplot_pts_1d=100)
+        import matplotlib.pyplot as plt
+        plt.show()
+
+    def check_helmholtz(self, domain_bounds, orders, sol_string, wnum_string,
+                        bndry_types):
+        sol_fun, wnum_fun, forc_fun, flux_funs = (
+            setup_helmholtz_manufactured_solution(
+                sol_string, wnum_string, len(domain_bounds)//2))
+
+        wnum_fun = Function(wnum_fun)
+        forc_fun = Function(forc_fun)
+        sol_fun = Function(sol_fun)
+
+        nphys_vars = len(orders)
+        bndry_conds = []
+        for dd in range(2*nphys_vars):
+            if bndry_types[dd] == "D":
+                bndry_conds.append([sol_fun, "D"])
+            else:
+                if bndry_types[dd] == "R":
+                    alpha = 1
+                else:
+                    alpha = 0
+                bndry_conds.append(
+                    [partial(robin_bndry_fun, sol_fun, flux_funs, dd//2,
+                             (-1)**(dd+1), alpha), "R", alpha])
+
+        mesh = CartesianProductCollocationMesh(
+            domain_bounds, orders, bndry_conds)
+        solver = Helmholtz(mesh, wnum_fun, forc_fun)
+        sol = solver.solve()
+
+        print(np.linalg.norm(
+            sol_fun(solver.mesh.mesh_pts)-sol))
+        assert np.linalg.norm(
+            sol_fun(solver.mesh.mesh_pts)-sol) < 1e-9
+
+    def test_helmholtz(self):
+        test_cases = [
+            [[0, 1], [16], "x**2", "1", ["N", "D"]],
+            [[0, .5, 0, 1], [16, 16], "y**2*x**2", "1", ["N", "D", "D", "D"]]]
+        for test_case in test_cases:
+            self.check_helmholtz(*test_case)
+        
+
+if __name__ == "__main__":
+    autopde_test_suite = \
+        unittest.TestLoader().loadTestsFromTestCase(TestAutoPDE)
+    unittest.TextTestRunner(verbosity=2).run(autopde_test_suite)
