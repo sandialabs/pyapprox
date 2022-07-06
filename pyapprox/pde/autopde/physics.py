Implement correct shallow shelf velocity jacobians

pyapprox/pde/autopde/physics.py
@@ -420,8 +420,8 @@ class ShallowShelfVelocities(AbstractSpectralCollocationPhysics):
 
     def _effective_strain_rate_2d(self, dudx_ij):
         return (dudx_ij[0][0]**2 + dudx_ij[1][1]**2+dudx_ij[0][0]*dudx_ij[1][1]
-               + (dudx_ij[0][1]+dudx_ij[1][0])**2/4+self._homotopy_val)**(1/2)
-        
+                + (dudx_ij[0][1]+dudx_ij[1][0])**2/4+self._homotopy_val)**(1/2)
+
     def _effective_strain_rate(self, dudx_ij):
         if self.mesh.nphys_vars == 2:
             return self._effective_strain_rate_2d(dudx_ij)
@@ -434,7 +434,7 @@ class ShallowShelfVelocities(AbstractSpectralCollocationPhysics):
         # d/du (ux**2 + vy**2 + ux*vy + 0.25*(uy+vx)**2)**(1/2)
         # = 0.5*srate**(-0.5)*d/du(ux**2+...)
         srate = self._effective_strain_rate(dudx_ij)
-        dmats = [self.mesh._meshes[0]._dmat(dd) for dd in range(self.mesh.nphys_vars)] 
+        dmats = [self.mesh._meshes[0]._dmat(dd) for dd in range(self.mesh.nphys_vars)]
         tmp = [2*dudx_ij[dd][dd][:, None]*dmats[dd]
                for dd in range(self.mesh.nphys_vars)]
         if self.mesh.nphys_vars == 2:
@@ -469,11 +469,12 @@ class ShallowShelfVelocities(AbstractSpectralCollocationPhysics):
         dmats = [self.mesh._meshes[0]._dmat(dd)
                  for dd in range(self.mesh.nphys_vars)]
         if self.mesh.nphys_vars == 1:
-            return [[2*dmats[0]]]
-        vec1_jac = [2*dmats[0], dmats[1]/2]
-        vec2_jac = [dmats[0], 2*dmats[1]]
+            return ([[2*dmats[0]]], )
+        # vec1_jac = [[dv1[0]/du, dv1[1]/du], dv1[0]/dv, dv[1]/dv]
+        vec1_jac = [[2*dmats[0], dmats[1]/2], [dmats[1], dmats[0]/2]]
+        vec2_jac = [[dmats[1]/2, dmats[0]], [dmats[0]/2, 2*dmats[1]]]
         return vec1_jac, vec2_jac
-        
+
     def _raw_residual_nD(self, split_sols, depth_vals):
         pderiv = self.mesh._meshes[0].partial_deriv
         div = self.mesh._meshes[0].div
@@ -502,14 +503,17 @@ class ShallowShelfVelocities(AbstractSpectralCollocationPhysics):
         dmats = [self.mesh._meshes[0]._dmat(dd)
                  for dd in range(self.mesh.nphys_vars)]
         jac = []
-        for dd in range(self.mesh.nphys_vars): # loop over jacobian row blocks
-            jac.append([0 for ii in range(self.mesh.nphys_vars)])
-            for jj in range(self.mesh.nphys_vars): # compute columns of jacobian row block
-                for ii in range(self.mesh.nphys_vars): # compute divergence
-                    jac[dd][jj] += -multi_dot((dmats[ii], (
-                        (2*depth_vals[:, 0:1]*vecs[dd][:, ii:ii+1]*visc_jac[jj]) +
-                        ((2*visc*depth_vals[:, 0])[:, None])*vecs_jac[dd][jj])))
-            jac[dd][dd] += torch.diag(self._beta_vals[:, 0])
+        # loop over jacobian rows (blocks)
+        for ii in range(self.mesh.nphys_vars):
+            jac.append([0 for jj in range(self.mesh.nphys_vars)])
+            # loop over jacobian columns (blocks)
+            for jj in range(self.mesh.nphys_vars):
+                # loop over phys_vars to compute divergence
+                for dd in range(self.mesh.nphys_vars):
+                    jac[ii][jj] += -multi_dot((dmats[dd], (
+                        (2*depth_vals[:, :1]*vecs[ii][:, dd:dd+1]*visc_jac[jj]) +
+                        ((2*visc*depth_vals[:, 0])[:, None])*vecs_jac[ii][jj][dd])))
+            jac[ii][ii] += torch.diag(self._beta_vals[:, 0])
         return torch.vstack([torch.hstack(j) for j in jac])
 
     def _raw_residual(self, sol):
@@ -641,5 +645,3 @@ class FirstOrderStokesIce(AbstractSpectralCollocationPhysics):
         if bndry_index == 2:
             return vals + self._beta_vals[idx, 0]*sol[idx]
         return vals
-
-

pyapprox/pde/autopde/test_manual_pde.py
@@ -221,7 +221,6 @@ class TestManualPDE(unittest.TestCase):
         for test_case in test_cases:
             self._check_advection_diffusion_reaction(*test_case)
 
-
     def _check_transient_advection_diffusion_reaction(
             self, domain_bounds, orders, sol_string,
             diff_string, vel_strings, react_funs, bndry_types,
@@ -462,7 +461,7 @@ class TestManualPDE(unittest.TestCase):
         # print(j_auto)
         # print(j_man)
         assert np.allclose(j_auto, j_man)
-        
+
         assert np.allclose(
             solver.residual._raw_residual(exact_sol[:, 0])[0], 0, atol=2e-8)
         assert np.allclose(
@@ -802,7 +801,7 @@ class TestManualPDE(unittest.TestCase):
             init_guess = torch.randn(init_guess.shape, dtype=torch.double)*0
         else:
             init_guess = (init_guess+torch.randn(init_guess.shape)*5e-3)
-        
+
         np.set_printoptions(precision=2, suppress=True, threshold=100000, linewidth=1000)
         dudx_ij = solver.residual._derivs(mesh.split_quantities(init_guess[:, 0]))
         j_visc_man = torch.hstack(solver.residual._viscosity_jac(dudx_ij))
@@ -814,19 +813,26 @@ class TestManualPDE(unittest.TestCase):
         # print(j_visc_auto)
         assert np.allclose(j_visc_auto, j_visc_man.numpy())
 
-        j_man = solver.residual._vector_components_jac(dudx_ij)[0][0]
-        j_auto = torch.autograd.functional.jacobian(
-            lambda s: solver.residual._vector_components(solver.residual._derivs(mesh.split_quantities(s)))[0][:, 0],
-            init_guess[:, 0].clone().requires_grad_(True), strict=True).numpy()
-        assert np.allclose(j_man, j_auto[:, :j_man.shape[1]])
+        j_man = solver.residual._vector_components_jac(dudx_ij)
+        for ii in range(nphys_vars):
+            for jj in range(nphys_vars):
+                j_auto = torch.autograd.functional.jacobian(
+                    lambda s: solver.residual._vector_components(
+                        solver.residual._derivs(
+                            mesh.split_quantities(s)))[ii][:, jj],
+                    init_guess[:, 0].clone().requires_grad_(True),
+                    strict=True).numpy()
+                for dd in range(nphys_vars):
+                    cnt = j_man[ii][dd][jj].shape[1]
+                    assert np.allclose(j_man[ii][dd][jj].numpy(),
+                                       j_auto[:, dd*cnt:(dd+1)*cnt])
 
         j_man = solver.residual._raw_residual(init_guess.squeeze())[1]
         j_auto = torch.autograd.functional.jacobian(
             lambda s: solver.residual._raw_residual(s)[0],
             init_guess[:, 0].clone().requires_grad_(True), strict=True).numpy()
-        print((j_man-j_auto).numpy()[:16, :16])
         assert np.allclose(j_man, j_auto)
-        
+
         sol = solver.solve(init_guess, tol=1e-7, verbosity=2, maxiters=100)
         split_sols = mesh.split_quantities(sol)
         for exact_v, v in zip(exact_vel_vals, split_sols):
@@ -849,13 +855,14 @@ class TestManualPDE(unittest.TestCase):
              ["D", "D"], True],
             [[0, 1], [9], ["(x+2)**2"], "1+x**2", "-x**2", "1",
              ["D", "D"], False],
-            [[0, 1, 0, 1], [3, 3], ["(x+1)**2", "(y+1)*(x+1)"], "1+x+y", # [10, 10]
+            [[0, 1, 0, 1], [10, 10],
+             ["(x+1)**2*(y+1)", "(y+1)*(x+1)"], "1+x+y",
              "1+x+y**2", "1", ["D", "D", "D", "D"], True],
             [[0, 2, 0, 2], [15, 15], ["(x+1)**2", "(y+1)**2"], "1+x+y",
              "0-x-y", "1", ["D", "D", "D", "D"], False]
         ]
         # may need to setup backtracking for Newtons method
-        for test_case in test_cases[2:3]:
+        for test_case in test_cases[::2]:
             self._check_shallow_shelf_solver_mms(*test_case)
 
     def test_first_order_stokes_ice_mms(self):