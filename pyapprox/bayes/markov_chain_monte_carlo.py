50a72e64431193e9ff9127aee65e5de2c1f9201a
John Jakeman <jdjakem@users.noreply.github.com>
2022-07-18 14:22:46 -0600

    make pymc3 a required dependency and move wrappers back out of pyapprox_dev

--- pyapprox/bayes/markov_chain_monte_carlo.py
+++ pyapprox/bayes/markov_chain_monte_carlo.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
 import pymc3 as pm
 import numpy as np
-import theano
 import theano.tensor as tt
 from scipy.optimize import approx_fprime
 from pyapprox.variables.marginals import get_distribution_info
@@ -12,7 +10,8 @@ class GaussianLogLike(object):
     A Gaussian log-likelihood function for a model with parameters given in 
     sample
     """
-    def __init__(self,model,data,noise_covar):
+
+    def __init__(self, model, data, noise_covar):
         r"""
         Initialise the Op with various things that our log-likelihood 
         function requires.
@@ -28,25 +27,25 @@ class GaussianLogLike(object):
         noise_covar : float, np.ndarray (nobs), np.ndarray (nobs,nobs)
             The noise covariance
         """
-        self.model=model
-        self.data=data
-        assert self.data.ndim==1
+        self.model = model
+        self.data = data
+        assert self.data.ndim == 1
         self.ndata = data.shape[0]
         self.noise_covar_inv = self.noise_covariance_inverse(noise_covar)
 
-    def noise_covariance_inverse(self,noise_covar):
+    def noise_covariance_inverse(self, noise_covar):
         if np.isscalar(noise_covar):
             inv_covar = 1/noise_covar
-        elif noise_covar.ndim==1:
-            assert noise_covar.shape[0]==self.data.shape[0]
+        elif noise_covar.ndim == 1:
+            assert noise_covar.shape[0] == self.data.shape[0]
             inv_covar = 1/noise_covar
-        elif noise_covar.ndim==2:
-            assert noise_covar.shape==[self.ndata,self.ndata]
+        elif noise_covar.ndim == 2:
+            assert noise_covar.shape == [self.ndata, self.ndata]
             inv_covar = np.linalg.inv(noise_covar)
         return inv_covar
 
     # def noise_covariance_determinant(self, noise_covar):
-    #     r"""The determinant is only necessary in log likelihood if the noise 
+    #     r"""The determinant is only necessary in log likelihood if the noise
     #     covariance has a hyper-parameter which is being inferred which is
     #     not currently supported"""
     #     if np.isscalar(noise_covar):
@@ -57,20 +56,21 @@ class GaussianLogLike(object):
     #         determinant = np.linalg.det(noise_covar)
     #     return determinant
 
-    def __call__(self,samples):
+    def __call__(self, samples):
         model_vals = self.model(samples)
-        assert model_vals.ndim==2
-        assert model_vals.shape[1]==self.ndata
-        vals = np.empty((model_vals.shape[0],1))
+        assert model_vals.ndim == 2
+        assert model_vals.shape[1] == self.ndata
+        vals = np.empty((model_vals.shape[0], 1))
         for ii in range(model_vals.shape[0]):
-            residual = self.data - model_vals[ii,:]
+            residual = self.data - model_vals[ii, :]
             if (np.isscalar(self.noise_covar_inv) or
-                self.noise_covar_inv.ndim==1):
-                vals[ii]=(residual.T*self.noise_covar_inv).dot(residual)
+                    self.noise_covar_inv.ndim == 1):
+                vals[ii] = (residual.T*self.noise_covar_inv).dot(residual)
             else:
-                vals[ii]=residual.T.dot(self.noise_covar_inv).dot(residual)
+                vals[ii] = residual.T.dot(self.noise_covar_inv).dot(residual)
         vals *= -0.5
         return vals
+
 
 class LogLike(tt.Op):
     r"""
@@ -79,24 +79,26 @@ class LogLike(tt.Op):
     values (the parameters that define our model) and returning a 
     single "scalar" value (the log-likelihood)
     """
-    itypes = [tt.dvector] # expects a vector of parameter values when called
-    otypes = [tt.dscalar] # outputs a single scalar value (the log likelihood)
+    itypes = [tt.dvector]  # expects a vector of parameter values when called
+    otypes = [tt.dscalar]  # outputs a single scalar value (the log likelihood)
 
     def __init__(self, loglike):
         # add inputs as class attributes
         self.likelihood = loglike
 
     def perform(self, node, inputs, outputs):
-        samples, = inputs # important
+        samples, = inputs  # important
         # call the log-likelihood function
         logl = self.likelihood(samples)
-        outputs[0][0] = np.array(logl) # output the log-likelihood
+        outputs[0][0] = np.array(logl)  # output the log-likelihood
 
 # define a theano Op for our likelihood function
+
+
 class LogLikeWithGrad(LogLike):
 
-    itypes = [tt.dvector] # expects a vector of parameter values when called
-    otypes = [tt.dscalar] # outputs a single scalar value (the log likelihood)
+    itypes = [tt.dvector]  # expects a vector of parameter values when called
+    otypes = [tt.dscalar]  # outputs a single scalar value (the log likelihood)
 
     def __init__(self, loglike, loglike_grad=None):
         r"""
@@ -113,15 +115,16 @@ class LogLikeWithGrad(LogLike):
         """
 
         super().__init__(loglike)
-        
+
         # initialise the gradient Op (below)
-        self.logpgrad = LogLikeGrad(self.likelihood,loglike_grad)
+        self.logpgrad = LogLikeGrad(self.likelihood, loglike_grad)
 
     def grad(self, inputs, g):
         # the method that calculates the gradients - it actually returns the
         # vector-Jacobian product - g[0] is a vector of parameter values
-        samples, = inputs # important
+        samples, = inputs  # important
         return [g[0]*self.logpgrad(samples)]
+
 
 class LogLikeGrad(tt.Op):
 
@@ -146,8 +149,8 @@ class LogLikeGrad(tt.Op):
         self.likelihood_grad = loglike_grad
 
     def perform(self, node, inputs, outputs):
-        samples,=inputs
-        
+        samples, = inputs
+
         # calculate gradients
         if self.likelihood_grad is None:
             # define version of likelihood function to pass to
@@ -155,59 +158,62 @@ class LogLikeGrad(tt.Op):
             def lnlike(values):
                 return self.likelihood(values)
             grads = approx_fprime(
-                samples,lnlike,2*np.sqrt(np.finfo(float).eps))
+                samples, lnlike, 2*np.sqrt(np.finfo(float).eps))
         else:
             grads = self.likelihood_grad(samples)
         outputs[0][0] = grads
 
-def extract_mcmc_chain_from_pymc3_trace(trace,var_names,ndraws,nburn,njobs):
+
+def extract_mcmc_chain_from_pymc3_trace(trace, var_names, ndraws, nburn, njobs):
     nvars = len(var_names)
-    samples = np.empty((nvars,(ndraws-nburn)*njobs))
+    samples = np.empty((nvars, (ndraws-nburn)*njobs))
     effective_sample_size = -np.ones(nvars)
     for ii in range(nvars):
-        samples[ii,:]=trace.get_values(
-            var_names[ii],burn=nburn,chains=np.arange(njobs))
+        samples[ii, :] = trace.get_values(
+            var_names[ii], burn=nburn, chains=np.arange(njobs))
         try:
-            effective_sample_size[ii]=pm.ess(trace)[var_names[ii]].values
+            effective_sample_size[ii] = pm.ess(trace)[var_names[ii]].values
         except:
             print('could not compute ess. likely issue with theano')
 
+    return samples, effective_sample_size
 
-    return samples,effective_sample_size
 
-def extract_map_sample_from_pymc3_dict(map_sample_dict,var_names):
+def extract_map_sample_from_pymc3_dict(map_sample_dict, var_names):
     nvars = len(var_names)
-    map_sample = np.empty((nvars,1))
+    map_sample = np.empty((nvars, 1))
     for ii in range(nvars):
-        map_sample[ii]=map_sample_dict[var_names[ii]]
+        map_sample[ii] = map_sample_dict[var_names[ii]]
     return map_sample
 
 
-def get_pymc_variables(variables,pymc_var_names=None):
+def get_pymc_variables(variables, pymc_var_names=None):
     nvars = len(variables)
     if pymc_var_names is None:
-        pymc_var_names = ['z_%d'%ii for ii in range(nvars)]
-    assert len(pymc_var_names)==nvars
+        pymc_var_names = ['z_%d' % ii for ii in range(nvars)]
+    assert len(pymc_var_names) == nvars
     pymc_vars = []
     for ii in range(nvars):
         pymc_vars.append(
-            get_pymc_variable(variables[ii],pymc_var_names[ii]))
+            get_pymc_variable(variables[ii], pymc_var_names[ii]))
     return pymc_vars, pymc_var_names
 
-def get_pymc_variable(rv,pymc_var_name):
+
+def get_pymc_variable(rv, pymc_var_name):
     name, scales, shapes = get_distribution_info(rv)
-    if rv.dist.name=='norm':
+    if rv.dist.name == 'norm':
         return pm.Normal(
-            pymc_var_name,mu=scales['loc'],sigma=scales['scale'])
-    if rv.dist.name=='uniform':        
-        return pm.Uniform(pymc_var_name,lower=scales['loc'],
+            pymc_var_name, mu=scales['loc'], sigma=scales['scale'])
+    if rv.dist.name == 'uniform':
+        return pm.Uniform(pymc_var_name, lower=scales['loc'],
                           upper=scales['loc']+scales['scale'])
     msg = f'Variable type: {name} not supported'
     raise Exception(msg)
 
+
 def run_bayesian_inference_gaussian_error_model(
-        loglike,variables,ndraws,nburn,njobs,
-        algorithm='nuts',get_map=False,print_summary=False,loglike_grad=None,
+        loglike, variables, ndraws, nburn, njobs,
+        algorithm='nuts', get_map=False, print_summary=False, loglike_grad=None,
         seed=None):
     r"""
     Draw samples from the posterior distribution using Markov Chain Monte 
@@ -246,13 +252,13 @@ def run_bayesian_inference_gaussian_error_model(
 
     get_map : boolean
         If true return the MAP
-    
+
     print_summary : boolean
         If true print summary statistics about the posterior samples
 
     loglike_grad : callable
         Function with signature
-      
+
        ``loglikegrad(z) -> np.ndarray (nvars)``
 
         where ``z`` is a 2D np.ndarray with shape (nvars,nsamples
@@ -262,12 +268,12 @@ def run_bayesian_inference_gaussian_error_model(
         produce consistent results by setting numpy.random.seed instead
         seed must be passed in
     """
-    
+
     # create our Op
-    if algorithm!='nuts':
+    if algorithm != 'nuts':
         logl = LogLike(loglike)
     else:
-        logl = LogLikeWithGrad(loglike,loglike_grad)
+        logl = LogLikeWithGrad(loglike, loglike_grad)
 
     # use PyMC3 to sampler from log-likelihood
     with pm.Model():
@@ -285,37 +291,38 @@ def run_bayesian_inference_gaussian_error_model(
         if get_map:
             map_sample_dict = pm.find_MAP()
 
-        if algorithm=='smc':
-            assert njobs==1 # njobs is always 1 when using smc
-            trace = pm.sample_smc(ndraws);
+        if algorithm == 'smc':
+            assert njobs == 1  # njobs is always 1 when using smc
+            trace = pm.sample_smc(ndraws)
         else:
-            if algorithm=='metropolis':
-                step=pm.Metropolis(pymc_variables)
-            elif algorithm=='nuts':
-                step=pm.NUTS(pymc_variables)
-            
+            if algorithm == 'metropolis':
+                step = pm.Metropolis(pymc_variables)
+            elif algorithm == 'nuts':
+                step = pm.NUTS(pymc_variables)
+
             trace = pm.sample(
                 ndraws, tune=nburn, discard_tuned_samples=True,
-                start=None,cores=njobs,step=step,
-                compute_convergence_checks=False,random_seed=seed)
+                start=None, cores=njobs, step=step,
+                compute_convergence_checks=False, random_seed=seed)
             # compute_convergence_checks=False avoids bugs in theano
-            
+
         if print_summary:
             try:
                 print(pm.summary(trace))
             except:
                 print('could not print summary. likely issue with theano')
-        
-        samples, effective_sample_size=extract_mcmc_chain_from_pymc3_trace(
-            trace,pymc_var_names,ndraws,nburn,njobs)
-    
+
+        samples, effective_sample_size = extract_mcmc_chain_from_pymc3_trace(
+            trace, pymc_var_names, ndraws, nburn, njobs)
+
         if get_map:
             map_sample = extract_map_sample_from_pymc3_dict(
-                map_sample_dict,pymc_var_names)
+                map_sample_dict, pymc_var_names)
         else:
             map_samples = None
-        
+
     return samples, effective_sample_size, map_sample
+
 
 class PYMC3LogLikeWrapper():
     r"""
@@ -323,21 +330,22 @@ class PYMC3LogLikeWrapper():
     Main difference is that PYMC3 often passes 1d arrays where as
     Pyapprox assumes 2d arrays.
     """
-    def __init__(self,loglike,loglike_grad=None):
-        self.loglike=loglike
-        self.loglike_grad=loglike_grad
 
-    def __call__(self,x):
-        if x.ndim==1:
-            xr = x[:,np.newaxis]
+    def __init__(self, loglike, loglike_grad=None):
+        self.loglike = loglike
+        self.loglike_grad = loglike_grad
+
+    def __call__(self, x):
+        if x.ndim == 1:
+            xr = x[:, np.newaxis]
         else:
-            xr=x
+            xr = x
         vals = self.loglike(xr)
         return vals.squeeze()
 
-    def gradient(self,x):
-        if x.ndim==1:
-            xr = x[:,np.newaxis]
+    def gradient(self, x):
+        if x.ndim == 1:
+            xr = x[:, np.newaxis]
         else:
-            xr=x
+            xr = x
         return self.loglike_grad(xr).squeeze()
--- pyapprox/bayes/tests/test_markov_chain_monte_carlo.py
+++ pyapprox/bayes/tests/test_markov_chain_monte_carlo.py
@@ -2,14 +2,15 @@ import numpy as np
 import unittest
 from scipy.stats import norm, uniform
 
-import pyapprox as pya
-from pyapprox.variables.marginals import IndependentMarginalsVariable
+from pyapprox.variables.joint import IndependentMarginalsVariable
 from pyapprox.util.utilities import get_tensor_product_quadrature_rule
 from pyapprox.surrogates.orthopoly.quadrature import gauss_jacobi_pts_wts_1D
 from pyapprox.bayes.laplace import \
     laplace_posterior_approximation_for_linear_models
 
-from pyapprox_dev.bayesian_inference.markov_chain_monte_carlo import *
+from pyapprox.bayes.markov_chain_monte_carlo import (
+    GaussianLogLike, PYMC3LogLikeWrapper,
+    run_bayesian_inference_gaussian_error_model)
 
 
 class LinearModel(object):
@@ -20,17 +21,17 @@ class LinearModel(object):
         Amatrix : np.ndarray (nobs,nvars)
             The Jacobian of the linear model
         """
-        self.Amatrix=Amatrix
+        self.Amatrix = Amatrix
 
-    def __call__(self,samples):
+    def __call__(self, samples):
         """
         Evaluate the model
         """
-        assert samples.ndim==2
+        assert samples.ndim == 2
         vals = self.Amatrix.dot(samples).T
         return vals
 
-from pyapprox.interface.wrappers import evaluate_1darray_function_on_2d_array
+
 class ExponentialQuarticLogLikelihoodModel(object):
     def __init__(self):
         self.a = 3.0
@@ -40,27 +41,28 @@ class ExponentialQuarticLogLikelihoodModel(object):
         return value
 
     def gradient(self, x):
-        assert x.ndim==2
-        assert x.shape[1]==1
+        assert x.ndim == 2
+        assert x.shape[1] == 1
         grad = -np.array([12./5.*x[0]**3-4.*x[0]*x[1],
                           4.*x[1]-2.*x[0]**2])
         return grad
 
-    def __call__(self,x):
+    def __call__(self, x):
         return np.array([self.loglikelihood_function(x)]).T
+
 
 class TestMCMC(unittest.TestCase):
 
     def test_linear_gaussian_inference(self):
         # set random seed, so the data is reproducible each time
-        np.random.seed(1)  
-        
-        nobs  = 10  # number of observations
+        np.random.seed(1)
+
+        nobs = 10  # number of observations
         noise_stdev = .1  # standard deviation of noise
         x = np.linspace(0., 9., nobs)
-        Amatrix = np.hstack([np.ones((nobs,1)),x[:,np.newaxis]])
+        Amatrix = np.hstack([np.ones((nobs, 1)), x[:, np.newaxis]])
 
-        univariate_variables = [norm(1,1),norm(0,4)]
+        univariate_variables = [norm(1, 1), norm(0, 4)]
         variables = IndependentMarginalsVariable(
             univariate_variables)
 
@@ -71,45 +73,44 @@ class TestMCMC(unittest.TestCase):
         model = LinearModel(Amatrix)
 
         # make data
-        data = noise_stdev*np.random.randn(nobs)+model(true_sample)[0,:]
+        data = noise_stdev*np.random.randn(nobs)+model(true_sample)[0, :]
         loglike = GaussianLogLike(model, data, noise_stdev**2)
         loglike = PYMC3LogLikeWrapper(loglike)
 
         # number of draws from the distribution
         ndraws = 5000
         # number of "burn-in points" (which we'll discard)
-        nburn = min(1000,int(ndraws*0.1))
+        nburn = min(1000, int(ndraws*0.1))
         # number of parallel chains
-        njobs=4
+        njobs = 4
 
-        #algorithm='nuts'
-        algorithm='metropolis'
+        # algorithm='nuts'
+        algorithm = 'metropolis'
         samples, effective_sample_size, map_sample = \
             run_bayesian_inference_gaussian_error_model(
-                loglike,variables,ndraws,nburn,njobs,
-                algorithm=algorithm,get_map=True,print_summary=False)
+                loglike, variables, ndraws, nburn, njobs,
+                algorithm=algorithm, get_map=True, print_summary=False)
 
         prior_mean = np.asarray(
             [rv.mean() for rv in variables.marginals()])
         prior_hessian = np.diag(
             [1./rv.var() for rv in variables.marginals()])
         noise_covariance_inv = 1./noise_stdev**2*np.eye(nobs)
-       
+
         exact_mean, exact_covariance = \
             laplace_posterior_approximation_for_linear_models(
                 Amatrix, prior_mean, prior_hessian,
                 noise_covariance_inv, data)
 
-        print('mcmc mean error',samples.mean(axis=1)-exact_mean)
-        print('mcmc cov error',np.cov(samples)-exact_covariance)
-        print('MAP sample',map_sample)
-        print('exact mean',exact_mean.squeeze())
-        print('exact cov',exact_covariance)
-        assert np.allclose(map_sample,exact_mean)
+        print('mcmc mean error', samples.mean(axis=1)-exact_mean)
+        print('mcmc cov error', np.cov(samples)-exact_covariance)
+        print('MAP sample', map_sample)
+        print('exact mean', exact_mean.squeeze())
+        print('exact cov', exact_covariance)
+        assert np.allclose(map_sample, exact_mean)
         assert np.allclose(
-            exact_mean.squeeze(), samples.mean(axis=1),atol=1e-2)
+            exact_mean.squeeze(), samples.mean(axis=1), atol=1e-2)
         assert np.allclose(exact_covariance, np.cov(samples), atol=1e-2)
-
 
         # plot the traces
         # _ = pm.traceplot(trace)
@@ -117,22 +118,21 @@ class TestMCMC(unittest.TestCase):
 
     def test_exponential_quartic(self):
         # set random seed, so the data is reproducible each time
-        np.random.seed(2)  
-        
-        univariate_variables = [uniform(-2,4),uniform(-2,4)]
-        plot_range = np.asarray([-1,1,-1,1])*2
+        np.random.seed(2)
+
+        univariate_variables = [uniform(-2, 4), uniform(-2, 4)]
         variables = IndependentMarginalsVariable(
             univariate_variables)
 
         loglike = ExponentialQuarticLogLikelihoodModel()
-        loglike = PYMC3LogLikeWrapper(loglike,loglike.gradient)
+        loglike = PYMC3LogLikeWrapper(loglike, loglike.gradient)
 
         # number of draws from the distribution
         ndraws = 500
         # number of "burn-in points" (which we'll discard)
-        nburn = min(1000,int(ndraws*0.1))
+        nburn = min(1000, int(ndraws*0.1))
         # number of parallel chains
-        njobs=4
+        njobs = 4
 
         def unnormalized_posterior(x):
             # avoid use of pymc3 wrapper which only evaluates samples 1 at
@@ -140,28 +140,28 @@ class TestMCMC(unittest.TestCase):
             vals = np.exp(loglike.loglike(x))
             rvs = variables.marginals()
             for ii in range(variables.num_vars()):
-                vals[:,0] *= rvs[ii].pdf(x[ii,:])
+                vals[:, 0] *= rvs[ii].pdf(x[ii, :])
             return vals
 
         def univariate_quadrature_rule(n):
-            x,w = gauss_jacobi_pts_wts_1D(n,0,0)
-            x*=2
-            return x,w
-        x,w = get_tensor_product_quadrature_rule(
-            100,variables.num_vars(),univariate_quadrature_rule)
-        evidence = unnormalized_posterior(x)[:,0].dot(w)
-        #print('evidence',evidence)
+            x, w = gauss_jacobi_pts_wts_1D(n, 0, 0)
+            x *= 2
+            return x, w
+        x, w = get_tensor_product_quadrature_rule(
+            100, variables.num_vars(), univariate_quadrature_rule)
+        evidence = unnormalized_posterior(x)[:, 0].dot(w)
+        # print('evidence',evidence)
 
-        exact_mean = ((x*unnormalized_posterior(x)[:,0]).dot(w)/evidence)
-        #print(exact_mean)
+        exact_mean = ((x*unnormalized_posterior(x)[:, 0]).dot(w)/evidence)
+        # print(exact_mean)
 
         algorithm = 'nuts'
         #algorithm = 'smc'
         samples, effective_sample_size, map_sample = \
             run_bayesian_inference_gaussian_error_model(
-                loglike,variables,ndraws,nburn,njobs,
-                algorithm=algorithm,get_map=True,print_summary=True,
-                loglike_grad = loglike.gradient, seed=2)
+                loglike, variables, ndraws, nburn, njobs,
+                algorithm=algorithm, get_map=True, print_summary=True,
+                loglike_grad=loglike.gradient, seed=2)
 
         # from pyapprox.util.visualization import get_meshgrid_function_data
         # import matplotlib
@@ -172,19 +172,18 @@ class TestMCMC(unittest.TestCase):
         #     cmap=matplotlib.cm.coolwarm)
         # plt.plot(samples[0,:],samples[1,:],'ko')
         # plt.show()
-        
-        print('mcmc mean error',samples.mean(axis=1)-exact_mean)
-        print('MAP sample',map_sample)
-        print('exact mean',exact_mean.squeeze())
-        print('MCMC mean',samples.mean(axis=1))
-        assert np.allclose(map_sample,np.zeros((variables.num_vars(),1)))
-        #tolerance 3e-2 can be exceeded for certain random runs
+
+        print('mcmc mean error', samples.mean(axis=1)-exact_mean)
+        print('MAP sample', map_sample)
+        print('exact mean', exact_mean.squeeze())
+        print('MCMC mean', samples.mean(axis=1))
+        assert np.allclose(map_sample, np.zeros((variables.num_vars(), 1)))
+        # tolerance 3e-2 can be exceeded for certain random runs
         assert np.allclose(
-            exact_mean.squeeze(), samples.mean(axis=1),atol=3e-2)
+            exact_mean.squeeze(), samples.mean(axis=1), atol=3e-2)
 
 
-
-if __name__== "__main__":    
+if __name__ == "__main__":
     mcmc_test_suite = unittest.TestLoader().loadTestsFromTestCase(
-         TestMCMC)
+        TestMCMC)
     unittest.TextTestRunner(verbosity=2).run(mcmc_test_suite)
