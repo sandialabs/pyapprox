6251727335c924a90b01f7d3285aa2ba60eaa386
John Jakeman <jdjakem@users.noreply.github.com>
2022-11-28 16:01:59 -0700

    switch torch to pytorch in setup dependencies

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setuptools.setup(
         'scipy >= 1.0.0',
         'Cython',
         'sympy',
-        'torch',
+        'pytorch',
         'scikit-learn',
         'pytest-cov',
         'pytest>=4.6',
