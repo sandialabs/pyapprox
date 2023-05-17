Fix bug in Turbine domain. Normals were incorrectly computed because transform scaling was setting left bound to be greater than right bound

pyapprox/pde/autopde/tests/test_autopde.py
@@ -202,6 +202,16 @@ class TestAutoPDE(unittest.TestCase):
                 [partial(transform.normal, ii) for ii in range(2*nphys_vars)])
             mesh = TransformedCollocationMesh(orders, transform)
 
+        # import matplotlib.pyplot as plt
+        # print(transform)
+        # for ii in range(4):
+        #     idx = mesh._bndry_indices[ii]
+        #     eps = 0.05
+        #     normals = mesh._bndrys[ii].normals(mesh.mesh_pts[:, idx])*eps
+        #     for kk in range(normals.shape[0]):
+        #         plt.arrow(*mesh.mesh_pts[:, idx][:, kk], *normals[kk])
+        # plt.show()
+
         # bndry_cond returned by _get_boundary_funs is du/dx=fun
         # apply boundary condition kdu/dx.n=fun
         # for bndry_cond in bndry_conds:
@@ -209,7 +219,7 @@ class TestAutoPDE(unittest.TestCase):
         #         _adf_bndry_fun, bndry_cond[0], diff_fun)
 
         if ("_u_" in diff_string):
-            assert diff_jac is not None
+            assert nl_diff_funs[1] is not None
         solver = SteadyStatePDE(AdvectionDiffusionReaction(
             mesh, bndry_conds, diff_fun, vel_fun, react_funs[0], forc_fun,
             react_funs[1], nl_diff_fun=nl_diff_funs[0],
@@ -250,7 +260,8 @@ class TestAutoPDE(unittest.TestCase):
         solver.physics._clear_data()
         sol = solver.solve(
             init_guess=exact_sol[:, 0]+np.random.normal(
-                0, 1e-2, exact_sol.shape[0]), tol=1e-8, rtol=1e-12, verbosity=2)[:, None]
+                0, 1e-2, exact_sol.shape[0]), tol=1e-8, rtol=1e-12,
+            verbosity=2)[:, None]
         assert np.linalg.norm(
             sol_fun(mesh.mesh_pts)-sol) < 1e-9
 
@@ -421,11 +432,11 @@ class TestAutoPDE(unittest.TestCase):
             # the solution is not quadratic in the canonical domain
             # due to nonlinearity of polar coordinate transform
             # 14
-            [None, [20, 20], "y**2*x**2", "1", ["0", "0"],
+            [None, [20, 20], "(x+y)**2", "1", ["0", "0"],
              [lambda sol: 0*sol,
               lambda sol: torch.zeros((sol.shape[0],))],
              ["D", "D", "D", "N"], ["C", "C"], polar_transform],
-            [None, [20, 20], "y**2*x**2", "1", ["0", "0"],
+            [None, [20, 20], "(x+y)**2", "1", ["0", "0"],
              [lambda sol: 1*sol**2,
               lambda sol: 2*sol[:, 0]],
              ["D", "D", "D", "N"], ["C", "C"], ellipse_transform],

pyapprox/pde/hdg/pde_coupling.py
@@ -328,7 +328,6 @@ class AbstractDomainDecomposition(ABC):
                 normal_vals_ii = mesh._bndrys[bndry_id].normals(
                     mesh._bndry_slice(mesh.mesh_pts, idx, 1))
                 self._normals[jj][ii] = normal_vals_ii
-                # print(normal_vals_ii, jj, ii, "Normal vals")
             else:
                 normal_vals_ii = self._normals[jj][ii]
             physics._store_data = True
@@ -372,8 +371,6 @@ class AbstractDomainDecomposition(ABC):
         if len(self._iface_bases) == 0:
             store = True
             # self._to_interface_bases = []
-        print(fluxes[0], "F")
-        print(fluxes[1])
         for ii in range(self._nsubdomains):
             for mm, iface1 in enumerate(self._subdomain_to_interface_map[ii]):
                 # The values of fluxes, i.e. different rows of Jacobian
@@ -386,11 +383,6 @@ class AbstractDomainDecomposition(ABC):
                     interface1._interpolate_from_subdomain(
                         self._subdomain_models[ii], bndry_id1,
                         fluxes[ii][mm])[:, 0])
-                print(ii, mm)
-                print(interface1._interpolate_from_subdomain(
-                    self._subdomain_models[ii], bndry_id1,
-                    fluxes[ii][mm])[:, 0])
-                print(residual, "R")
                 if store:
                     basis_to_iface1 = torch.as_tensor(
                         interface1._to_interface_basis(
@@ -521,7 +513,6 @@ class AbstractDomainDecomposition(ABC):
                                     orth_active_dim, [0, -1]]))):
                         # An interface exists
                         interface_cnt = len(self._interfaces)
-                        print(subdomain_id, neigh_id, orth_active_dim, interface_cnt)
                         self._interface_to_bndry_map.append(
                             [subdomain_id, jj, neigh_id, kk])
                         self._interfaces.append(SubdomainInterface1D(
@@ -1036,7 +1027,6 @@ class SteadyStateDomainDecompositionSolver():
         return subdomain_sols
 
     def _solve_subdomain(self, jj, **subdomain_newton_kwargs):
-        print("###", jj)
         sol = self._decomp._subdomain_models[jj].solve(
             None, **subdomain_newton_kwargs)
         drdu = self._decomp._subdomain_models[jj].physics._residual(sol)[1]
@@ -1203,11 +1193,17 @@ class TurbineDomainDecomposition(AbstractTwoDDomainDecomposition):
         scale = width_max/rmax
         alpha, beta = 1.3, 0.1
         theta0 = alpha*np.pi/2
+        # transform_0 = self._get_polar_subdomain(
+        #     rmin, rmax, scale, 2*np.pi-theta0, theta0)
+        # transform_0._normal_sign = PolarTransform._normal_sign
         transform_0 = self._get_polar_subdomain(
-            rmin, rmax, scale, 2*np.pi-theta0, theta0)
+            rmin, rmax, scale, theta0, 2*np.pi-theta0)
         theta1 = (alpha-beta)*np.pi/2
+        # transform_1 = self._get_polar_subdomain(
+        #     rmin, rmax, scale, theta0, theta1)
+        # transform_1._normal_sign = PolarTransform._normal_sign
         transform_1 = self._get_polar_subdomain(
-            rmin, rmax, scale, theta0, theta1)
+            rmin, rmax, scale, theta1, theta0)
         surf_string = f"sqrt({rmin**2}-_r_**2)"
         bed_string = f"-sqrt({rmin**2}-_r_**2)"
         x0 = rmin*np.cos(theta0)
@@ -1219,12 +1215,21 @@ class TurbineDomainDecomposition(AbstractTwoDDomainDecomposition):
                 [x0, x1, 0, 1],
                 [scale*x0, scale*x1, 0, 1])
             ])
+        # transform_3 = self._get_polar_subdomain(
+        #     rmin, rmax, scale, 2*np.pi-theta1, 2*np.pi-theta0)
+        # transform_3._normal_sign = PolarTransform._normal_sign
         transform_3 = self._get_polar_subdomain(
-            rmin, rmax, scale, 2*np.pi-theta1, 2*np.pi-theta0)
+            rmin, rmax, scale, 2*np.pi-theta0, 2*np.pi-theta1)
+        # transform_4 = self._get_polar_subdomain(
+        #     rmin, rmax, scale, theta1, np.pi/2)
+        # transform_4._normal_sign = PolarTransform._normal_sign
         transform_4 = self._get_polar_subdomain(
-            rmin, rmax, scale, theta1, np.pi/2)
+            rmin, rmax, scale, np.pi/2, theta1)
+        # transform_5 = self._get_polar_subdomain(
+        #     rmin, rmax, scale, 2*np.pi-np.pi/2, 2*np.pi-theta1)
+        # transform_5._normal_sign = PolarTransform._normal_sign
         transform_5 = self._get_polar_subdomain(
-            rmin, rmax, scale, 2*np.pi-np.pi/2, 2*np.pi-theta1)
+             rmin, rmax, scale, 2*np.pi-theta1, 2*np.pi-np.pi/2)
         x2 = width_max*0.2
         x_end = 1.0*width_max
         delta = height_max/2
@@ -1261,11 +1266,11 @@ class TurbineDomainDecomposition(AbstractTwoDDomainDecomposition):
         #               for transform in transforms]
 
         # transforms = [transforms[4], transforms[6]]
-        transforms = [transforms[5], transforms[7]]
+        # transforms = [transforms[5], transforms[7]]
         # transforms = [transforms[8], transforms[11]]
         # transforms = [transforms[2], transforms[3]]
-        self._nsubdomains = 2
-        self._ninterfaces = 1
+        # self._nsubdomains = 2
+        # self._ninterfaces = 1
         # transforms = [transforms[1], transforms[2], transforms[3],
         #               transforms[4], transforms[6]]
         # self._nsubdomains = 5

pyapprox/pde/hdg/tests/test_pde_coupling.py
@@ -145,7 +145,7 @@ class TestPDECoupling(unittest.TestCase):
             assert np.absolute(res_norm) < 2e-14
             for jj in range(ninterfaces):
                 sp_jac[ii, jj] = res.diff(interface_syms[jj])
-                
+
         # boundary conditions cannot be passed into partial
         # as only shallow copy will be made and so when domain decomp
         # sets interpolation on some boundaries it inadvertently effects
@@ -188,7 +188,7 @@ class TestPDECoupling(unittest.TestCase):
             exact_dirichlet_vals)[1]
         # print(jac, sp_jac)
         assert np.allclose(jac, sp_jac)
-        
+
         init_dirichlet_vals = np.ones((domain_decomp._ninterfaces, 1))
         # using sp_jac newton conveges in one iteration which it should
         # but because of finite difference errors using finite difference
@@ -285,7 +285,7 @@ class TestPDECoupling(unittest.TestCase):
         # plt.show()
 
         np.set_printoptions(linewidth=1000, precision=4, suppress=True)
-        
+
         exact_dirichlet_vals = exact_sol(interface_mesh)
         residual = domain_decomp._assemble_dirichlet_neumann_map_jacobian(
             exact_dirichlet_vals)[0]
@@ -453,7 +453,7 @@ class TestPDECoupling(unittest.TestCase):
             domain_decomp.init_subdomains(init_subdomain_model)
 
         interface_mesh = domain_decomp.interface_mesh()
-            
+
         # domain_decomp.plot_mesh_grid(plt.subplots(1, 1)[1], color='k')
         # plt.plot(interface_mesh[0], interface_mesh[1], 'o')
         # plt.show()
@@ -479,29 +479,38 @@ class TestPDECoupling(unittest.TestCase):
                         continue
                     idx = model.physics.mesh._bndry_indices[bndry_id]
                     bndry_pts = model.physics.mesh.mesh_pts[:, idx]
-                    bndry_vals = model.physics._bndry_conds[bndry_id][0](bndry_pts)
+                    bndry_vals = model.physics._bndry_conds[bndry_id][0](
+                        bndry_pts)
                     assert np.allclose(bndry_vals, sol_fun(bndry_pts))
-                    
+
             subdomain_sols = [
                 model.solve(prev_sol, times[ii-1], time)[0][:, -1:]
                 for prev_sol, model in zip(subdomain_sols,
                                            domain_decomp._subdomain_models)]
-            
+
             # ax = plt.subplots(1, 1)[1]
             # domain_decomp.plot(subdomain_sols, 51, ax)
             # exact_subdomain_sols = [
             #     sol_fun(model.physics.mesh.mesh_pts)
             #     for model in domain_decomp._subdomain_models]
-            # domain_decomp.plot(exact_subdomain_sols, 51, ax, ls='--')
+            # domain_decomp.plot(exact_subdomain_sols, 51, ax)
             # if nphys_vars == 2:
             #     plt.plot(interface_mesh[0], interface_mesh[1], 'ko')
+            # for model_jj in domain_decomp._subdomain_models:
+            #     mesh = model_jj.physics.mesh
+            #     for ii in range(4):
+            #         idx = mesh._bndry_indices[ii]
+            #         eps = 0.05
+            #         normals = mesh._bndrys[ii].normals(mesh.mesh_pts[:, idx])*eps
+            #         for kk in range(normals.shape[0]):
+            #             plt.arrow(*mesh.mesh_pts[:, idx][:, kk], *normals[kk])
             # plt.show()
 
             for (sol, model) in zip(
                     subdomain_sols, domain_decomp._subdomain_models):
                 # print(sol, sol_fun(model.physics.mesh.mesh_pts))
-                print(np.abs(
-                    sol-sol_fun(model.physics.mesh.mesh_pts)).max())
+                # print(np.abs(
+                #     sol-sol_fun(model.physics.mesh.mesh_pts)).max())
                 assert np.allclose(
                     sol, sol_fun(model.physics.mesh.mesh_pts), atol=atol)
 
@@ -509,7 +518,7 @@ class TestPDECoupling(unittest.TestCase):
                 exact_prev_sols, time-deltat, deltat, False, False]
             decomp_solver._decomp._solve_subdomain = (
                 decomp_solver._solve_subdomain_expanded)
-                
+
             residual, jac = (
                 domain_decomp._assemble_dirichlet_neumann_map_jacobian(
                     exact_dirichlet_vals))
@@ -517,9 +526,8 @@ class TestPDECoupling(unittest.TestCase):
             # im = decomp_solver._decomp.plot(subdomain_sols, 51, plt, eps=1e-1)
             # plt.colorbar(im[0])
             # plt.show()
-            print('res', residual, atol)
+            # print('res', residual, atol)
             assert np.allclose(residual, 0, atol=atol)
-            assert False
 
         npts_1d = 21
         if nphys_vars == 1:
@@ -570,7 +578,7 @@ class TestPDECoupling(unittest.TestCase):
             #     # plt.plot(can_bndry_pts, sol_fun(bndry_pts), '--')
             #     # plt.plot(can_bndry_pts, subdomain_sols[ii][0][idx]-sol_fun(bndry_pts))
 
-                
+
             #     axs = plt.subplots(1, 3, sharey=True, figsize=(3*8, 6))[1]
             #     full_exact_sol = sol_fun(solver.physics.mesh.mesh_pts)
             #     levels = np.linspace(
@@ -644,7 +652,7 @@ class TestPDECoupling(unittest.TestCase):
              ["D", "D", "D", "D"], None, "turbine", 1e-5]
         ]
         ii = 0
-        for test_case in test_cases[-1:]:
+        for test_case in test_cases:
             # print(ii)
             # print(test_case)
             np.random.seed(1)  # controls direction of finite difference