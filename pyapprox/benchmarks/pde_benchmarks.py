Stop lstsqlinop basis running out of memory for large number of input function and output function samples and coefficients. Fix kle whc computing sign on eignenvectors. In special case when sign = 0 all eig vecs were set to zero

pyapprox/benchmarks/pde_benchmarks.py
@@ -216,7 +216,6 @@ class AdvectionDiffusionReactionKLEModel():
         sample_copy = torch.as_tensor(sample.copy(), dtype=torch.double)
         self._set_random_sample(sample_copy)
         sol = self._fwd_solver.solve(**self._newton_kwargs)
-        print(sol.shape, self._fwd_solver.physics.mesh.mesh_pts.shape)
         qoi = self._functional(sol, sample_copy).numpy()
         if not return_grad:
             return qoi
@@ -500,8 +499,8 @@ class Burgers1DParameterizedModel(SingleSampleModel):
         super().__init__()
         sigma, tau, gamma, neigs = 7**2, 7, 2.5, 1024//2
         domain_bounds = [0, 1]
-        # self._orders = [1024//2]
-        self._orders = [1024//8]
+        self._orders = [1024//2]
+        # self._orders = [1024//8]
         self._rand_field = PeriodicReiszGaussianRandomField(
             sigma, tau, gamma, neigs, domain_bounds, backend=TorchLinAlgMixin
         )
@@ -561,7 +560,7 @@ class DarcyKLE2DModel(SingleSampleModel):
             mesh.mesh_pts, lenscales, sigma=1., nterms=100,
             use_log=True, mean_field=0., backend=TorchLinAlgMixin
         )
-        forc_fun = partial(full_fun_axis_1, 0., oned=False)
+        forc_fun = partial(full_fun_axis_1, 1., oned=False)
         vel_fun = partial(constant_vel_fun,  [0., 0.])
         react_funs = None
         self._model = AdvectionDiffusionReactionKLEModel(

pyapprox/pde/kle/kle.py
@@ -303,7 +303,7 @@ class AbstractKLE(ABC):
             # always compute eigenvalue decomposition using scipy because
             # it can be used to only compute subset of eigenvectors
             # then we cast these back to correct linalg type. The downside
-            # is that we cannot use autograd on quantities used to consturct K.
+            # is that we cannot use autograd on quantities used to construct K.
             # but the need for this is unlikely
             eig_vals, eig_vecs = eigh(
                 self._bkd.to_numpy(K),  # turbo=False,
@@ -322,9 +322,8 @@ class AbstractKLE(ABC):
             eig_vecs = 1/sqrt_weights[:, None]*sym_eig_vecs
             eig_vals = sym_eig_vals
         eig_vecs = adjust_sign_eig(eig_vecs)
-        # II = self._bkd.argsort(eig_vals)[::-1][:self._nterms]
         II = self._bkd.flip(self._bkd.argsort(eig_vals))[:self._nterms]
-        assert self._bkd.all(eig_vals[II] > 0), eig_vals[II]
+        assert self._bkd.all(eig_vals[II] > 0), (eig_vals[II], self._bkd.where(eig_vals[II]<=0)[0])
         self._sqrt_eig_vals = self._bkd.sqrt(eig_vals[II])
         self._eig_vecs = eig_vecs[:, II]
 
@@ -337,7 +336,6 @@ class AbstractKLE(ABC):
         coef : np.ndarray (nterms, nsamples)
             The coefficients of the KLE basis
         """
-        print(coef.shape)
         assert coef.ndim == 2
         assert coef.shape[0] == self._nterms
         if self._use_log: