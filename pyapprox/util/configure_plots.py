95844b058fc97c60e4ffb66798033515128c2a59
John Jakeman <jdjakem@users.noreply.github.com>
2024-09-25 16:55:33 -0600

    update configure_plots.py with examples of good plot settings

--- pyapprox/util/configure_plots.py
+++ pyapprox/util/configure_plots.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['font.size'] = 16
-mpl.rcParams['lines.linewidth'] = 3
-mpl.rcParams['text.usetex'] = True # use latex for all text handling
-mpl.rcParams['savefig.bbox'] = 'tight'
-mpl.rcParams['savefig.format'] = 'pdf' # gives best resolution plots
-mpl.rcParams['axes.labelsize'] = 20
-mpl.rcParams['axes.titlesize'] = 20
-mpl.rcParams['xtick.labelsize'] = 20
-mpl.rcParams['ytick.labelsize'] = 20
-mpl.rcParams['legend.fontsize'] = 16
-#print mpl.rcParams.keys()
-mpl.rcParams['text.latex.preamble'] = r'\usepackage{siunitx}\usepackage{amsmath}\usepackage{amssymb}'
+plt.rcParams["font.size"] = 16
+# set font to default font used by most latex document classes
+plt.rcParams["font.family"] = "Computer Modern"
+plt.rcParams["lines.linewidth"] = 3
+# use latex for all text handling (slows down ploting)
+plt.rcParams["text.usetex"] = True
+# remove all extra white space
+plt.rcParams["savefig.bbox"] = "tight"
+# plot with transparent background
+plt.rcParams['savefig.transparent'] = True
+# gives best resolution plots
+plt.rcParams["savefig.format"] = "pdf"
+plt.rcParams["axes.labelsize"] = 20
+plt.rcParams["axes.titlesize"] = 20
+plt.rcParams["xtick.labelsize"] = 20
+plt.rcParams["ytick.labelsize"] = 20
+plt.rcParams["legend.fontsize"] = 16
+# print plt.rcParams.keys()
+plt.rcParams["text.latex.preamble"] = (
+    r"\usepackage{siunitx}\usepackage{amsmath}\usepackage{amssymb}"
+)
 
 # NOTES
-# using plt.plot(visible=False ) will allow linestyle/marker to be included
+# using plt.plot(visible=False) will allow linestyle/marker to be included
 # in legend but line will not plotted
-
