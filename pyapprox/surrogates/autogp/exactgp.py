Fix output scaling of autogp

pyapprox/surrogates/autogp/exactgp.py
@@ -95,7 +95,7 @@ class ExactGaussianProcess(OptimizedRegressor):
         return self._in_trans.map_from_canonical(self._ctrain_samples)
 
     def get_train_values(self):
-        return self._out_trans.map_from_canonical(self._ctrain_values)
+        return self._out_trans.map_from_canonical(self._ctrain_values.T).T
 
     def _training_kernel_matrix(self) -> Tuple:
         # must only pass in X and not Y to kernel otherwise if noise kernel
@@ -181,17 +181,16 @@ class ExactGaussianProcess(OptimizedRegressor):
         self.hyp_list.set_active_opt_params(active_opt_params)
         return self._neg_log_likelihood_with_hyperparameter_trend()
 
-    def _evaluate_prior(self, samples: Array, return_std: bool):
-        trend = self._out_trans.map_from_canonical(
-            self._canonical_trend(self._in_trans.map_to_canonical(samples))
-        )
+    def _evaluate_canonical_prior(self, samples: Array, return_std: bool):
+        canonical_trend = self._canonical_trend(
+            self._in_trans.map_to_canonical(samples).T
+        ).T
         if not return_std:
-            return trend
-        return trend, self._in_trans.map_stdev_from_canonical(
-            self._bkd.sqrt(self.kernel.diag(samples))
-        )
+            return canonical_trend, None
+        canonical_std = self._bkd.sqrt(self.kernel.diag(samples))
+        return canonical_trend, canonical_std
 
-    def _evaluate_posterior(self, samples: Array, return_std: bool):
+    def _evaluate_canonical_posterior(self, samples: Array, return_std: bool):
         if self._coef is None:
             self._coef_args = self._factor_training_kernel_matrix()
             self._coef = self._solve_coefficients(*self._coef_args)
@@ -203,9 +202,8 @@ class ExactGaussianProcess(OptimizedRegressor):
         canonical_trend = self._canonical_trend(
             canonical_samples
         ) + self._bkd.multidot((kmat_pred, self._coef))
-        trend = self._out_trans.map_from_canonical(canonical_trend)
         if not return_std:
-            return trend
+            return canonical_trend, None
 
         canonical_pointwise_variance = (
             self._canonical_posterior_pointwise_variance(
@@ -219,24 +217,33 @@ class ExactGaussianProcess(OptimizedRegressor):
             )
             warnings.warn(msg, UserWarning)
         canonical_pointwise_variance[canonical_pointwise_variance < 0] = 0
-        pointwise_stdev = self._out_trans.map_stdev_from_canonical(
-            np.sqrt(canonical_pointwise_variance)
-        )
-        assert pointwise_stdev.shape == trend.shape
-        return trend, pointwise_stdev
-        # return trend, canonical_pointwise_variance[:, None]
+        canonical_pointwise_stdev = np.sqrt(canonical_pointwise_variance.T).T
+        assert canonical_pointwise_stdev.shape == canonical_trend.shape
+        return canonical_trend, canonical_pointwise_stdev
+
+    def _canonical_evaluate(self, samples: Array, return_std: bool):
+        if self._ctrain_samples is None:
+            return self._evaluate_canonical_prior(
+                samples, return_std
+            )
+        return self._evaluate_canonical_posterior(samples, return_std)
 
     def evaluate(self, samples: Array, return_std: bool):
         """
         Use when standard deviation of GP is needed.
         Otherwise use __call__
         """
-        if self._ctrain_samples is None:
-            return self._evaluate_prior(samples, return_std)
-        return self._evaluate_posterior(samples, return_std)
+        can_vals, can_std = self._canonical_evaluate(samples, return_std)
+        if return_std:
+            return (
+                self._out_trans.map_from_canonical(can_vals),
+                self._out_trans.map_from_canonical(can_std)
+            )
+        return self._out_trans.map_from_canonical(can_vals)
 
     def _values(self, samples):
-        return self.evaluate(samples, False)
+        # regressor expects values in the canonical domain
+        return self._canonical_evaluate(samples, False)[0]
 
     def __repr__(self):
         return "{0}({1})".format(
@@ -323,7 +330,7 @@ class MOExactGaussianProcess(ExactGaussianProcess):
             s for s in self._map_samples_to_canonical(train_samples)
         ]
         self._ctrain_values = self._bkd.vstack(
-            [self._out_trans.map_to_canonical(v) for v in train_values]
+            [self._out_trans.map_to_canonical(v.T).T for v in train_values]
         )
 
     def _map_samples_to_canonical(self, samples):
@@ -347,8 +354,8 @@ class MOExactGaussianProcess(ExactGaussianProcess):
         for s in self._ctrain_samples:
             train_values.append(
                 self._out_trans.map_from_canonical(
-                    self._ctrain_values[cnt:cnt+s.shape[1]]
-                )
+                    self._ctrain_values[cnt:cnt+s.shape[1]].T
+                ).T
             )
             cnt += s.shape[1]
         return train_values

pyapprox/surrogates/autogp/tests/test_gaussian_process.py
@@ -43,65 +43,6 @@ from pyapprox.util.linearalgebra.numpylinalg import NumpyLinAlgMixin
 from pyapprox.util.linearalgebra.torchlinalg import TorchLinAlgMixin
 
 
-def _check_apply(
-    sample,
-    symb,
-    fun,
-    apply_fun,
-    fd_eps=None,
-    direction=None,
-    relative=True,
-    disp=False,
-    bkd=NumpyLinAlgMixin,
-):
-    if sample.ndim != 2:
-        raise ValueError(
-            "sample with shape {0} must be 2D array".format(sample.shape)
-        )
-    if fd_eps is None:
-        fd_eps = bkd.flip(bkd.logspace(-13, 0, 14))
-    if direction is None:
-        nvars = sample.shape[0]
-        direction = bkd.array(np.random.normal(0, 1, (nvars, 1)))
-        direction /= bkd.linalg.norm(direction)
-
-    row_format = "{:<12} {:<25} {:<25} {:<25}"
-    headers = [
-        "Eps",
-        "norm({0}v)".format(symb),
-        "norm({0}v_fd)".format(symb),
-        "Rel. Errors" if relative else "Abs. Errors",
-    ]
-    if disp:
-        print(row_format.format(*headers))
-    row_format = "{:<12.2e} {:<25} {:<25} {:<25}"
-    errors = []
-    val = fun(sample)
-    directional_grad = apply_fun(sample, direction)
-    for ii in range(fd_eps.shape[0]):
-        sample_perturbed = sample.copy() + fd_eps[ii] * direction
-        perturbed_val = fun(sample_perturbed)
-        fd_directional_grad = (perturbed_val - val) / fd_eps[ii]
-        errors.append(
-            bkd.norm(
-                fd_directional_grad.reshape(directional_grad.shape)
-                - directional_grad
-            )
-        )
-        if relative:
-            errors[-1] /= bkd.norm(directional_grad)
-        if disp:
-            print(
-                row_format.format(
-                    fd_eps[ii],
-                    bkd.norm(directional_grad),
-                    bkd.norm(fd_directional_grad),
-                    errors[ii],
-                )
-            )
-    return bkd.array(errors)
-
-
 class TestNystrom:
     def setUp(self):
         np.random.seed(1)
@@ -308,7 +249,6 @@ class TestGaussianProcess:
 
         pyagp_vals, pyagp_std = pyagp(test_samples, return_std=True)
         gp_vals, gp_std = gp.evaluate(test_samples, return_std=True)
-        # print(gp_std[:, 0]-pyagp_std)
         assert np.allclose(
             bkd.to_numpy(gp_std[:, 0]), pyagp_std, atol=1e-6
         )
@@ -602,7 +542,9 @@ class TestGaussianProcess:
         )
         xx = bkd.linspace(-1, 1, 31)[None, :]
         gp_mean, gp_std = gp.evaluate([xx] * noutputs, return_std=True)
-        peer_gp_mean, peer_gp_std = peer_gp.evaluate([xx] * noutputs, return_std=True)
+        peer_gp_mean, peer_gp_std = peer_gp.evaluate(
+            [xx] * noutputs, return_std=True
+        )
         assert np.allclose(peer_gp_mean, gp_mean)
         assert np.allclose(peer_gp_std, gp_std)
 

pyapprox/variables/transforms.py
@@ -124,25 +124,28 @@ class AffineBoundedVariableTransformation(object):
         return self.nvars
 
 
-class AffineTransform(object):
+from pyapprox.util.transforms import Transform
+from pyapprox.util.linearalgebra.numpylinalg import NumpyLinAlgMixin
+class AffineTransform(Transform):
     r"""
     Apply an affine transformation to a
     :py:class:`pyapprox.variables.IndependentMarginalsVariable`
     """
 
-    def __init__(self, variable, enforce_bounds=False):
+    def __init__(self, variable, enforce_bounds=False, bkd=NumpyLinAlgMixin):
         """
         Variable uniquness dependes on both the type of random variable
         e.g. beta, gaussian, etc. and the parameters of that distribution
         e.g. loc and scale parameters as well as any additional parameters
         """
-        if (type(variable) != IndependentMarginalsVariable):
+        self._bkd = bkd
+        if not isinstance(variable, IndependentMarginalsVariable):
             variable = IndependentMarginalsVariable(variable)
         self.variable = variable
         self.enforce_bounds = enforce_bounds
         self.identity_map_indices = None
 
-        self.scale_parameters = np.empty((self.variable.nunique_vars, 2))
+        self.scale_parameters = self._bkd.empty((self.variable.nunique_vars, 2))
         for ii in range(self.variable.nunique_vars):
             var = self.variable.unique_variables[ii]
             # name, scale_dict, __ = get_distribution_info(var)
@@ -154,7 +157,7 @@ class AffineTransform(object):
             #     lb, ub = -1, 1
             #     scale /= (ub-lb)
             #     loc = loc-scale*lb
-            self.scale_parameters[ii, :] = np.hstack(
+            self.scale_parameters[ii, :] = self._bkd.array(
                 transform_scale_parameters(var))
 
     def set_identity_maps(self, identity_map_indices):
@@ -171,7 +174,7 @@ class AffineTransform(object):
         self.identity_map_indices = identity_map_indices
 
     def map_to_canonical(self, user_samples):
-        canonical_samples = user_samples.copy().astype(float)
+        canonical_samples = self._bkd.copy(user_samples)
         for ii in range(self.variable.nunique_vars):
             indices = self.variable.unique_variable_indices[ii]
             loc, scale = self.scale_parameters[ii, :]
@@ -198,7 +201,7 @@ class AffineTransform(object):
         return canonical_samples
 
     def map_from_canonical(self, canonical_samples):
-        user_samples = canonical_samples.copy().astype(float)
+        user_samples = self._bkd.copy(canonical_samples)
         for ii in range(self.variable.nunique_vars):
             indices = self.variable.unique_variable_indices[ii]
             loc, scale = self.scale_parameters[ii, :]