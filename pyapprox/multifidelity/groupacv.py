Fix bug with group acv simultaneous estimation of mean plus variance

pyapprox/multifidelity/groupacv.py
@@ -189,6 +189,13 @@ class GroupACVLogDetObjective(GroupACVObjective):
             npartition_samples_1d
         )
         sign, logdet = self._bkd.slogdet(cov)
+        if logdet < -1e16:
+            # when cov is singular logdet returns np.inf
+            # make sure to return positive value to indicate
+            # to minimizer this is a bad point. Only really is
+            # an issue if starting from poor initial guess or using
+            # global optimizer
+            return self._bkd.asarray([[np.inf]])
         # conversion below is necessary for torch
         return self._bkd.hstack((logdet,))[:, None]
 
@@ -719,13 +726,18 @@ class GroupACVEstimator:
     def _psi_matrix_from_sigma(self, Sigma):
         # TODO instead of applying R matrices just collect correct rows
         # and columns
-        reg_mat = (
+        psi_reg_mat = (
             self._bkd.eye(self.nmodels() * self._stat.nstats())
             * self._reg_blue
         )
+        # sigma_reg_mat = self._bkd.eye(Sigma.shape[0]) * self._reg_blue
+        # print(Sigma)
         return (
-            self._bkd.multidot((self._R, self._inv(Sigma), self._R.T))
-            + reg_mat
+            self._bkd.multidot(
+                # (self._R, self._inv(Sigma + sigma_reg_mat), self._R.T)
+                (self._R, self._inv(Sigma), self._R.T)
+            )
+            + psi_reg_mat
         )
 
     def _psi_matrix(self, npartition_samples):
@@ -830,7 +842,12 @@ class GroupACVEstimator:
         # with torch if npartition samples is not torch.double need to
         # think of a check that works for all backends
         if round_nsamples:
-            rounded_npartition_samples = self._bkd.floor(npartition_samples)
+            # add 1e-15 to avoid rounding down value that is at the constraint
+            # boundary but has numerical noise. Best value depends on
+            # constraint satisfaction tolerance
+            rounded_npartition_samples = self._bkd.floor(
+                npartition_samples + 1e-4
+            )
         else:
             rounded_npartition_samples = npartition_samples
         self._set_optimized_params_base(
@@ -1086,7 +1103,7 @@ class GroupACVEstimator:
         beta = self._grouped_acv_beta(self._optimized_sigma)
         assert self._bkd.allclose(
             beta.sum(axis=1), self._bkd.ones(beta.shape[0])
-        )
+        ), beta.sum(axis=1)
         alpha = self._bkd.zeros(
             (beta.shape[0], (self._nmodels - 1) * self._stat.nstats())
         )
@@ -1100,37 +1117,89 @@ class GroupACVEstimator:
                 alpha[:, jj - self._stat.nstats()] += self._bkd.maximum(
                     beta[:, kk], zeros
                 )
+                # alpha[:, jj - self._stat.nstats()] -= self._bkd.maximum(
+                #     beta[:, kk], zeros
+                # )
                 kk += 1
         return alpha
+
+    def _extract_from_flattened_subset_matrix(
+        self, mat: Array, subset_idx: int
+    ) -> Array:
+        nprev_stats = sum(
+            [subset.shape[0] for subset in self._subsets[:subset_idx]]
+        )
+        subset = self._subsets[subset_idx]
+        subset_mat = mat[:, nprev_stats : nprev_stats + subset.shape[0]]
+        R = self._restriction_matrix(subset)
+        expanded_mat = (R.T @ subset_mat.T).T
+        return expanded_mat
+
+    def _group_to_traditional_estimators_from_alpha(
+        self, subset_ests: List[Array], alpha: Array
+    ) -> Array:
+        beta = self._grouped_acv_beta(self._optimized_sigma)
+        # beta shape (nstats, sum_s\insubsets nstats * nmodels_in_subset(s)
+        Q0 = self._bkd.zeros((self._stat.nstats(),))
+        Qe = self._bkd.zeros((self._stat.nstats(), (self._nmodels - 1)))
+        Qu = self._bkd.zeros((self._stat.nstats(), (self._nmodels - 1)))
+        # zeros = self._bkd.zeros((self._stat.nstats(), (self._nmodels - 1)))
+        for ii, subset in enumerate(self._subsets):
+            beta_tilde = self._extract_from_flattened_subset_matrix(beta, ii)
+            # print(subset)
+            # print(beta)
+            # print(beta_tilde)
+            B0_tilde = beta_tilde[:, : self._stat.nstats()]
+            BL_tilde = beta_tilde[:, self._stat.nstats() :]
+            R = self._restriction_matrix(subset)
+            Q_tilde = R.T @ subset_ests[ii]
+            Q0_tilde = Q_tilde[: self._stat.nstats()]
+            QL_tilde = Q_tilde[self._stat.nstats() :]
+            print("\n", subset)
+            # print(beta)
+            print(subset_ests[ii])
+            print(Q_tilde, "QT")
+            # print(Q0_tilde, "Q0")
+            # print(B0_tilde, "B0")
+            # print(QL_tilde, "QL")
+            # print(BL_tilde, "BL")
+            # print(subset_ests[ii])
+            Q0 += B0_tilde @ Q0_tilde
+            # print(BL_tilde.shape, zeros.shape, alpha.shape)
+            # we = self._bkd.maximum(BL_tilde, zeros) / alpha
+            # wu = -self._bkd.minimum(BL_tilde, zeros) / alpha
+            nstats = self._stat.nstats()
+            # print(alpha.shape, beta.shape, Qe.shape)
+            for kk in range(beta.shape[0]):
+                for ll in range(1, self._nmodels):
+                    print(
+                        kk,
+                        ll,
+                        (ll - 1) * nstats + kk,
+                        BL_tilde.shape,
+                        alpha.shape,
+                    )
+                    wu = (1 / alpha[kk, (ll - 1) * nstats + kk]) * max(
+                        BL_tilde[kk, (ll - 1) * nstats + kk], 0.0
+                    )
+                    we = -(1 / alpha[kk, (ll - 1) * nstats + kk]) * min(
+                        BL_tilde[kk, (ll - 1) * nstats + kk], 0.0
+                    )
+                    Qe[kk, ll - 1] += QL_tilde[(ll - 1) * nstats + kk] * we
+                    Qu[kk, ll - 1] += QL_tilde[(ll - 1) * nstats + kk] * wu
+            # print(Qe)
+        return Q0, Qe, Qu
 
     def _group_to_traditional_estimators(
         self, subset_ests: List[Array]
     ) -> Array:
-        print(subset_ests)
+        print(subset_ests, "SE")
         # Implement equations (15) in arxiv paper
         # wu = w_l^{k,u} and  we = w_l^{k,e} from arxiv paper
-        beta = self._grouped_acv_beta(self._optimized_sigma)
         alpha = self._traditional_acv_weights()
-        zeros = self._bkd.zeros((beta.shape[0],))
-        Qe, Qu = [], []
-        for ll in range(1, self._nmodels):
-            Qe.append(0.0)
-            Qu.append(0.0)
-            kk = 0
-            for ii, subset in enumerate(self._subsets):
-                for jj in subset:
-                    if jj < self._stat.nstats():
-                        kk += 1
-                        continue
-                    we = self._bkd.maximum(beta[:, kk], zeros)
-                    wu = -self._bkd.minimum(beta[:, kk], zeros)
-                    print(Qe[-1], we, subset_ests)
-                    Qe[-1] += we * subset_ests[ii]
-                    Qu[-1] += wu * subset_ests[ii]
-                    kk += 1
-            Qe[-1] /= alpha[:, ll - 1]
-            Qu[-1] /= alpha[:, ll - 1]
-        return Qe, Qu
+        return self._group_to_traditional_estimators_from_alpha(
+            subset_ests, alpha
+        )
 
     def __call__(self, values_per_model: List[Array]) -> Array:
         values_per_subset = self._separate_values_per_model(values_per_model)

pyapprox/multifidelity/stats.py
@@ -993,8 +993,19 @@ class MultiOutputMeanAndVariance(MultiOutputStatistic):
             ).flatten()[self._tril_idx_flat]
             for ii in range(nmodels_in_subset)
         ]
+        means = self._bkd.mean(values, axis=0)
+
+        # return self._bkd.hstack([means, self._bkd.hstack(flat_covs)])
         return self._bkd.hstack(
-            [self._bkd.mean(values, axis=0), self._bkd.hstack(flat_covs)]
+            [
+                self._bkd.hstack(
+                    (
+                        means[ii * self._nqoi : (ii + 1) * self._nqoi],
+                        flat_covs[ii],
+                    )
+                )
+                for ii in range(nmodels_in_subset)
+            ]
         )
 
     def high_fidelity_estimator_covariance(self, nhf_samples: int) -> Array:
@@ -1244,7 +1255,50 @@ class MultiOutputMeanAndVariance(MultiOutputStatistic):
         )
         block_12 = self._Bcomp[np.ix_(mean_idx0, var_idx1)] * P_MN
         block_21 = self._Bcomp[np.ix_(mean_idx1, var_idx0)].T * P_MN
+        # block = block_2x2(
+        #     [[block_11, block_12], [block_21, block_22]], self._bkd
+        # )
         # Note ordering of statistics must be consistent with sample_estimate
-        return block_2x2(
-            [[block_11, block_12], [block_21, block_22]], self._bkd
-        )
+        # and est._subsets which is stats model 0, stats_model 1 and so on
+        # blocks 11, 12, 21, and 22 do not follow this ordering so
+        # reorder
+        nstats = self.nstats()
+        nqoi = self.nqoi()
+        ncov_stats = self._tril_idx_flat.shape[0]
+        model_ids0 = subset0[::nstats] // nstats
+        model_ids1 = subset1[::nstats] // nstats
+        rows = []
+        for ii in range(len(model_ids0)):
+            # mean row
+            row = []
+            for jj in range(len(model_ids1)):
+                row.append(
+                    block_11[
+                        ii * nqoi : (ii + 1) * nqoi,
+                        jj * nqoi : (jj + 1) * nqoi,
+                    ]
+                )
+                row.append(
+                    block_12[
+                        ii * nqoi : (ii + 1) * nqoi,
+                        jj * ncov_stats : (jj + 1) * ncov_stats,
+                    ]
+                )
+            rows.append(self._bkd.hstack(row))
+            # covariance row
+            row = []
+            for jj in range(len(model_ids1)):
+                row.append(
+                    block_21[
+                        ii * ncov_stats : (ii + 1) * ncov_stats,
+                        jj * nqoi : (jj + 1) * nqoi,
+                    ]
+                )
+                row.append(
+                    block_22[
+                        ii * ncov_stats : (ii + 1) * ncov_stats,
+                        jj * ncov_stats : (jj + 1) * ncov_stats,
+                    ]
+                )
+            rows.append(self._bkd.hstack(row))
+        return self._bkd.vstack(rows)

pyapprox/multifidelity/tests/test_groupacv.py
@@ -511,8 +511,8 @@ class TestGroupACV:
         Sigma = est._sigma(est._rounded_npartition_samples)
         atol, rtol = 4e-3, 3e-2
         # print(est._stat)
-        # print(bkd.diag(mc_group_cov))
-        # print(bkd.diag(Sigma))
+        print(bkd.diag(mc_group_cov))
+        print(bkd.diag(Sigma), "S")
         # print(bkd.diag(mc_group_cov) - bkd.diag(Sigma))
         # print(
         #     (bkd.diag(mc_group_cov) - bkd.diag(Sigma)) / bkd.diag(mc_group_cov)
@@ -614,6 +614,116 @@ class TestGroupACV:
         )
         return cov, W, B, costs, funs, benchmark
 
+    def _check_group_acv_to_traditional_acv(
+        self, est, mfmc_est, benchmark, funs
+    ):
+        # assumes nested mfmc subsets e.g. [0, 1] [1, 2], [2] with 3 models
+        bkd = self.get_backend()
+        import torch
+
+        torch.set_printoptions(precision=12, linewidth=1000)
+        # print(est._traditional_acv_weights())
+        # print(
+        #     mfmc_est._optimized_weights, mfmc_est._optimized_weights.shape, "W"
+        # )
+        samples_per_model = est.generate_samples_per_model(
+            benchmark.variable().rvs
+        )
+        values_per_model = [
+            bkd.array(funs[ii](samples_per_model[ii]))
+            for ii in range(est.nmodels())
+        ]
+        subset_values = est._separate_values_per_model(values_per_model)
+        subset_ests = []
+        for kk in range(est.nsubsets()):
+            if subset_values[kk].shape[0] > 0:
+                print(kk, "K")
+                subset_est = est._stat.sample_estimate(subset_values[kk])
+                print(subset_est.shape, "V")
+            else:
+                subset_est = bkd.zeros(len(est.subsets[kk]))
+            subset_ests.append(subset_est)
+        Q0, Qe, Qu = est._group_to_traditional_estimators(subset_ests)
+        # Q0, Qe, Qu = est._group_to_traditional_estimators_from_alpha(
+        #     subset_ests, mfmc_est._optimized_weights
+        # )
+        nqoi = est._stat.nqoi()
+        print(subset_values[0].shape, subset_values[1].shape, nqoi)
+        print(
+            subset_values[0].mean(axis=0),
+            bkd.var(subset_values[0], axis=0, ddof=1),
+            "M0",
+        )
+        print(
+            subset_values[1].mean(axis=0),
+            bkd.var(subset_values[1], axis=0, ddof=1),
+            "M2",
+        )
+        print(subset_ests)
+        if est.nmodels() == 2:
+            acv_values = [
+                bkd.zeros((0, nqoi)),
+                subset_values[0][:, :nqoi],
+                subset_values[0][:, nqoi : 2 * nqoi],
+                subset_values[1],
+            ]
+        else:
+            acv_values = [
+                bkd.zeros((0, nqoi)),
+                subset_values[0][:, :nqoi],
+                subset_values[0][:, nqoi : 2 * nqoi],
+                subset_values[1][:, :nqoi],
+                subset_values[1][:, nqoi : 2 * nqoi],
+                subset_values[2],
+            ]
+
+        # print(acv_values[0])
+        # print(acv_values[1], subset_values[0], "a")
+        print(bkd.var(acv_values[1], axis=0, ddof=1))
+        print(mfmc_est)
+        Q0_acv = mfmc_est._stat.sample_estimate(acv_values[1])
+        Qe_acv = bkd.stack(
+            [
+                mfmc_est._stat.sample_estimate(acv_values[2 * ii])
+                for ii in range(1, est.nmodels())
+            ],
+            axis=1,
+        )
+        Qu_acv = bkd.stack(
+            [
+                mfmc_est._stat.sample_estimate(acv_values[2 * ii + 1])
+                for ii in range(1, est.nmodels())
+            ],
+            axis=1,
+        )
+        print(Q0, "Q0")
+        print(Q0_acv, "Q0_acv")
+        assert bkd.allclose(Q0, Q0_acv)
+        print(Qe, "Qe")
+        print(Qe_acv, "Qe_acv")
+        print(Qu, "Qu")
+        print(Qu_acv, "Qu_acv")
+        print(bkd.hstack((Qe, Qu)))
+        print(bkd.hstack((Qe_acv, Qu_acv)))
+        for ii in range(Qe.shape[0]):
+            print(
+                bkd.sort(bkd.hstack((Qe[ii], Qu[ii]))),
+                bkd.sort(bkd.hstack((Qe_acv[ii], Qu_acv[ii]))),
+            )
+            assert bkd.allclose(
+                bkd.sort(bkd.abs(bkd.hstack((Qe[ii], Qu[ii])))),
+                bkd.sort(bkd.abs(bkd.hstack((Qe_acv[ii], Qu_acv[ii])))),
+            )
+        assert bkd.allclose(Qe, Qe_acv)
+        assert bkd.allclose(Qu, Qu_acv)
+        # weight conversion is only unqiue up to a sign
+        print(bkd.abs(est._traditional_acv_weights()))
+        print(bkd.abs(mfmc_est._optimized_weights))
+        assert bkd.allclose(
+            bkd.abs(est._traditional_acv_weights()),
+            bkd.abs(mfmc_est._optimized_weights),
+        )
+
     def _check_mfmc_nested_estimation(self, nmodels, qoi_idx, statname):
         bkd = self.get_backend()
         est_type = "nested"
@@ -684,34 +794,10 @@ class TestGroupACV:
             mfmc_est._rounded_npartition_samples,
             round_nsamples=False,
         )
-        import torch
 
-        torch.set_printoptions(precision=12, linewidth=1000)
-        print(est._traditional_acv_weights())
-        print(
-            mfmc_est._optimized_weights, mfmc_est._optimized_weights.shape, "W"
-        )
-
-        samples_per_model = est.generate_samples_per_model(
-            benchmark.variable().rvs
-        )
-        values_per_model = [
-            bkd.array(funs[ii](samples_per_model[ii]))
-            for ii in range(est.nmodels())
-        ]
-        subset_values = est._separate_values_per_model(values_per_model)
-        subset_ests = []
-        for kk in range(est.nsubsets()):
-            if subset_values[kk].shape[0] > 0:
-                subset_est = est._stat.sample_estimate(subset_values[kk])
-            else:
-                subset_est = bkd.zeros(len(est.subsets[kk]))
-            subset_ests.append(subset_est)
-        print(est._group_to_traditional_estimators(subset_ests), "Q")
-        # assert False
-        assert bkd.allclose(
-            est._traditional_acv_weights(), mfmc_est._optimized_weights
-        )
+        # self._check_group_acv_to_traditional_acv(
+        #     est, mfmc_est, benchmark, funs
+        # )
 
         # self._check_sigma_matrix_of_estimator(
         #     est, int(2e4), funs, benchmark.variable()
@@ -767,10 +853,10 @@ class TestGroupACV:
         est.set_optimizer(opt)
         iterate = est._init_guess(target_cost)
         est.allocate_samples(target_cost, iterate=iterate, round_nsamples=True)
-        print(est._optimized_covariance)
-        print(mfmc_est._optimized_covariance)
-        print(est._optimized_covariance - mfmc_est._optimized_covariance)
-        print(est._optimized_criteria, mfmc_est._optimized_criteria)
+        # print(est._optimized_covariance)
+        # print(mfmc_est._optimized_covariance)
+        # print(est._optimized_covariance - mfmc_est._optimized_covariance)
+        # print(est._optimized_criteria, mfmc_est._optimized_criteria)
         assert np.allclose(
             est._optimized_covariance, mfmc_est._optimized_covariance
         )
@@ -780,13 +866,17 @@ class TestGroupACV:
 
     def test_mfmc_nested_estimation(self):
         test_cases = [
-            [3, [0], "mean"],
+            # [2, [0], "mean"],
+            # [3, [0], "mean"],
+            # [2, [0, 1], "mean"],
             # [2, [0, 1, 2], "mean"],
             # [3, [0, 1], "mean"],
+            # [3, [0, 1, 2], "mean"],
             # [2, [0], "variance"],
             # [2, [0, 1, 2], "variance"],
             # [3, [0, 1], "variance"],
             # [2, [0], "mean_variance"],
+            [2, [0, 1], "mean_variance"],
         ]
         for test_case in test_cases:
             np.random.seed(1)

pyapprox/optimization/minimize.py
@@ -410,7 +410,7 @@ class ConstraintPenalizedObjective(Model):
         self._unconstrained_objective = unconstrained_objective
         self._constraints = constraints
         self._penalty = None
-        self._enforce_hard = True
+        self._enforce_hard = enforce_hard
 
     def nqoi(self) -> int:
         return 1
@@ -430,7 +430,8 @@ class ConstraintPenalizedObjective(Model):
                     if self._enforce_hard:
                         return (np.inf + con_val * 0)[:, None]
                     # if constraint violated add a penalty
-                    cons_term += -con_val * self._penalty
+                    else:
+                        cons_term += -con_val * self._penalty
         return self._unconstrained_objective(samples) + cons_term
 
 