c183f2bf7df8c41cb808c47e78dbb20f9ab95192
John Jakeman <29109026+jdjakem@users.noreply.github.com>
2025-11-29 15:37:48 -0700

    Add type safe chained optimizer that chains a global and local optimizer together

---
+++ pyapprox/typing/optimization/minimize/chained/chained_optimizer.py
@@ -0,0 +1,56 @@
+from typing import Generic
+
+from pyapprox.typing.util.backend import Array
+from pyapprox.typing.optimization.minimize.chained.protocols import (
+    OptimizerProtocol,
+)
+from pyapprox.typing.optimization.minimize.scipy.scipy_result import (
+    ScipyOptimizerResultWrapper,
+)
+
+
+class ChainedOptimizer(Generic[Array]):
+    """
+    A chained optimizer that uses a global optimizer followed by a local
+    optimizer.
+
+    This class is useful for combining the strengths of a global optimizer
+    (e.g., differential evolution) with a local optimizer (e.g., trust-constr)
+    for better convergence and accuracy.
+
+    Parameters
+    ----------
+    global_optimizer : ScipyDifferentialEvolutionOptimizer
+        The global optimizer to use for the initial optimization.
+    local_optimizer : ScipyTrustConstrOptimizer
+        The local optimizer to refine the solution obtained by the global
+    optimizer.
+    """
+
+    def __init__(
+        self,
+        global_optimizer: OptimizerProtocol[Array],
+        local_optimizer: OptimizerProtocol[Array],
+    ):
+        self._global_optimizer = global_optimizer
+        self._local_optimizer = local_optimizer
+
+    def minimize(
+        self, init_guess: Array
+    ) -> ScipyOptimizerResultWrapper[Array]:
+        """
+        Perform optimization using the chained approach.
+
+        Returns
+        -------
+        ScipyOptimizerResultWrapper
+            The final optimization result after applying both optimizers.
+        """
+        # Step 1: Perform global optimization
+        global_result = self._global_optimizer.minimize(init_guess)
+
+        # Step 2: Use the result of the global optimizer as the initial guess for the local optimizer
+        print(global_result.optima())
+        local_result = self._local_optimizer.minimize(global_result.optima())
+
+        return local_result
---
+++ pyapprox/typing/optimization/minimize/chained/protocols.py
@@ -0,0 +1,42 @@
+from typing import Protocol, Generic, Optional
+
+from pyapprox.typing.util.backend import Array, Backend
+from pyapprox.typing.optimization.minimize.scipy.scipy_result import (
+    ScipyOptimizerResultWrapper,
+)
+
+
+class OptimizerProtocol(Protocol, Generic[Array]):
+    """
+    This protocol defines the required methods and attributes for an optimizer
+    to be compatible with the ChainedOptimizer.
+    """
+
+    def minimize(
+        self, init_guess: Array
+    ) -> ScipyOptimizerResultWrapper[Array]:
+        """
+        Perform the optimization.
+
+        Parameters
+        ----------
+        init_guess : Optional[Array], optional
+            Initial guess for the optimization variables. Defaults to None.
+
+        Returns
+        -------
+        ScipyOptimizerResultWrapper
+            Wrapped optimization result.
+        """
+        ...
+
+    def bkd(self) -> Backend[Array]:
+        """
+        Get the backend used for computations.
+
+        Returns
+        -------
+        Backend[Array]
+            Backend used for computations.
+        """
+        ...
