57ec1e19e7d7bd51e406a385a71c51ef578dbc92
John Jakeman <29109026+jdjakem@users.noreply.github.com>
2025-11-29 09:08:42 -0700

    restructure of typed optimzation modules

--- pyapprox/typing/optimization/minimize/constraints/protocols/validation.py
+++ pyapprox/typing/optimization/minimize/constraints/protocols/validation.py
@@ -1,100 +1,13 @@
-from typing import (
-    Protocol,
-    runtime_checkable,
-    Generic,
-    Union,
-    Sequence,
-    List,
-    Any,
-)
+from typing import Sequence, Any
 
-from pyapprox.typing.util.backend import Array, Backend
-from pyapprox.typing.optimization.linear_constraint import (
+from pyapprox.typing.optimization.minimize.constraints.protocols.nonlinear import (
+    NonlinearConstraintProtocol,
+    NonlinearConstraintProtocolWithJacobian,
+    NonlinearConstraintProtocolWithJacobianAndHVP,
+)
+from pyapprox.typing.optimization.minimize.constraints.linear import (
     PyApproxLinearConstraint,
 )
-
-
-from typing import Protocol, runtime_checkable, Generic
-from pyapprox.typing.util.backend import Array, Backend
-from scipy.optimize import LinearConstraint as ScipyLinearConstraint
-
-
-@runtime_checkable
-class LinearConstraintProtocol(Protocol, Generic[Array]):
-    def to_scipy(self) -> ScipyLinearConstraint: ...
-
-    def A(self) -> Array: ...
-
-    def lb(self) -> Array: ...
-
-    def ub(self) -> Array: ...
-
-    def bkd(self) -> Backend[Array]: ...
-
-
-@runtime_checkable
-class NonlinearConstraintProtocol(Protocol, Generic[Array]):
-    def bkd(self) -> Backend[Array]: ...
-
-    def nvars(self) -> int: ...
-
-    def nqoi(self) -> int: ...
-
-    def __call__(self, samples: Array) -> Array: ...
-
-    def lb(self) -> Array: ...
-
-    def ub(self) -> Array: ...
-
-
-@runtime_checkable
-class NonlinearConstraintProtocolWithJacobian(Protocol, Generic[Array]):
-    def bkd(self) -> Backend[Array]: ...
-
-    def nvars(self) -> int: ...
-
-    def nqoi(self) -> int: ...
-
-    def __call__(self, samples: Array) -> Array: ...
-
-    def lb(self) -> Array: ...
-
-    def ub(self) -> Array: ...
-
-    def jacobian(self, sample: Array) -> Array: ...
-
-
-@runtime_checkable
-class NonlinearConstraintProtocolWithJacobianAndHVP(Protocol, Generic[Array]):
-    def bkd(self) -> Backend[Array]: ...
-
-    def nvars(self) -> int: ...
-
-    def nqoi(self) -> int: ...
-
-    def __call__(self, samples: Array) -> Array: ...
-
-    def lb(self) -> Array: ...
-
-    def ub(self) -> Array: ...
-
-    def jacobian(self, sample: Array) -> Array: ...
-
-    def weighted_hvp(self, sample: Array, vec: Array) -> Array: ...
-
-
-UnionOfNonlinearConstraintProtocols = Union[
-    NonlinearConstraintProtocol[Array],
-    NonlinearConstraintProtocolWithJacobian[Array],
-    NonlinearConstraintProtocolWithJacobianAndHVP[Array],
-]
-
-SequenceOfUnionOfConstraintProtocols = Sequence[
-    Union[
-        UnionOfNonlinearConstraintProtocols[Array],
-        LinearConstraintProtocol[Array],
-    ]
-]
 
 
 def validate_nonlinear_constraint(obj: object) -> None:
