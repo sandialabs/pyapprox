3caefb6b21d2d006f17931ad77da3b6adb244f34
John Jakeman <29109026+jdjakem@users.noreply.github.com>
2025-12-08 07:12:47 -0700

    Add Phase 6: Extended univariate polynomials (Laguerre, discrete, numeric)
    - Add LaguerrePolynomial1D for Gamma distribution
    - Add discrete polynomials:
      - KrawtchoukPolynomial1D for Binomial distribution
      - HahnPolynomial1D for Hypergeometric distribution
      - CharlierPolynomial1D for Poisson distribution
      - DiscreteChebyshevPolynomial1D for uniform discrete
    - Add numeric polynomials from arbitrary measures:
      - DiscreteNumericOrthonormalPolynomial1D using Lanczos algorithm
      - WeightedSamplePolynomial1D for uniform weights
    - Add comprehensive tests (20 tests, all passing)
    - Total affine module: 147 tests passing

--- pyapprox/typing/surrogates/affine/univariate/__init__.py
+++ pyapprox/typing/surrogates/affine/univariate/__init__.py
@@ -1,4 +1,25 @@
-"""Univariate basis functions for affine surrogates."""
+"""Univariate basis functions for affine surrogates.
+
+This module provides orthonormal polynomial bases for various
+probability distributions:
+
+Continuous Distributions
+------------------------
+- Jacobi family: Legendre (uniform), Chebyshev (arcsine)
+- Hermite: Gaussian distribution
+- Laguerre: Gamma distribution
+
+Discrete Distributions
+----------------------
+- Krawtchouk: Binomial distribution
+- Hahn: Hypergeometric distribution
+- Charlier: Poisson distribution
+- DiscreteChebyshev: Uniform discrete distribution
+
+Arbitrary Measures
+------------------
+- DiscreteNumericOrthonormalPolynomial1D: From weighted samples
+"""
 
 from pyapprox.typing.surrogates.affine.univariate.orthopoly_base import (
     OrthonormalPolynomial1D,
@@ -19,6 +40,28 @@ from pyapprox.typing.surrogates.affine.univariate.hermite import (
     hermite_recurrence,
 )
 
+from pyapprox.typing.surrogates.affine.univariate.laguerre import (
+    LaguerrePolynomial1D,
+    laguerre_recurrence,
+)
+
+from pyapprox.typing.surrogates.affine.univariate.discrete import (
+    KrawtchoukPolynomial1D,
+    krawtchouk_recurrence,
+    HahnPolynomial1D,
+    hahn_recurrence,
+    CharlierPolynomial1D,
+    charlier_recurrence,
+    DiscreteChebyshevPolynomial1D,
+    discrete_chebyshev_recurrence,
+)
+
+from pyapprox.typing.surrogates.affine.univariate.numeric import (
+    DiscreteNumericOrthonormalPolynomial1D,
+    WeightedSamplePolynomial1D,
+    lanczos_recursion,
+)
+
 from pyapprox.typing.surrogates.affine.univariate.quadrature import (
     GaussQuadratureRule,
     GaussLobattoQuadratureRule,
@@ -29,15 +72,31 @@ __all__ = [
     "OrthonormalPolynomial1D",
     "evaluate_orthonormal_polynomial_1d",
     "evaluate_orthonormal_polynomial_derivatives_1d",
-    # Jacobi family
+    # Jacobi family (continuous)
     "JacobiPolynomial1D",
     "LegendrePolynomial1D",
     "Chebyshev1stKindPolynomial1D",
     "Chebyshev2ndKindPolynomial1D",
     "jacobi_recurrence",
-    # Hermite family
+    # Hermite family (continuous)
     "HermitePolynomial1D",
     "hermite_recurrence",
+    # Laguerre family (continuous)
+    "LaguerrePolynomial1D",
+    "laguerre_recurrence",
+    # Discrete polynomials
+    "KrawtchoukPolynomial1D",
+    "krawtchouk_recurrence",
+    "HahnPolynomial1D",
+    "hahn_recurrence",
+    "CharlierPolynomial1D",
+    "charlier_recurrence",
+    "DiscreteChebyshevPolynomial1D",
+    "discrete_chebyshev_recurrence",
+    # Numeric/arbitrary polynomials
+    "DiscreteNumericOrthonormalPolynomial1D",
+    "WeightedSamplePolynomial1D",
+    "lanczos_recursion",
     # Quadrature
     "GaussQuadratureRule",
     "GaussLobattoQuadratureRule",
