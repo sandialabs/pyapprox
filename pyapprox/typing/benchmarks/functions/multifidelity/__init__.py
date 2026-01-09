46f180f51ab9f353f29c74bb7c2b82b7e899e3af
John Jakeman <29109026+jdjakem@users.noreply.github.com>
2026-01-08 20:01:47 -0700

    Add polynomial ensemble multifidelity benchmark
    Configurable polynomial model ensemble for testing
    multifidelity estimators with known correlations.

--- pyapprox/typing/benchmarks/functions/multifidelity/__init__.py
+++ pyapprox/typing/benchmarks/functions/multifidelity/__init__.py
@@ -1,1 +1,11 @@
 """Multifidelity test function ensembles."""
+
+from pyapprox.typing.benchmarks.functions.multifidelity.polynomial_ensemble import (
+    PolynomialModelFunction,
+    PolynomialEnsemble,
+)
+
+__all__ = [
+    "PolynomialModelFunction",
+    "PolynomialEnsemble",
+]
